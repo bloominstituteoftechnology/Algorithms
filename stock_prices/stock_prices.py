e7e8a2f01b2e0778a09e2d1e0c7ccb9f4aaf1317
Sean Chen <skypemaster007@gmail.com>
2018-08-23 16:33:08 -0500

    Add test_stock_prices.py

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -4,7 +4,8 @@ def find_max_profit(prices):
   min_price = prices[0]
   max_profit = prices[1] - min_price
 
-  for price in prices:
+  for i in range(1, len(prices)):
+    price = prices[i]
     max_profit = max(price - min_price, max_profit)
     min_price = min(price, min_price)
 
