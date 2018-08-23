d99eefbf6bd90bb5abbcec19aa37cc463b5e19ec
Sean Chen <skypemaster007@gmail.com>
2018-08-23 13:21:26 -0500

    Remove unused main in stock_prices.py

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python
 
-import sys
-
 def find_max_profit(prices):
   min_price = prices[0]
   max_profit = prices[1] - min_price
@@ -11,11 +9,3 @@ def find_max_profit(prices):
     min_price = min(price, min_price)
 
   return max_profit
-
-
-if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    prices = sys.argv[1]
-    print("Max profit for the given list of prices is {profit}.".format(profit=find_max_profit(prices)))
-  else:
-    print("Usage: stock_prices.py [prices]")
