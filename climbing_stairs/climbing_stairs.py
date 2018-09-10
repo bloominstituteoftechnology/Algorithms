b48a6bb66d48c7407bfd0890c5d94d392b96dfc4
Daniel Chamorro <dan@dcitllc.com>
2018-09-10 18:57:46 -0400

    Completed

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -2,26 +2,29 @@
 
 import sys
 
+
 def climbing_stairs(n, cache=None):
-  # what happens when n == 0?
-  if n == 0:
-    return 1
-  elif n < 0:
-    return 0
-  # check if the cache has our answer
-  elif cache and cache[n] > 0:
-    return cache[n]
-  else:
-    if not cache:
-      # initialize a cache
-      cache = {i: 0 for i in range(n+1)}
-    cache[n] = climbing_stairs(n-1, cache) + climbing_stairs(n-2, cache) + climbing_stairs(n-3, cache) 
-    return cache[n]
+    # what happens when n == 0?
+    if n == 0:
+        return 1
+    elif n < 0:
+        return 0
+    # check if the cache has our answer
+    elif cache and cache[n] > 0:
+        return cache[n]
+    else:
+        if not cache:
+            # initialize a cache
+            cache = {i: 0 for i in range(n+1)}
+        cache[n] = climbing_stairs(
+            n-1, cache) + climbing_stairs(n-2, cache) + climbing_stairs(n-3, cache)
+        return cache[n]
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_stairs = int(sys.argv[1])
-    print("There are {ways} ways for a child to jump {n} stairs.".format(ways=climbing_stairs(num_stairs), n=num_stairs))
-  else:
-    print('Usage: climbing_stairs.py [num_stairs]')
+    if len(sys.argv) > 1:
+        num_stairs = int(sys.argv[1])
+        print("There are {ways} ways for a child to jump {n} stairs.".format(
+            ways=climbing_stairs(num_stairs), n=num_stairs))
+    else:
+        print('Usage: climbing_stairs.py [num_stairs]')
--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,31 +2,33 @@
 
 import sys
 
+
 def rock_paper_scissors(n):
-  # return an array
-  outcomes = []
-  # we don't have a list of possible plays
-  plays = ['rock', 'paper', 'scissors']
+    # return an array
+    outcomes = []
+    # we don't have a list of possible plays
+    plays = ['rock', 'paper', 'scissors']
 
-  # generate a n-length permutation of possible plays
-  # base case when n == 0
-  # we'll move towards our base by decrementing n
-  # every time we decrement n, we'll add another possible play to the list we're generating
+    # generate a n-length permutation of possible plays
+    # base case when n == 0
+    # we'll move towards our base by decrementing n
+    # every time we decrement n, we'll add another possible play to the list we're generating
 
-  # define an inner recursive helper function
-  def generate_plays(rounds_left, result=[]):
-    if rounds_left == 0:
-      outcomes.append(result)
-      return
-    for play in plays:
-      generate_plays(rounds_left - 1, result + [play])
+    # define an inner recursive helper function
+    def generate_plays(rounds_left, result=[]):
+        if rounds_left == 0:
+            outcomes.append(result)
+            return
+        for play in plays:
+            generate_plays(rounds_left - 1, result + [play])
 
-  generate_plays(n, [])
-  return outcomes
+    generate_plays(n, [])
+    return outcomes
+
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
