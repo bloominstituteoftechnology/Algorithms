111e8263c64522960e605ee48bf0b82661414734
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-09-10 15:50:10 -0700

    cache note

---
+++ notes.py
@@ -0,0 +1,31 @@
+fib_cache = {}
+def fib(n):
+    # if we have cached the value, then return it
+    if n in fib_cache:
+        return fib_cache[n]
+    # compute the Nth term
+    if n <= 2:
+        value = n
+    elif n>2:
+      value = fib(n-1) + fib(n-2)
+
+    # cache the value and return it
+    fib_cache[n] = value
+    return value
+
+print(fib(5))
+
+
+# *** V2 ***
+
+def fib(n, cache):
+    if n < 2:
+        return n
+    elif cache[n] > 0:
+        return cache[n]
+    cache[n] = fib(n - 1, cache) + fib(n - 2, cache)
+    return cache[n]
+
+n = 35
+cache = {i: 0 for i in range(n + 1)}
+print(fib(n, cache))
