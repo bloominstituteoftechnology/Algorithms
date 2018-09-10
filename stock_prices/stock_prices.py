8f731385d732ff702b4cda8e29d2008180f92bab
Ari Mercado <ari@Apples-Mac-3.local>
2018-09-10 14:23:03 -0700

    stock prices passes 2/4 tests

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -7,15 +7,27 @@ def find_max_profit(prices):
    # keep track of the max profit we've seen so far
    # iterate through our prices list and update these two variables
    # return our max profit we've seen so far
-   min_price = prices[0]
-   max_profit = prices[1] - min_price
+  #  min_price = prices[0]
+  #  max_profit = prices[1] - min_price
 
-   for i in range(1, len(prices)):
-     price = prices[i]
-     max_profit = max(price - min_price, max_profit)
-     min_price = min(price, min_price)
+  #  for i in range(1, len(prices)):
+  #    price = prices[i]
+  #    max_profit = max(price - min_price, max_profit)
+  #    min_price = min(price, min_price)
   
-   return max_profit
+  #  return max_profit
+  max_price = max(prices)
+  lowest_price = min(prices)
+  prices_and_indexs = [[price, prices.index(price)] for price in prices]
+  for price_and_index in prices_and_indexs:
+    for price in prices:
+      if max_price == price and price_and_index[1] < prices.index(price):
+        #print(price_and_index[1], prices.index(price))
+        return price - lowest_price
+    else:
+      prices.remove(max_price)
+      new_max_price = max(prices)
+      return new_max_price - max_price
 
 
 if __name__ == '__main__':
