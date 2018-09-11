e4cf9326270cc3184f6e9796571de0f6eb7b75b6
chainchompa <chainchompa@gmail.com>
2018-09-11 18:10:26 -0400

    completed climbing stairs

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -2,9 +2,18 @@
 
 import sys
 
-def climbing_stairs(n):
-  pass
-
+def climbing_stairs(n, cache=None):
+  if n == 0 or n == 1:
+    return 1
+  elif n == 2:
+    return 2
+  elif cache and cache[n]:
+    return cache[n]
+  else:
+    if not cache:
+        cache = {i: 0 for i in range(n+1)}
+    cache[n] = climbing_stairs(n-1, cache) + climbing_stairs(n-2, cache) + climbing_stairs(n-3, cache)
+    return cache[n]
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
