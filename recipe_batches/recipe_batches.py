5a290e3b02b123f685b8b43f8bd08ca03340f90d
William <kwontaewoongah@gmail.com>
2018-10-05 14:19:33 -0700

    finished recipe_batches.py

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,12 +3,24 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  batches=None
+  ingredient_keys=ingredients.keys()
+  for ingredient in ingredient_keys:
+    enough_ingredient=0
+    while ingredients[ingredient]-recipe[ingredient]>=0:
+      enough_ingredient+=1
+      ingredients[ingredient]-=recipe[ingredient]
+    if batches==None:
+      batches=enough_ingredient
+    elif enough_ingredient<batches:
+      batches=enough_ingredient
+  return batches
+
 
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
   # your implementation with different inputs
   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+  ingredients = { 'milk': 132, 'butter': 50, 'flour': 51 }
   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
