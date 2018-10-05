eb46ab104be19cad6bb3d19fc32ca8cb3122f9ff
Erik Kimsey <erik@erikkimsey.com>
2018-10-05 17:35:44 -0400

    Second commit

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python
+# Changes to simply make a commit and PR
 
 import sys
 
+
 def climbing_stairs(n, cache=None):
-  pass 
+    pass
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_stairs = int(sys.argv[1])
-    print("There are {ways} ways for a child to jump {n} stairs.".format(ways=climbing_stairs(num_stairs), n=num_stairs))
-  else:
-    print('Usage: climbing_stairs.py [num_stairs]')
+    if len(sys.argv) > 1:
+        num_stairs = int(sys.argv[1])
+        print("There are {ways} ways for a child to jump {n} stairs.".format(
+            ways=climbing_stairs(num_stairs), n=num_stairs))
+    else:
+        print('Usage: climbing_stairs.py [num_stairs]')
