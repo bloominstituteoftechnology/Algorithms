d62779f5f2d8a4f6d02c04adf85dfb92a407efab
William <kwontaewoongah@gmail.com>
2018-10-05 19:26:31 -0700

    wrote an iterative solution to climbing_stairs that seems to be o of n passes small tests needs to be refactored for big

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -3,7 +3,17 @@
 import sys
 
 def climbing_stairs(n, cache=None):
-  pass 
+  if n==0 or n==1:    
+     return 1   
+  elif n==2:     
+    return 2   
+  else:     
+    num_list=[1,1,2]    
+    counter=2     
+    while counter is not n:       
+      num_list.append(num_list[-1]+num_list[-2]+num_list[-3])       
+      counter+=1     
+      return num_list[-1]   
 
 
 if __name__ == "__main__":
