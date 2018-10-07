7b150e87a823cf49495a2a8a7b2d0e0b174d29ce
William <kwontaewoongah@gmail.com>
2018-10-07 15:33:44 -0700

    refactored rps.py

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -5,24 +5,17 @@ import sys
 def rock_paper_scissors(n):
   if n==0:
     return [[]]
-  elif n==1:
-    return [['rock'],['paper'],['scissors']]
   else:
-    counter=2
-    choices_dict={0:[[]],1:[['rock'],['paper'],['scissors']]}
+    counter=1
+    choices_dict={0:[[]]}
     while counter<=n:
       last_entry=choices_dict[list(choices_dict.keys())[-1]]
       choices_dict[counter]=[]
       for i in range(len(last_entry)):
-        last_entry_item=last_entry[i][0:]
-        last_entry_item.append('rock')
-        choices_dict[counter].append(last_entry_item)
-        last_entry_item=last_entry_item[0:-1]
-        last_entry_item.append('paper')
-        choices_dict[counter].append(last_entry_item)
-        last_entry_item=last_entry_item[0:-1]
-        last_entry_item.append('scissors')
-        choices_dict[counter].append(last_entry_item)
+        last_entry_item=last_entry[i]
+        choices_dict[counter].append(last_entry_item+['rock'])
+        choices_dict[counter].append(last_entry_item+['paper'])
+        choices_dict[counter].append(last_entry_item+['scissors'])
       counter+=1
   return choices_dict[n]
 
