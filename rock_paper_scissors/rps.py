ca188d4bfdc3d5c1e49ec666f344184dbf8f04f8
William <kwontaewoongah@gmail.com>
2018-10-07 05:50:51 -0700

    came up with an iterative solution to rps that passes tests

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,7 +3,28 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  if n==0:
+    return [[]]
+  elif n==1:
+    return [['rock'],['paper'],['scissors']]
+  else:
+    counter=2
+    choices_dict={0:[[]],1:[['rock'],['paper'],['scissors']]}
+    while counter<=n:
+      last_entry=choices_dict[list(choices_dict.keys())[-1]]
+      choices_dict[counter]=[]
+      for i in range(len(last_entry)):
+        last_entry_item=last_entry[i][0:]
+        last_entry_item.append('rock')
+        choices_dict[counter].append(last_entry_item)
+        last_entry_item=last_entry_item[0:-1]
+        last_entry_item.append('paper')
+        choices_dict[counter].append(last_entry_item)
+        last_entry_item=last_entry_item[0:-1]
+        last_entry_item.append('scissors')
+        choices_dict[counter].append(last_entry_item)
+      counter+=1
+  return choices_dict[n]
 
 
 if __name__ == "__main__":
