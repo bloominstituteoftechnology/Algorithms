26e1561aeb1bf8d70302f1836d203bb0e50fe8e6
codejoncode <jonathanjholloway@gmail.com>
2018-10-08 16:36:24 -0500

    Completed the rock_paper_scissors.py assignment

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,20 @@
 
 import sys
 
+from itertools import product
+
+
 def rock_paper_scissors(n):
-  pass 
+    choices = ['rock', 'scissors', 'paper']
+
+    result = product(choices, repeat=n)
+
+    return list(result)
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
