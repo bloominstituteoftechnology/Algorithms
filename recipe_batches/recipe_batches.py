5ec287c6969ae2a1b86c045270f6bc826fc6a756
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-10-08 19:59:02 -0400

    recipe_batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,11 +3,19 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+    min_batches = None
+    for i in recipe:
+        if i in ingredients:
+            batches = math.floor(ingredients[i] / recipe[i])
+        else:
+            batches = 0
+        min_batches =  batches if min_batches is None or batches < min_batches else min_batches
+    return min_batches
+
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
+  # Change the entries of these dictionaries to test
   # your implementation with different inputs
   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
   ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
