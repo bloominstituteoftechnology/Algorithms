9b27c35d49148fb7ae355862dda4af7368456f87
LaikaFusion <newrad4@gmail.com>
2018-10-08 15:55:44 -0400

    attempted stair problem

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python
+import functools,sys
 
-import sys
-
+@functools.lru_cache(maxsize=None)
 def climbing_stairs(n, cache=None):
-  pass 
+    if (n == 1 or n == 0):
+        return 1
+    elif (n == 2):
+        return 2
+   
+    return climbing_stairs(n-1) + climbing_stairs(n-2) + climbing_stairs(n-3)
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_stairs = int(sys.argv[1])
-    print("There are {ways} ways for a child to jump {n} stairs.".format(ways=climbing_stairs(num_stairs), n=num_stairs))
-  else:
-    print('Usage: climbing_stairs.py [num_stairs]')
+    if len(sys.argv) > 1:
+        num_stairs = int(sys.argv[1])
+        print("There are {ways} ways for a child to jump {n} stairs.".format(
+            ways=climbing_stairs(num_stairs), n=num_stairs))
+    else:
+        print('Usage: climbing_stairs.py [num_stairs]')
