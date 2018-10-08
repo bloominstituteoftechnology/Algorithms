bedd2e3ef7e7ae80919b562b9a99db9067011b83
Rachel DiCesare <racheldicesare@missrachelthedeveloper.com>
2018-10-08 19:12:35 -0400

    completed stock prices

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,9 +1,17 @@
-#!/usr/bin/python
-
 import argparse
 
 def find_max_profit(prices):
+  max_prof = prices[1] - prices[0]
+  for x in range(1 , len(prices)):
+    for y in range(x+1, len(prices)):
+      difference = prices[y] - prices[x]
+      if difference > max_prof:
+        max_prof = difference
+  return max_prof
   
+  
+  
+
 
 
 if __name__ == '__main__':
@@ -13,3 +21,22 @@ if __name__ == '__main__':
   args = parser.parse_args()
 
   print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
