c035f9183c092765423272af25de26be44c82c02
Carey <careycbaldwin@gmail.com>
2018-10-08 16:47:47 -0400

    wrote two versions of find_max_profit function

--- 1_stock_prices/stock_prices.py
+++ 1_stock_prices/stock_prices.py
@@ -1,10 +1,34 @@
 #!/usr/bin/python
 
+import math
 import argparse
 
-def find_max_profit(prices):
-  pass
+# References
+# https://medium.com/@antash/six-ways-to-find-max-value-of-a-list-in-python-b7d7ccfabc0d
 
+
+# How can I turn a list into an object?
+# How can I iterate over an object?
+
+def find_max_profit(prices):
+  max = 0
+  prices = [int(x) for x in prices]
+  for price in prices:
+    if price > max:
+      max = price
+  return max
+
+# recursive version
+
+# def find_max_profit(prices):
+#   if len(prices) == 1:
+#     return prices[0]
+#   v1 = prices[0]
+#   v2 = find_max_profit(prices[1:])
+#   if v1 > v2:
+#     return v1
+#   else:
+#     return v2
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
