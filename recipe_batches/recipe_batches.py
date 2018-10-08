d9ba81dd71ab0e99142decd8bc1706ff334b7d87
LaikaFusion <newrad4@gmail.com>
2018-10-08 13:17:21 -0400

    recipe batches problem done

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,12 +3,18 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  canMakeArr = []
+  for key in list(recipe.keys()):
+    if key not in ingredients:
+      return(0)
+    possibleBatchs = math.floor(ingredients[key]/ recipe[key])
+    canMakeArr.append(possibleBatchs)
+  return (sorted(canMakeArr)[0])
 
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
   # your implementation with different inputs
   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+  ingredients = { 'milk': 132, 'butter': 50, 'flour': 51 }
   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
