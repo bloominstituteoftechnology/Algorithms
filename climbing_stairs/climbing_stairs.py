fabb14431ed9343f3521912d021fb77fdadc55b3
MichaelBoegner <cmikeb@gmail.com>
2018-10-08 17:39:50 -0400

    Think I've figured out algorithm. Now implementation

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -2,8 +2,18 @@
 
 import sys
 
+#Built dict determining the base number of whole actions possible for each option of 3,2,1 respectively 
+#Next step is to iterate over n - base number of whole actions and the subsequent permutations to get n to 0 
+#if options[3] > 0, then n - (3 * permutation(options[3])) and then recurse this until n = 0 for each option[x] reduction 
+#keep dict of permutations and check to see if it's been called already via memoizer
+
 def climbing_stairs(n, cache=None):
-  pass 
+  options = dict((k, int(n/k)) for k, v in options.items())
+  count = 0
+
+  print(options[1])
+
+  print(options)
 
 
 if __name__ == "__main__":
