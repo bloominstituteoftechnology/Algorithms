0263abcbd86a6c00a53f1fb75ceffd020a4c8de9
your <jurgenkela@gmail.com>
2018-10-08 18:20:12 -0400

    stock_prices completed

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,8 +3,17 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  min_price = prices[0]
+  max_profit = prices[1] - prices[0]
+  for i in range(1, len(prices)):
+    if prices[i] < min_price:
+      min_price = prices[i]
+    elif prices[i] - min_price > max_profit:
+      max_profit = prices[i] - min_price
+  return max_profit
 
+
+# find_max_profit([1050, 270, 1540, 3800, 2])
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
