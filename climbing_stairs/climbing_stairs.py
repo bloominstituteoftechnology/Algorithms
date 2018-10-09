8034879bf6679981a548293fcdebd137b83a375f
Grant Reighard <grant.reighard@gmail.com>
2018-10-09 15:28:28 -0400

    Inital commit. Completed first three problems.

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -2,9 +2,25 @@
 
 import sys
 
-def climbing_stairs(n, cache=None):
-  pass 
+n = 50
+cache = {i: 0 for i in range(n + 1)}
 
+def climbing_stairs(n, cache=None):
+  if n < 0:
+    return 0
+  elif n == 0 or n == 1:
+    return 1
+  elif n == 2:
+    return 2
+  elif n == 3:
+    return 4
+  elif cache and cache[n]:
+    return cache[n]
+  else:
+    if not cache:
+      cache = {i: 0 for i in range(n + 1)}
+    cache[n] = climbing_stairs(n-1, cache) + climbing_stairs(n-2, cache) + climbing_stairs(n-3, cache)
+    return cache[n]
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,7 +3,12 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  largest = -100
+  for i, first_el in enumerate(prices):
+    for j, second_el in enumerate(prices):
+      if j > i and second_el - first_el > largest:
+        largest = second_el - first_el
+  return largest
 
 
 if __name__ == '__main__':
