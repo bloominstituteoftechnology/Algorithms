508501632904a3718c0aaef3765fdd91d48365e3
LaikaFusion <newrad4@gmail.com>
2018-10-09 17:22:15 -0400

    value weight method

--- knapsack/knapsack.py
+++ knapsack/knapsack.py
@@ -6,23 +6,31 @@ from collections import namedtuple
 Item = namedtuple('Item', ['index', 'size', 'value'])
 
 def knapsack_solver(items, capacity):
-  return greedy(items,capacity)
-  
-  
-def greedy(items,capacity):
   itemsSelected =[]
   totalValue = 0
   capcityIntial = capacity
-  sortedItemsValue = sorted(items, key=lambda k: k['value'],reverse=True)
-
+  sortedItemsValue = valueCalc(items,capacity)
   for item in sortedItemsValue:
     if capacity-item["weight"] < 0:
       continue
     itemsSelected.append(item['name'])
     capacity -= item["weight"]
     totalValue += item["value"]
-  return {'Value':totalValue,"Size":capcityIntial-capacity,"Chosen":itemsSelected}
 
+  print(sorted(itemsSelected))
+  return {'Value':totalValue,"Size":capcityIntial-capacity,"Chosen":sorted(itemsSelected)}
+
+
+  
+def greedy(items,capacity):
+  return sorted(items, key=lambda k: k['value'],reverse=True)
+
+ 
+
+def valueCalc(items,capacity):
+  for item in items:
+    item["calcedRatio"] = item["value"]/item["weight"]
+  return sorted(items, key=lambda k: k['calcedRatio'],reverse=True)
 
 if __name__ == '__main__':
   if len(sys.argv) > 1:
