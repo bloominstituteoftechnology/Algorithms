b31cffddac41a2e26ace40f745f8eff792517978
rverdi642 <rverdi642@gmail.com>
2018-10-09 16:41:41 -0500

    Richard Verdier -Climbing stairs & Stock Prices RPS done

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,14 +1,29 @@
 #!/usr/bin/python
 
 import sys
+#
+# def rock_paper_scissors(n):
+#   pass
 
 def rock_paper_scissors(n):
-  pass 
+  list = ['rock', 'paper', 'scissors']
+  outcome = []
 
+  def helper(storage, plays):
+    if plays == 0:
+      outcome.append(storage)
+      return
+    for l in list:
+      helper(storage + [l], plays - 1)
 
-if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+  helper([], n)
+  return outcome
+
+print(rock_paper_scissors(5))
+
+# if __name__ == "__main__":
+#   if len(sys.argv) > 1:
+#     num_plays = int(sys.argv[1])
+#  print(rock_paper_scissors(5))
+# else:
+#   print('Usage: rps.py [num_plays]')
