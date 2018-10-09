cea749b4dc2ebaa7c88782e5be2a2e99a07781d5
MichaelBoegner <cmikeb@gmail.com>
2018-10-09 16:48:13 -0400

    attempting rps again

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -5,6 +5,26 @@ import sys
 def rock_paper_scissors(n):
   pass 
 
+#   n = 3
+# moves = ['rock', 'paper', 'scissors']
+
+# def rps(n, moves, x=-1, count=0):
+#   game = []
+#   count += 1
+
+#   if count > n:
+#     return game
+
+#   if x > 2:
+#     x = -1
+  
+#   x += 1
+#   game = [[move, moves[x]] for move in moves] 
+
+#   return rps(n, moves)
+
+# print(rps(n, moves))
+
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
