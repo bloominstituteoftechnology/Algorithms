147c9e7ca7e6d73564564777091e4afbca739890
Sukhada Gholba <sukhadagholba@gmail.com>
2018-10-08 22:29:16 -0700

    completed stock price problem

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,13 +1,35 @@
 #!/usr/bin/python
 
 import argparse
+import math
+
+
+diff = []
+
 
 def find_max_profit(prices):
-  pass
+    global diff
+    diff = []
+
+    for i in prices:
+        find_diff(i, prices)
+
+    diff.sort()
+    return (diff[len(diff)-1])   
+       
+
+def find_diff(a, prices):
+    global diff
+    
+    for j in prices:
+        if prices.index(a) < prices.index(j):
+            diff.append(j-a)
+
+
 
 
 if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
+  #This is just some code to accept inputs from the command line
   parser = argparse.ArgumentParser(description='Find max profit from prices.')
   parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
   args = parser.parse_args()
