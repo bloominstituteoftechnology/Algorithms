50b84ee5326b0b82554aa3cf2f03ece0a90cbda8
tulf <timzulf@gmail.com>
2018-10-09 17:37:17 -0400

    Tim Zulf- Just commiting what passes.

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -3,12 +3,17 @@
 import sys
 
 def climbing_stairs(n, cache=None):
-  pass 
+    count = {0:1, 1:1, 2:2}
+    counter = 3
+    while counter <= n:
+        count[counter] = count[counter-1] + count[counter-2] + count[counter-3]
+        counter += 1
+    return count[n]
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_stairs = int(sys.argv[1])
+    if len(sys.argv) > 1:
+        num_stairs = int(sys.argv[1])
     print("There are {ways} ways for a child to jump {n} stairs.".format(ways=climbing_stairs(num_stairs), n=num_stairs))
-  else:
+else:
     print('Usage: climbing_stairs.py [num_stairs]')
