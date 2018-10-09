062f2ae3338c317ebc7d954c76dbfc3d9b7734be
JoCatherine Griggs <jocafrin@gmail.com>
2018-10-09 08:28:39 -0400

    climbing_stairs complete

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -3,7 +3,12 @@
 import sys
 
 def climbing_stairs(n, cache=None):
-  pass 
+    stair_count={0:1,1:1,2:2}
+    counter=3
+    while counter<=n:
+      stair_count[counter]=stair_count[counter-1]+stair_count[counter-2]+stair_count[counter-3]
+      counter+=1
+    return stair_count[n]
 
 
 if __name__ == "__main__":
