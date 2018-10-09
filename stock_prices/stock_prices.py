8710afbdbb343df1a9751b9cb172cc60d70762d1
AI2EJAY <JHammett1314@gmail.com>
2018-10-08 20:14:43 -0500

    Working on stocks. right now not dry at all & not what I need

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,7 +3,30 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  cheapStock = 9999999999999999999999999999999999999999999999
+  expensiveStock = 0
+  cheapCount = 0
+  expensiveCount = 0
+  cheapestAtCount = 0
+  expensiveAtCount = 0
+
+  for price in prices:
+    cheapCount += 1
+    print(cheapCount)
+    if cheapStock > price:
+      cheapStock = price
+      cheapestAtCount = cheapCount   
+      print([cheapStock, cheapestAtCount])
+
+  for price in prices:
+    expensiveCount += 1
+    print(expensiveCount)
+    if expensiveStock < price:
+      expensiveStock = price 
+      expensiveAtCount = expensiveCount    
+      print([expensiveStock, expensiveAtCount])
+
+  return expensiveStock - cheapStock
 
 
 if __name__ == '__main__':
