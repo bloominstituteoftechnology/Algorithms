815a91c939b9419bec04f3ff4186082faf4066ac
mkerbleski <mkerbles@gmail.com>
2018-10-09 13:10:33 -0600

    complete climbing_stairs for small numbers

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -3,8 +3,19 @@
 import sys
 
 def climbing_stairs(n, cache=None):
-  pass 
 
+  if n <= 0: 
+    return 0
+  elif n == 1: 
+    return 1
+  elif n == 2: 
+    return 2
+  elif n == 3: 
+    return 4 
+  return climbing_stairs(n-1, cache) + climbing_stairs(n-2, cache) + climbing_stairs(n-3, cache)   
+
+  pass 
+# does not work for big numbers yet
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
