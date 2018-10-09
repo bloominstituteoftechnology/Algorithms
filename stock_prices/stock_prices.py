be9aa4bd40a771d483dfd17c469f861daeb58986
Alberto-Funk <laptopreneur@gmail.com>
2018-10-08 19:02:23 -0700

    Stock Prices Complete

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,7 +3,37 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  low = prices[0]
+  i = 0
+  while i < len(prices):
+    if prices[i] < low:
+      low = prices[i]
+    i += 1
+  if low == prices[-1]:
+    k = 0
+    secondLow = prices[0]
+    while k < len(prices) - 1:
+      if prices[k] < secondLow:
+        secondLow = prices[k]
+      k += 1
+    j = prices.index(secondLow)
+    high = prices[prices.index(secondLow) + 1]
+    while j < len(prices):
+      if prices[j] > high:
+        high = prices[j] 
+      j += 1
+    if high == secondLow:
+      return -(high - low)
+    else:
+      return high - secondLow
+  else:
+    j = prices.index(low)
+    high = prices[prices.index(low) + 1]
+    while j < len(prices):
+      if prices[j] > high:
+        high = prices[j] 
+      j += 1
+    return high - low
 
 
 if __name__ == '__main__':
