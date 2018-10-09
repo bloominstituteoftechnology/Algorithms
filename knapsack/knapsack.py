c78974570809d1281f4aab738eec2cdb603c8154
Sam Khaled <samkhaled@outlook.com>
2018-10-09 18:45:22 -0400

    knapsack completed

--- knapsack/knapsack.py
+++ knapsack/knapsack.py
@@ -5,8 +5,52 @@ from collections import namedtuple
 
 Item = namedtuple('Item', ['index', 'size', 'value'])
 
+
 def knapsack_solver(items, capacity):
-  pass
+    # dp solution with memoization, bottom up solution
+    cache = [[{} for _ in range(capacity+1)] for _ in range(len(items) + 1)]
+
+    for i in range(len(items)+1):
+        for s in range(capacity+1):
+            if i == 0 or s == 0:
+                cache[i][s] = {'Value': 0, 'Chosen': []}
+            elif items[i-1].size > s:
+                cache[i][s] = cache[i-1][s]
+            else:
+                test1 = cache[i-1][s]
+                test2 = {'Value': cache[i-1][s-items[i-1].size]['Value'] + items[i-1].value,
+                         'Chosen': cache[i-1][s-items[i-1].size]['Chosen'] + [items[i-1].index]}
+                if test1['Value'] > test2['Value']:
+                    cache[i][s] = test1
+                else:
+                    cache[i][s] = test2
+
+    return cache[len(items)][capacity]
+
+    # naive solution, top down, passes small tests, runtime on medium is too long so I stopped it
+    # def helper(n, c, current_list, value):
+    #     if n == 0 or c == 0:
+    #         return {'Value': value, 'Chosen': current_list}
+    #     elif items[n-1].size > c:
+    #         ret = helper(n-1, c, current_list, value)
+    #         return {'Value': ret['Value'], 'Chosen': ret['Chosen']}
+    #     else:
+    #         test1 = helper(n-1, c, current_list, value)
+    #         test2 = helper(n-1, c - items[n-1].size, current_list + [items[n-1].index], value + items[n-1].value)
+    #         if test1['Value'] > test2['Value']:
+    #             return {'Value': test1['Value'], 'Chosen': test1['Chosen']}
+    #         else:
+    #             return {'Value': test2['Value'], 'Chosen': test2['Chosen']}
+    #
+    # res = helper(len(items), capacity, [], 0)
+    # return {'Value': res['Value'], 'Chosen': sorted(res['Chosen'])}
+
+
+"""
+The value/weight ratio implementation is a greedy algorithm, which would work for some cases,
+but won't fulfill many edge cases. In this case, a dynamic programming approach with memoization
+would be superior to the greedy approach.
+"""
   
 
 if __name__ == '__main__':
