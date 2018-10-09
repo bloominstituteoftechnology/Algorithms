f60c5e2a456aa8204965f35596ae86d81575bae6
Rachel DiCesare <racheldicesare@missrachelthedeveloper.com>
2018-10-09 17:07:33 -0400

    corrected climbing_stairs

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -2,23 +2,30 @@
 
 import sys
 
+n = 50
+cache = {i: 0 for i in range(n + 1)}
+
 def climbing_stairs(n, cache=None):
 # base case
   if n < 0:
     return 0
-  if n == 0 or n ==1:
+  elif n == 0 or n ==1:
     return 1
   elif n ==2:
     return 2
   elif n == 3:
     return 4
-  # move towards one of our base cases
-  return climbing_stairs(n-1) + climbing_stairs(n-2) + climbing_stairs(n-3)
+  elif cache and cache[n]:
+     return cache[n]
+  else:
+    if not cache:
+      cache = {i: 0 for i in range(n + 1)}
+    cache[n] = climbing_stairs(n-1,cache) + climbing_stairs(n-2,cache) + climbing_stairs(n-3,cache)
+  return cache[n]
 
-
-# if __name__ == "__main__":
-#   if len(sys.argv) > 1:
-#     num_stairs = int(sys.argv[1])
-#     print("There are {ways} ways for a child to jump {n} stairs.".format(ways=climbing_stairs(num_stairs), n=num_stairs))
-#   else:
-#     print('Usage: climbing_stairs.py [num_stairs]')
+if __name__ == "__main__":
+  if len(sys.argv) > 1:
+    num_stairs = int(sys.argv[1])
+    print("There are {ways} ways for a child to jump {n} stairs.".format(ways=climbing_stairs(num_stairs), n=num_stairs))
+  else:
+    print('Usage: climbing_stairs.py [num_stairs]')
