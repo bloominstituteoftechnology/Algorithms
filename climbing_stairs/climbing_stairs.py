826735e72f24bc465fe6d25874c391ffaa3c6605
sca4cs <shannon.atkinson.ca@gmail.com>
2018-10-09 15:45:44 -0400

    climbing_stairs solution for large and small n

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -6,14 +6,20 @@ def climbing_stairs(n, cache=None):
   # base case
   if n < 0:
     return 0
-  if n == 0 or n ==1:
+  elif n == 0 or n ==1:
     return 1
   elif n ==2:
     return 2
   elif n == 3:
     return 4
   # move towards one of our base cases
-  return climbing_stairs(n-1) + climbing_stairs(n-2) + climbing_stairs(n-3)
+  elif cache and cache[n] > 0:
+    return cache[n]
+  else:
+    if not cache:
+      cache = {i: 0 for i in range(n+1)}
+    cache[n] = climbing_stairs(n-1, cache) + climbing_stairs(n-2, cache) + climbing_stairs(n-3, cache)
+    return cache[n]
 
   # get a bunch of return values of 1, 2, or 4 at the terminating point of each branch; all get added up
 
