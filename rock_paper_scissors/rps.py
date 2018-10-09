f1b3856cda0daaf031f71c2aabdcec90d0fc40e2
Kyle Meltzer <jimmy13@Kyles-MacBook-Pro.local>
2018-10-09 18:35:42 -0500

    recipes and rps

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,26 @@
 
 import sys
 
+
 def rock_paper_scissors(n):
-  pass 
+    plays = ["rock", "paper", "scissors"]
+    outcomes = []
+
+    def find_outcome(n, result=[]):
+        if n == 0:
+            outcomes.append(result)
+            return
+
+        for play in plays:
+            find_outcome(n-1, result + [play])
+
+    find_outcome(n, [])
+    return outcomes
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
