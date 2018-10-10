95c448be72c87aa7f543f051222271d77cff4c9d
Naaz Shaikh <naazhome@Naazs-MBP-3.hsd1.or.comcast.net>
2018-10-10 10:59:14 -0700

    climbing stairs solution

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -2,13 +2,30 @@
 
 import sys
 
+
 def climbing_stairs(n, cache=None):
-  pass 
+    if n < 0:
+        return 0
+    elif n == 0:
+        return 1
+    # check if answer is in the cache
+    elif cache and cache[n] > 0:
+        return cache[n]
+    else:
+        if not cache:
+            # cache = {i: 0 for i in range(n + 1)}
+            cache = [0 for i in range(n + 1)]
+
+        # populate the cache
+        cache[n] = climbing_stairs(
+            n - 1, cache) + climbing_stairs(n - 2, cache) + climbing_stairs(n - 3, cache)
+        return cache[n]
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_stairs = int(sys.argv[1])
-    print("There are {ways} ways for a child to jump {n} stairs.".format(ways=climbing_stairs(num_stairs), n=num_stairs))
-  else:
-    print('Usage: climbing_stairs.py [num_stairs]')
+    if len(sys.argv) > 1:
+        num_stairs = int(sys.argv[1])
+        print("There are {ways} ways for a child to jump {n} stairs.".format(
+            ways=climbing_stairs(num_stairs), n=num_stairs))
+    else:
+        print('Usage: climbing_stairs.py [num_stairs]')
