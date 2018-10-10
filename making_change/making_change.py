aa324096809340f2576fb83e2ca2b4ad631308ab
rverdi642 <rverdi642@gmail.com>
2018-10-09 22:16:26 -0500

    RIchard Verdier-  all done excepting knapsack

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -6,27 +6,15 @@ def making_change(amount, denominations):
   pass 
 # USD - $1, $5, $10, etc.
 currency = [1, 5, 10, 20, 50, 100]
+if amount == 0:
+    return 1
+if amount == 0:
+    Return 0
+if len(denominations) == 0 and amount > 0:
+    return 0
+else:
+    return making_change(amount - denominations[-1], denominations) + making_change(amount - denominations[:1])
 
-# the amount to change - $120
-amount = 120
-
-
-minimum_number_of_currency = [0]
-currency_composition = [[]]
-
-for i in xrange(1, amount + 1):
-    best = 10000
-    currenncyCombo = None
-
-    for j in currency:
-        if i - j > -1 and minimum_number_of_currency[i - j] + 1 < best:
-            best = minimum_number_of_currency[i - j] + 1
-            currencyCombo = currency_composition[i - j] + [j]
-
-    minimum_number_of_currency.append(best)
-    currency_composition.append(currencyCombo)
-
-    print '{0:3} {1} {2}'.format(i, best, currencyCombo)
 
 if __name__ == "__main__":
   # Test our your implementation from the command line
