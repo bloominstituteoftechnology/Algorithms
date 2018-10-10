4011a538d7d77b7d29a9caffd767879135991ad7
Sukhada Gholba <sukhadagholba@gmail.com>
2018-10-10 12:05:41 -0700

    completed problem

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,9 +1,27 @@
 #!/usr/bin/python
-
 import sys
+from itertools import product
+
 
 def rock_paper_scissors(n):
-  pass 
+    choices=['rock','paper','scissors']
+    outcomes=[]
+
+    def find_result(n, result=[]):
+        if n==0:
+            outcomes.append(result)
+            return
+
+        for choice in choices:
+            find_result(n-1, result+[choice])
+
+    find_result(n,[])
+    return outcomes
+
+
+
+
+
 
 
 if __name__ == "__main__":
