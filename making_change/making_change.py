2d52861d1e49edc1f438d5bc59773af18b6aaf46
Naaz Shaikh <naazhome@Naazs-MBP-3.hsd1.or.comcast.net>
2018-10-10 19:44:01 -0700

    making change

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -2,16 +2,39 @@
 
 import sys
 
+
+# def making_change(amount, denominations):
+#   # base cases
+#     if amount == 0:
+#         return 1
+#     if amount < 0:
+#         return 0
+#     if len(denominations) == 0 and amount > 0:
+#         return 0
+#     else:
+#         return making_change(amount - denominations[-1], denominations) + making_change(amount, denominations[:-1])
+
+
 def making_change(amount, denominations):
-  pass 
+    cache = [0 for i in range(amount + 1)]
+    cache[0] = 1
+
+# loop over the denominations list
+    for coin in denominations:
+        for higher_amount in range(coin, amount + 1):
+            remainder = higher_amount - coin
+            cache[higher_amount] += cache[remainder]
+
+    return cache[amount]
 
 
 if __name__ == "__main__":
-  # Test our your implementation from the command line
-  # with `python making_change.py [amount]` with different amounts
-  if len(sys.argv) > 1:
-    denominations = [1, 5, 10, 25, 50]
-    amount = int(sys.argv[1])
-    print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
-  else:
-    print("Usage: making_change.py [amount]")
+    # Test our your implementation from the command line
+    # with `python making_change.py [amount]` with different amounts
+    if len(sys.argv) > 1:
+        denominations = [1, 5, 10, 25, 50]
+        amount = int(sys.argv[1])
+        print("There are {ways} ways to make {amount} cents.".format(
+            ways=making_change(amount, denominations), amount=amount))
+    else:
+        print("Usage: making_change.py [amount]")
