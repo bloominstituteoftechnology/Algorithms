83b96c66a0f0e1c480e78568cee795348aae1678
Carey <careycbaldwin@gmail.com>
2018-10-11 22:10:33 -0400

    updates to climbing stairs

--- 3_climbing_stairs/climbing_stairs.py
+++ 3_climbing_stairs/climbing_stairs.py
@@ -2,18 +2,18 @@
 
 import sys
 
-def climbing_stairs(n):
+def climbing_stairs(n, cache = None):
   if n < 0:
     return 0
-  elif n == 0 or n == 1:
+  elif n == 0:
     return 1
-  return climbing_stairs(n-1) + climbing_stairs(n-2) + climbing_stairs(n-3)
-
-print(climbing_stairs(10))
-
-# def climbing_stairs(n, cache=None):
-# pass 
-
+  # check if the answer is in the cache
+  elif cache and cache[n] > 0:
+    return cache[n]
+  else:
+    if not cache:
+      cache = {i: 0 for i in range(n+1)}
+  return climbing_stairs(n-1, cache) + climbing_stairs(n-2, cache) + climbing_stairs(n-3, cache)
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
