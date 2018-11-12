4941ed9f11504d237c88596a2e4510c70b93d165
Andrew Liao <andrewoliao@gmail.com>
2018-11-12 10:20:42 -0800

    Implement stock_prices algorithm to work for non decreasing data sets

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,10 +2,59 @@
 
 import argparse
 
+# find_max_profit([1050, 270, 1540, 3800, 2])
+
+# To achieve O(N), I need to be able to do all calculations by traversing the list
+# once. Anything faster than O(N) is unlikely as to find maximum we need to have
+# inspected each element at least once
 def find_max_profit(prices):
-  pass
+	smallest = None
+	second_largest = None
+	largest = (prices[0], 0)
+
+	for index in range(1, len(prices)):
+		if largest[0] < prices[index]:
+			largest = (prices[index], index)
+		if index == 1:
+			smallest = (prices[index - 1], index)
+		elif smallest[0] > prices[index - 1]:
+			smallest = (prices[index - 1], index)
+
+	if not smallest or not largest:
+		return None
+	elif largest[1] > smallest[1]:
+		return largest[0] - smallest[0]
 
 
+
+
+
+'''
+	for index in reversed(range(0, len(prices))):
+			if not largest:
+				largest = prices[index]
+				smallest = prices[index - 1]
+			
+			if index > 1 and smallest > prices[index - 1]:
+				smallest = prices[index - 1]
+
+			if largest < prices[index]:
+				largest = prices[index] 
+				if index > 1:
+					smallest =  prices[index - 1] 
+				else:
+					smallest = prices[index]
+
+
+	if not (smallest or largest):
+		return None
+	else:
+		return largest - smallest
+'''
+
+
+
+'''
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
   parser = argparse.ArgumentParser(description='Find max profit from prices.')
@@ -13,3 +62,10 @@ if __name__ == '__main__':
   args = parser.parse_args()
 
   print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+'''
+
+prices = [100, 90, 80, 50, 20, 10]
+prices2 = [1050, 270, 1540, 3800, 2]
+prices3 = [10, 7, 5, 8, 11, 9]
+
+print(find_max_profit(prices))
