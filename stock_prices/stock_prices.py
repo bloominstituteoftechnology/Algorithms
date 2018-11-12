981285b69c1f3b11190fda8a5e8ef4a466665938
BrianARuff <thelonewolf198927@gmail.com>
2018-11-12 17:19:39 -0500

    updated stock prices algorithim

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,41 +1,21 @@
 # Stock Prices
 
-prices_arr = [1050, 270, 1540, 3800, 2]
+prices_arr = [1050, 270, 1540, 2000, 2, 3, 1]
 
 
 def find_max_profit(prices):
     # Initial variables
-    min_stock = prices[0]
-    min_index = prices[0]
-    max_stock = prices[0]
-    max_index = prices[0]
-
-    # Loop through array
-    for ind, num in enumerate(prices):
-        # Inner for loop to compare every value...
-        for ind2, num2 in enumerate(prices):
-            # Go through inner for loop
-            if ind2 < ind:
-                # Check if current number is less than minimum stock value and ensure that the index on the outer
-                # loop is greater than the current minimum index, I'm treating the index as a point in time.
-                if num < min_stock and ind < min_index:
-                    min_stock = num
-                    min_index = ind
-                # Do the same as above only for the max numbers
-                elif num > max_stock and ind < max_index:
-                    max_stock = num
-                    max_index = ind
-                # Compare inner for loop current num against minimum stock
-                elif num2 < min_stock:
-                    min_stock = num2
-                    min_index = ind
-                # Compare inner for loop current num against maximum stock
-                elif num2 > num:
-                    max_stock = num2
-                    max_index = ind2
-    return max_stock - min_stock
+    maximum_difference = prices[0]
+    minimum_price = prices[0]
+    for i in range(len(prices)):
+        current_price = prices[i]
+        if current_price < minimum_price:
+            minimum_price = current_price
+        elif (current_price - minimum_price) > maximum_difference:
+            maximum_difference = current_price - minimum_price
+    return maximum_difference
 
 
 print(find_max_profit(prices_arr))
 
-# I "think" this algorithm is O(N2) due to just one inner for loop
+# I "think" this algorithm is O(N) due to just one loop for everything.
