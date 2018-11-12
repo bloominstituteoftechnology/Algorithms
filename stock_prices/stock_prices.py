f5a4db48a6a7c9cc7a8ed097b90203d6058ced82
Andrew Liao <andrewoliao@gmail.com>
2018-11-12 10:39:02 -0800

    Fix issue where [10, 7, 5, 8, 11, 9, 4] returns 7 when it should return 6. Resolve by traversing the list backwards.

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -8,22 +8,28 @@ import argparse
 # once. Anything faster than O(N) is unlikely as to find maximum we need to have
 # inspected each element at least once
 def find_max_profit(prices):
-	smallest = None
+	
+	last_index = len(prices) - 1
+	
+	largest = (prices[last_index], last_index)
+	smallest = (prices[last_index - 1], last_index - 1)
 	second_largest = None
-	largest = (prices[0], 0)
+	
+	non_increasing = False
 
-	for index in range(1, len(prices)):
-		if largest[0] < prices[index]:
+	for index in reversed(range(0, len(prices))):
+		if largest[0] <= prices[index]:
 			largest = (prices[index], index)
-		if index == 1:
-			smallest = (prices[index - 1], index)
-		elif smallest[0] > prices[index - 1]:
-			smallest = (prices[index - 1], index)
+			if index == 0:
+				non_increasing == True
+			else:
+				smallest = (prices[index - 1], index - 1)
+				print(smallest)
+		if index > 1 and smallest[0] > prices[index - 1]:
+			smallest = (prices[index - 1], index-1)
+			print(smallest)
 
-	if not smallest or not largest:
-		return None
-	elif largest[1] > smallest[1]:
-		return largest[0] - smallest[0]
+	return largest[0] - smallest[0]
 
 
 
@@ -68,4 +74,4 @@ prices = [100, 90, 80, 50, 20, 10]
 prices2 = [1050, 270, 1540, 3800, 2]
 prices3 = [10, 7, 5, 8, 11, 9]
 
-print(find_max_profit(prices))
+print(find_max_profit(prices2))
