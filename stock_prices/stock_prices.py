b6c883ff8c9043d2d0cb9c58f8cc0309fe65f29e
Luisan <luisangelsc@gmail.com>
2018-11-12 12:34:25 -0600

    stock_pries.py iterative

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,8 +3,17 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  max = prices[1]-prices[0]
+  # print ('max:', max)
+  for index_buy in range(len(prices)):
+    # print ('price[index: ]', prices[index_buy:])
+    for sold in prices[index_buy+1:]:
+      if sold-prices[index_buy] > max:
+        max = sold-prices[index_buy]
+  return max
 
+# print ( find_max_profit([10, 7, 5, 8, 11, 9]) )
+# print (find_max_profit([100, 90, 80, 50, 20, 10]))
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
