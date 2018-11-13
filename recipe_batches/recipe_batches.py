631166ec64044e4461a1c904b3f9d0b0e5d9faf0
BrianARuff <thelonewolf198927@gmail.com>
2018-11-13 15:11:50 -0500

    recipe first run

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -1,14 +1,31 @@
 #!/usr/bin/python
 
-import math
+recipe_requirements = {'milk': 100, 'butter': 50, 'cheese': 10}
+ingredient_requirements = {'milk': 298, 'butter': 52, 'cheese': 10}
+
+recipe_requirements2 = {'milk': 100, 'butter': 50, 'cheese': 10}
+ingredient_requirements2 = {'milk': 398, 'butter': 102, 'cheese': 20}
+
+recipe_requirements3 = {'milk': 100, 'butter': 50, 'cheese': 10, 'eggs': 3}
+ingredient_requirements3 = {'milk': 98, 'butter': 102, 'cheese': 20}
+
 
 def recipe_batches(recipe, ingredients):
-  pass 
+    enough_materials = []
+    for item in recipe:
+        if item not in recipe or item not in ingredients:
+            return 0
+        if ingredients[item] / recipe[item] > 0:
+            enough_materials.append((ingredients[item] / recipe[item]))
+        else:
+            enough_materials.append(0)
+
+    if min(enough_materials) >= 1:
+        return min(enough_materials)
+    else:
+        return 0
 
 
-if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+print(recipe_batches(recipe_requirements, ingredient_requirements))
+print(recipe_batches(recipe_requirements2, ingredient_requirements2))
+print(recipe_batches(recipe_requirements3, ingredient_requirements3))
