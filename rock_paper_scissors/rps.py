e77dc96916a3c6a7e62852d17a66a088268b075b
decagondev <decagondevelopment@gmail.com>
2018-11-13 17:14:50 +0000

    (MVP) : rethink needed for the rock paper scisors problem as my initial thoughts on the built in were incorrect. I will try to make my own implementation of it being what i thought it should have been

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,12 +2,12 @@
 
 import sys
 
-import itertools
+from itertools import product
 
 # in the python ittertools we have some interesting builtins product() : https://docs.python.org/3/library/itertools.html
-def rock_paper_scissors(n):
+#def rock_paper_scissors(n):
   #creating a list of conditions to test and permute
-  conditions = ["r", "p", "s"]
+ # conditions = ["r", "p", "s"] # is it the input set that is the problem?
 
   # set the iteration count to the product of the conditions repeated (n) times
   # broken or rather solution that needed work iteration_count = itertools.product(conditions, repeat=n)
@@ -15,6 +15,10 @@ def rock_paper_scissors(n):
   # return iteration_count # FIXME: look in to lists either a cast / instantiation or list comprehension
 
   # FIXME: another faliure trying to use some offset version of list inside but i think a quick way will be just to instantiate it as a list return [itertools.product(conditions, repeat=n)]
+ # return list(itertools.product(conditions, repeat=n))
+
+ # the return value of this seems to feel like a tuple for 0 FIXME: maybe write my own product function???
+
 
 
 if __name__ == "__main__":
