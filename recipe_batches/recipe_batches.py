3e422a6e3af2e2d555f71f063d01a23c4869031a
Cameron Ray <cameronray@Camerons-MacBook-Pro.local>
2018-11-13 15:21:26 -0800

    recipe_bathes working solution complete

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,17 +2,31 @@
 
 import math
 
-# I'm looking for the integer quotient k of 2 integers a and b such that a > bk
+# I'm looking for the integer quotient k of 2 integers a and b such that a = bk + r
 
 def recipe_batches(recipe, ingredients):
-  if recipe.keys != ingredients.keys:
-    print(0)
-  
+  if recipe.keys() != ingredients.keys():
+    print('Missing ingredient')
+    return 0
+  else:
+    quotients = []
+    for i in ingredients:
+      quotient = ingredients[i] // recipe[i]
+      quotients.append(quotient)
+    print('quotients: ', quotients)
+    print('max batches: ', min(quotients))
+    return min(quotients)
+      
 
 
 recipe_batches(
   { 'milk': 100, 'flour': 4, 'sugar': 10, 'butter': 5 }, 
   { 'milk': 1288, 'flour': 9, 'sugar': 95 }
+)
+
+recipe_batches(
+  { 'milk': 100, 'butter': 50, 'cheese': 10 }, 
+  { 'milk': 198, 'butter': 52, 'cheese': 10 }
 )
 
 # if __name__ == '__main__':
