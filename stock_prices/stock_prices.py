f3c8f2b2b744d4e55ce5ea7ae26d526182867949
Joseph Chretien-Golden <joechre@gmail.com>
2018-11-13 17:14:38 -0600

    stocks

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -13,3 +13,5 @@ if __name__ == '__main__':
   args = parser.parse_args()
 
   print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+
+  if argparse == maxprofit 
