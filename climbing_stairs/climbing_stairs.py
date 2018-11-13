81dc56eac766e31a3f348a124cce82c0485c0d93
Andrew Liao <andrewoliao@gmail.com>
2018-11-13 15:40:42 -0800

    Change test_climbing_stairs.py to fit argument requirements of function

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -15,12 +15,9 @@ def climbing_stairs(n, cache = {}):
   return cache[n]
 
 
-# if __name__ == "__main__":
-#   if len(sys.argv) > 1:
-#     num_stairs = int(sys.argv[1])
-#     print("There are {ways} ways for a child to jump {n} stairs.".format(ways=climbing_stairs(num_stairs), n=num_stairs))
-#   else:
-#     print('Usage: climbing_stairs.py [num_stairs]')
-print(climbing_stairs(50, {}) == 10562230626642)
-print(climbing_stairs(100, {}) == 180396380815100901214157639)
-print(climbing_stairs(500, {}) == 1306186569702186634983475450062372018715120191391192207156664343051610913971927959744519676992404852130396504615663042713312314219527)
+if __name__ == "__main__":
+  if len(sys.argv) > 1:
+    num_stairs = int(sys.argv[1])
+    print("There are {ways} ways for a child to jump {n} stairs.".format(ways=climbing_stairs(num_stairs), n=num_stairs))
+  else:
+    print('Usage: climbing_stairs.py [num_stairs]')
