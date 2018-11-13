3968a386b4ccd829587722fd09ca33337798c715
decagondev <decagondevelopment@gmail.com>
2018-11-13 16:58:02 +0000

    (MVP) : first itteration with faliurs due to casting / list issues : FIXME: look in to lists either a cast / instantiation or list comprehension

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,8 +2,17 @@
 
 import sys
 
+import itertools
+
+# in the python ittertools we have some interesting builtins product() : https://docs.python.org/3/library/itertools.html
 def rock_paper_scissors(n):
-  pass 
+  #creating a list of conditions to test and permute
+  conditions = ["r", "p", "s"]
+
+  # set the iteration count to the product of the conditions repeated (n) times
+  iteration_count = itertools.product(conditions, repeat=n)
+
+  return iteration_count # FIXME: look in to lists either a cast / instantiation or list comprehension
 
 
 if __name__ == "__main__":
