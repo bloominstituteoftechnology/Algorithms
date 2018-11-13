3789f8a505aed2d3d19b9a8ec69002b8d4c3b0f1
Luisan <luisangelsc@gmail.com>
2018-11-12 18:55:16 -0600

    rps.py - no recursion

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,7 +3,27 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  if n:
+
+    moves = [['rock'], ['paper'], ['scissors']]
+    lista = moves.copy()
+    while n > 1:
+      for move in moves:
+          move_r = move.copy()
+          move_p = move.copy()
+          move_s = move.copy()
+          move_r.append('rock')
+          move_p.append('paper')
+          move_s.append('scissors')
+          lista.append(move_r)
+          lista.append(move_p)
+          lista.append(move_s)
+          lista.remove(move)
+      moves = lista.copy()
+      n -= 1
+    return  lista
+  else:
+      return [[]]
 
 
 if __name__ == "__main__":
