21920acec93e83d254c22b774d2b454688cc1273
decagondev <decagondevelopment@gmail.com>
2018-11-13 15:59:15 +0000

    (MVP) : initial itteration of recipe batches using a min() : failing due to some float issue

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python
 
 import math
+# synopsis :
+# Your function should output the maximum number of whole batches that can be made for the supplied recipe using the ingredients available to you, 
+# as indicated by the second dictionary.
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  # had a read over this for general dictionary stuff : http://www.pythonlearn.com/html-009/book010.html
+  # also to address a minimalistic approach 
+  # this one feels like it should be fairly straight forward. looking up a few builtins
+  
+  # initial thoughts were to use a min on the ingredients against an element vs 0 and use a single for loop to make sure i am only looping on a low factor
+  # but it seems that using a division i may need to fix up the return maybe floor or int()
+  return min(ingredients.get(elem, 0) / num for elem, num in recipe.items()) # FIXME: possibly cast to int?
 
 
 if __name__ == '__main__':
