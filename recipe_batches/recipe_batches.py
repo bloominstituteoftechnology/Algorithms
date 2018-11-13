8f4875cb9d543c4bf8d74f0e9db6229ad0887166
Timothy Hoang <timh1203@gmail.com>
2018-11-13 17:04:07 -0500

    Add naive for recipe_batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,44 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    make = []
+    for i in list(recipe.keys()):
+        if i in ingredients and recipe[i] < ingredients[i]:
+            make.append(math.floor(ingredients[i] / recipe[i]))
+        elif i not in ingredients:
+            return 0
+    return min(make)
 
 
-if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+# print(
+#     recipe_batches(
+#         {"milk": 100, "flour": 4, "sugar": 10, "butter": 5},
+#         {"milk": 1288, "flour": 9, "sugar": 95},
+#     )
+# )  # 0
+# print(
+#     recipe_batches(
+#         {"milk": 100, "butter": 50, "cheese": 10},
+#         {"milk": 198, "butter": 52, "cheese": 10},
+#     )
+# )  # 1
+# print(
+#     recipe_batches(
+#         {"milk": 2, "sugar": 40, "butter": 20}, {"milk": 5, "sugar": 120, "butter": 500}
+#     )
+# )  # 2
+# print(recipe_batches({"milk": 2}, {"milk": 200}))  # 100
+
+
+if __name__ == "__main__":
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {"milk": 100, "butter": 50, "flour": 5}
+    ingredients = {"milk": 132, "butter": 48, "flour": 51}
+    print(
+        "{batches} batches can be made from the available ingredients: {ingredients}.".format(
+            batches=recipe_batches(recipe, ingredients), ingredients=ingredients
+        )
+    )
