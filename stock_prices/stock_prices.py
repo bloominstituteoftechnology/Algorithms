091f33342486b2e9817f502d511e64dfaebaa6e1
decagondev <decagondevelopment@gmail.com>
2018-11-13 15:41:34 +0000

    (MVP) : first pass of the stock proces problem passes tests in 0.014s will revisit the problem and try for a second pass after looking at other problems

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,7 +2,22 @@
 
 import argparse
 
+# research : http://book.pythontips.com/en/latest/enumerate.html
+# had to use enumerate to fix an initial error
 def find_max_profit(prices):
+  largest_profit = 0
+  # for index, initial_number in prices: (fixed)
+  # 2 for loops nested : not very performant but just hashing out the idea of solving the problem 
+  for index, initial_number in enumerate(prices):
+    for secondary_number in prices[ index + 1: ]:
+      # set the current profit variable to the secondary - initial
+      profit = secondary_number - initial_number
+      # test for profit being greater than largest profit
+      if largest_profit == 0 or profit > largest_profit:
+        # set the largest profit to the profit of this itteration
+        largest_profit = profit
+  # return the largest computed profit
+  return largest_profit
   pass
 
 
