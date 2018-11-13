e8ff3b957e1edcf4fb602a801a1f6239ed7b342e
Andrew Liao <andrewoliao@gmail.com>
2018-11-12 16:32:53 -0800

    Fix issue with non increasing data set

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -13,65 +13,37 @@ def find_max_profit(prices):
 	
 	largest = (prices[last_index], last_index)
 	smallest = (prices[last_index - 1], last_index - 1)
-	second_largest = None
+	second_largest = (prices[last_index], last_index)
 	
 	non_increasing = False
 
 	for index in reversed(range(0, len(prices))):
 		if largest[0] <= prices[index]:
 			largest = (prices[index], index)
-			if index == 0:
-				non_increasing == True
-			else:
+			if index > 0:
+				# Set's smallest number to number to th
 				smallest = (prices[index - 1], index - 1)
 				print(smallest)
+		# set's second largest in case of non increasing list
+		if index < last_index and second_largest[0] < prices[index + 1] and prices[index + 1] < largest[0]:
+				second_largest = (prices[index + 1], index + 1)
+
+# Set's the smallest number if there is a smaller number at index
 		if index > 1 and smallest[0] > prices[index - 1]:
 			smallest = (prices[index - 1], index-1)
 			print(smallest)
 
-	return largest[0] - smallest[0]
-
-
-
-
-
-'''
-	for index in reversed(range(0, len(prices))):
-			if not largest:
-				largest = prices[index]
-				smallest = prices[index - 1]
-			
-			if index > 1 and smallest > prices[index - 1]:
-				smallest = prices[index - 1]
-
-			if largest < prices[index]:
-				largest = prices[index] 
-				if index > 1:
-					smallest =  prices[index - 1] 
-				else:
-					smallest = prices[index]
-
-
-	if not (smallest or largest):
-		return None
+	if largest[1] == 0:
+		return second_largest[0] - largest[0]
 	else:
-		return largest - smallest
-'''
+		return largest[0] - smallest[0]
 
 
 
-'''
-if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
-
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
-'''
-
-prices = [100, 90, 80, 50, 20, 10]
+prices = [100, 100, 90, 80, 50, 20, 10]
 prices2 = [1050, 270, 1540, 3800, 2]
 prices3 = [10, 7, 5, 8, 11, 9]
 
+print(find_max_profit(prices))
 print(find_max_profit(prices2))
+print(find_max_profit(prices3))
