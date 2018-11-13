4140611608814d1ccc1ff1dc8e7f1e520a4664d9
BrianARuff <thelonewolf198927@gmail.com>
2018-11-13 17:33:07 -0500

    climbing stairs, bottom up method o(n)

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -5,13 +5,22 @@ import sys
 
 def climbing_stairs(n, cache=None):
     # base case
-    if n <= 0:
-        return 0
+    if n < 2 and n > 0:
+        return 1
 
     # n is the number of stairs
     # can use a combo of 3, 2, or 1 to ascend steps
     # what are all the possible combos of ascending the steps
     # Use the cache to store the combinations
+    a = 0
+    b = 0
+    c = 1
+    for _ in range(n):
+        a, b, c = b, c, a + b + c
+    return c
+
+
+print(climbing_stairs(4))
 
 # if __name__ == "__main__":
 #     if len(sys.argv) > 1:
