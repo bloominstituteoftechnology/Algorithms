95cbfe48ef15d21794295b4b9fdd7d460d058e26
decagondev <decagondevelopment@gmail.com>
2018-11-13 17:49:55 +0000

    (MVP) : done first pass for the making change using a slow algorithm of nested for loops might try to optimize this if i get time

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -1,10 +1,28 @@
 #!/usr/bin/python
 
 import sys
+# my main issue was had to look up US denominations of money : https://en.wikipedia.org/wiki/Coins_of_the_United_States_dollar#Coins_in_circulation
+# going to use a chace to help with lookup hopefully will be a good enough for the test cases it is quite slow at 0.033s for 2 tests
 
 def making_change(amount, denominations):
-  pass 
+    # create a cache for this set the cache = a list of [0] + the ammount + 1 in case of a zero input
+    cache = [0] * (amount + 1)
 
+    # set the index of zero to 1
+    cache[0] = 1 
+
+    #using a nested for loop and adding to the cache
+    # loop over the denom in denominations
+    for denom in denominations:
+        # inner loop - upper in range of denom to ammount + 1
+        for upper in range(denom, amount+1):
+            # set the inner index to the upper - the denom at current outer itteration
+            inner_index = upper - denom
+            # set increment the cache of upper by the cache of inner_index
+            cache[upper] += cache[inner_index]
+
+    # at the end we can return the cache at index of the ammount
+    return cache[amount]
 
 if __name__ == "__main__":
   # Test our your implementation from the command line
