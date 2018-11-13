4bd12cf712e8108df910268ff43d16f778c19665
verydecent <wnjhwng@gmail.com>
2018-11-13 15:37:52 -0800

    Naive way to complete stairs

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,25 +3,44 @@
 import argparse
 
 def find_max_profit(prices):
+  max_profit = 0
+  for i in range(len(prices) - 1):
+    for j in range(i + 1, len(prices)):
+      if prices[j] > max_profit:
+        max_profit = prices[j] - prices[i]
+  return max_profit
 
-  lowest = prices[0]
-  highest = prices[0]
+def find_max_profit2(prices):
+  min_price = prices[0]
+  max_profit = 0.0
 
-  # highest = None
-  for value in prices[1:]:
-    if value < lowest:
-      lowest = value
-  
-  for value in prices[1:]:
-    if value > highest:
-      highest = value
+  for price in prices:
+    min_price = min(min_price, price)
+    compare_price = price - min_price
 
-  profit = highest - lowest
-  return profit
-  # return profit
+    max_profit = max(max_profit, compare_price)
+  return max_profit
+# def find_max_profit(prices):
 
-print(find_max_profit([5, 2, 1]))
-print(find_max_profit([1050, 270, 1540, 3800, 2]))
+#   lowest = prices[0]
+#   highest = prices[0]
+
+#   # highest = None
+#   for value in prices[1:]:
+#     if value < lowest:
+#       lowest = value
+
+#   for value in prices[1:]:
+#     if value > highest:
+#       highest = value
+
+#   profit = highest - lowest
+#   return profit
+#   # return profit
+
+# print(find_max_profit([5, 2, 1]))
+# print(find_max_profit([1050, 270, 1540, 3800, 2]))
+print(find_max_profit2([1050, 270, 1540, 3800, 2]))
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
