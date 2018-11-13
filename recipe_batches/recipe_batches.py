3ebfae3ea1380bdd37d4d0431faec8a4d0b245fb
iAmAdamReid <adam@adamreid.me>
2018-11-13 13:20:28 -0800

    Naive recursion for recipe_batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,8 +2,37 @@
 
 import math
 
-def recipe_batches(recipe, ingredients):
-  pass 
+def recipe_batches(recipe, ingredients, count=0):
+  # we need to globally track how many batches we've made, and pass recursively
+  # TODO: find how to do this w/o global variables
+  global batches
+  batches = count
+  can_cook = []
+
+  # if we do not have any necessary ingredient, return 0
+  if len(recipe) > len(ingredients):
+    return 0
+
+  # we need to see if we are able to make a batch
+  for item in ingredients:
+    if ingredients[item] >= recipe[item]:
+      can_cook.append(True)
+    else:
+      can_cook.append(False)
+
+  # if we have all necessary ingredients, add a batch to count and subtract from ingredients
+  if all(can_cook):
+    batches += 1
+    remainder = {key: ingredients[key] - recipe.get(key, 0) for key in ingredients.keys()}
+    # recursion call with the remaining ingredients
+    recipe_batches(recipe, remainder, batches)
+    
+  return batches
+
+
+  ### NOTES
+  # this subtracts the recipe from the ingredients and gives the remainder
+  # remainder = {key: ingredients[key] - recipe.get(key, 0) for key in ingredients.keys()}
 
 
 if __name__ == '__main__':
