c8b0c81e491443d3418a4243766fcfba2e57b0ab
Brandon White <bwhite06@stac.edu>
2018-11-13 10:07:27 -0500

    working on algorithms

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -1,10 +1,31 @@
 #!/usr/bin/python
 
 import sys
-
+from itertools import permutations 
 def climbing_stairs(n, cache=None):
-  pass 
 
+    stack=[]
+    
+
+    for i in range(n):
+      
+         stack.append(n)
+         
+    perm = permutations(stack)     
+      
+    for i in perm: 
+         result = i
+         
+
+         if n>=0: 
+            if n==0:
+               return 1
+            if n==1:
+               return int(str(result).strip('(,)'))
+            if n>=2:
+               return int(str(len(result)).strip('(,)'))
+
+  
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,9 +1,33 @@
 #!/usr/bin/python
 
 import sys
-
+import random
+from collections import deque
 def rock_paper_scissors(n):
-  pass 
+  stack=[[]]
+  list_of_ways= [['rock'],['paper'],['scissors']]
+
+ 
+
+  
+  if n==0:
+     return stack
+
+  if n==1:
+     
+     return list_of_ways
+      
+  if n>1:
+     y = n*2
+     stack.remove(stack[0])
+     for i in range(y):
+       player_one = random.choice(list_of_ways)
+       stack.append(player_one)
+       
+       player_two = random.choice(list_of_ways)
+       stack.append(player_two)
+       
+  return stack
 
 
 if __name__ == "__main__":
