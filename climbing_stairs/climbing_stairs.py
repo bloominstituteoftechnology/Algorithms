d3a964950a13eb4d5bf0d9937b9307fee84a33f3
Akshay Gadkari <akshay.gadkari@gmail.com>
2018-11-13 14:39:45 -0800

    modifying climbing stairs

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -2,13 +2,30 @@
 
 import sys
 
-def climbing_stairs(n, cache=None):
-  pass 
+#find results such that sum(results) = n
+# find combos = the different combinations of results
 
+def climbing_stairs(n):
+  # if n == 0 or n < 0:
+  #   print(0)
+  # elif n == 1:
+  #   print(1)
+  # elif n == 2:
+  #   print(2)
+  # else:
+  a = climbing_stairs(n - 1) + climbing_stairs(n - 2) + climbing_stairs(n - 3)
+  print(a)
 
-if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_stairs = int(sys.argv[1])
-    print("There are {ways} ways for a child to jump {n} stairs.".format(ways=climbing_stairs(num_stairs), n=num_stairs))
-  else:
-    print('Usage: climbing_stairs.py [num_stairs]')
+climbing_stairs(4)
+
+# def climbing_stairs(n, cache=None):
+#   pass
+
+# def()
+
+# if __name__ == "__main__":
+#   if len(sys.argv) > 1:
+#     num_stairs = int(sys.argv[1])
+#     print("There are {ways} ways for a child to jump {n} stairs.".format(ways=climbing_stairs(num_stairs), n=num_stairs))
+#   else:
+#     print('Usage: climbing_stairs.py [num_stairs]')
