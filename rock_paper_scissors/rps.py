8a4971064443b5180eff89c0ae55c8892719b884
Michael Trevino <preince560m@gmail.com>
2018-11-13 18:15:54 -0600

    help me :)

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,7 +3,37 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+ 
+  # output = [[]]
+  
+  # for i in range(0, n):
+  #   for j in range(0, len(output)):
+      
+  #     j = j+2*j
+      
+  #     output[j].append('rock')
+      
+  #     temp_list = output[j].copy()
+  #     del temp_list[len(temp_list) - 1]
+  #     temp_list.append('paper')
+  #     output.insert(j+1,temp_list)
+  #     temp_list = output[j].copy()
+  #     del temp_list[len(temp_list) - 1]
+  #     temp_list.append('scissors')
+      # output.insert(j+2,temp_list)
+      
+  # for i in range(0, n):
+  #   for j in range(0, len(output)):
+  #     temp_list = output[j].copy()
+  #     temp_list.append('paper')
+  #     output.append(temp_list)     
+  #     temp_list = output[j].copy()
+  #     temp_list.append('scissors')
+  #     output.append(temp_list)
+  #     output[j].append('rock')
+
+  return output
+
 
 
 if __name__ == "__main__":
