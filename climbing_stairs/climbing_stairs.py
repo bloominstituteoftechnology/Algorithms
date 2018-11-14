b663a9ee3f8fc565f7db55d44c85c1e9cbfc1506
Cameron Ray <cameronray@Camerons-MacBook-Pro.local>
2018-11-14 14:17:27 -0800

    I got a working solution for climbing_stairs last night. pushing it now.

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -3,12 +3,21 @@
 import sys
 
 def climbing_stairs(n, cache=None):
-  pass 
-
-
-if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_stairs = int(sys.argv[1])
-    print("There are {ways} ways for a child to jump {n} stairs.".format(ways=climbing_stairs(num_stairs), n=num_stairs))
+  if (n < 0):
+    print('You climbed past the top stair.')
+  elif (n == 0 or n == 1):
+    return   1
+  elif (n == 2):
+    return 2
   else:
-    print('Usage: climbing_stairs.py [num_stairs]')
+    return climbing_stairs(n - 3) + climbing_stairs(n - 2) + climbing_stairs(n - 1)
+
+
+print(climbing_stairs(4))
+
+# if __name__ == "__main__":
+#   if len(sys.argv) > 1:
+#     num_stairs = int(sys.argv[1])
+#     print("There are {ways} ways for a child to jump {n} stairs.".format(ways=climbing_stairs(num_stairs), n=num_stairs))
+#   else:
+#     print('Usage: climbing_stairs.py [num_stairs]')
