e8c5f652ac8a0c16f9207a83d19df94333008754
Lucas Beemer <lucasbeemer@gmail.com>
2018-11-14 10:01:52 -0800

    making change

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -3,7 +3,31 @@
 import sys
 
 def making_change(amount, denominations):
-  pass 
+  if amount == 0:
+    return 1
+
+  if amount < 0:
+    return -1
+
+  if len(denominations) == 0:
+    return 0
+
+  return making_change(amount - denominations[-1], denominations) + making_change(amount, denominations[:-1])
+
+
+def making_change2(amount, denominations):
+  ways = [0] * (amount + 1)
+
+  ways[0] = 1
+
+  for coin in denominations:
+    for higher_amount in range(coin, amount + 1):
+      remainder = higher_amount - coin
+      ways[higher_amount] += ways[remainder]
+      print(f'{coin} {ways}')
+
+  return ways[amount]
+
 
 
 if __name__ == "__main__":
@@ -12,6 +36,6 @@ if __name__ == "__main__":
   if len(sys.argv) > 1:
     denominations = [1, 5, 10, 25, 50]
     amount = int(sys.argv[1])
-    print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
+    print("There are {ways} ways to make {amount} cents.".format(ways=making_change2(amount, denominations), amount=amount))
   else:
     print("Usage: making_change.py [amount]")
