151fcbcdb40db3852477963d1ec4ede2087d2543
Akshay Gadkari <akshay.gadkari@gmail.com>
2018-11-13 16:39:56 -0800

    did part 3

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -2,30 +2,27 @@
 
 import sys
 
-#find results such that sum(results) = n
+# find results such that sum(results) = n
 # find combos = the different combinations of results
 
-def climbing_stairs(n):
-  # if n == 0 or n < 0:
-  #   print(0)
-  # elif n == 1:
-  #   print(1)
-  # elif n == 2:
-  #   print(2)
-  # else:
-  a = climbing_stairs(n - 1) + climbing_stairs(n - 2) + climbing_stairs(n - 3)
-  print(a)
+def climbing_stairs(n, cache=None):
+  if n == 0 or n < 0:
+    return 1
+  elif n == 1:
+    return 1
+  elif n == 2:
+    return 2
+  elif cache and cache[n] > 0:
+    return cache[n]
+  else:
+      if not cache:
+        cache = {i: 0 for i in range(n + 1)}
+      cache[n] = climbing_stairs(n-1, cache) + climbing_stairs(n-2, cache) + climbing_stairs(n-3, cache)
+      return cache[n]
 
-climbing_stairs(4)
-
-# def climbing_stairs(n, cache=None):
-#   pass
-
-# def()
-
-# if __name__ == "__main__":
-#   if len(sys.argv) > 1:
-#     num_stairs = int(sys.argv[1])
-#     print("There are {ways} ways for a child to jump {n} stairs.".format(ways=climbing_stairs(num_stairs), n=num_stairs))
-#   else:
-#     print('Usage: climbing_stairs.py [num_stairs]')
+if __name__ == "__main__":
+  if len(sys.argv) > 1:
+    num_stairs = int(sys.argv[1])
+    print("There are {ways} ways for a child to jump {n} stairs.".format(ways=climbing_stairs(num_stairs), n=num_stairs))
+  else:
+    print('Usage: climbing_stairs.py [num_stairs]')
