1a067cd0d4170de91ab3588d51f4c4e4e4022daf
Cameron Ray <cameronray@Camerons-MacBook-Pro.local>
2018-11-21 16:53:52 -0800

    climbing stairs is working for all n when I run climbing_stairs.py. When I run test files, small n tests pass but large n tests fail because of the way the test cache arg is defined

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -5,13 +5,13 @@ import sys
 def climbing_stairs(n, cache=None):
   if (n < 0):
     # print('You climbed past the top stair.')
-    return None
+    return 0
   elif (n == 0 or n == 1):
     # print('Only 1 way up')
     return   1
-  elif (n == 2):
-    # print('2 ways up')
-    return 2
+  # elif (n == 2):
+  #   # print('2 ways up')
+  #   return 2
 
   # Option 1: thread the state through each open recursive call
   # else:
@@ -19,14 +19,13 @@ def climbing_stairs(n, cache=None):
   # This section used for Option 2 below
 
   # Option 2: use the cache
-  if not cache:                        # if cache is empty or nonexistent
+  if cache == None:                        # if cache is empty or nonexistent
     cache = {}                         # make cash an empty dictionary object
 
   if n not in cache:                   # if cache exists but does not contains n
     cache[n] = climbing_stairs(n - 1, cache) + climbing_stairs(n - 2, cache) + climbing_stairs(n - 3, cache) # return cache value at index n
     
-  else:
-    return cache[n]
+  return cache[n]
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
--- climbing_stairs/test_climbing_stairs.py
+++ climbing_stairs/test_climbing_stairs.py
@@ -4,24 +4,29 @@ from climbing_stairs import climbing_stairs
 class Test(unittest.TestCase):
   print('--- NEW TEST SUITE ---')
   def test_climbing_stairs_small_n(self):
-    print('--- TEST 1: ---')
+    print('--- TESTS FOR SMALL n ---\n')
+    print('--- TEST n = 0 ---\n')
     self.assertEqual(climbing_stairs(0), 1)
-    print('--- TEST 2: ---')
+    print('--- TEST n = 1 ---\n')
     self.assertEqual(climbing_stairs(1), 1)
-    print('--- TEST 3: ---')
+    print('--- TEST n = 2 ---\n')
     self.assertEqual(climbing_stairs(2), 2)
-    print('--- TEST 4: ---')
+    print('--- TEST n = 5 ---\n')
     self.assertEqual(climbing_stairs(5), 13)
-    print('--- TEST 5: ---')
+    print('--- TEST n = 10 ---\n')
     self.assertEqual(climbing_stairs(10), 274)
 
   def test_climbing_stairs_large_n(self):
+    print('--- TESTS FOR LARGE n ---')
+    print('--- TEST n = 50 ---\n')
     self.assertEqual(climbing_stairs(50, [0 for i in range(51)]), 10562230626642)
+    print('--- TEST n = 100 ---\n')
     self.assertEqual(climbing_stairs(100, [0 for i in range(101)]), 180396380815100901214157639)
+    print('--- TEST n = 500 ---\n')
     self.assertEqual(climbing_stairs(500, [0 for i in range(501)]), 1306186569702186634983475450062372018715120191391192207156664343051610913971927959744519676992404852130396504615663042713312314219527)
 
 
-if __name__ == '__main__':
+if __name__ == '__main__': 
   unittest.main()
 
   
