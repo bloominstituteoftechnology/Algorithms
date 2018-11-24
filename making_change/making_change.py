e158eeaa15f6d9d8048b36381e7e568db52a95f9
Cameron Ray <cameronray@Camerons-MacBook-Pro.local>
2018-11-23 17:29:21 -0800

    making change completed. Working on rock paper scissors

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -3,6 +3,7 @@
 import sys
 
 def making_change(amount, denominations):
+  # --- method 1 start ---
   # if amount == 0:
   #   return 1 
   
@@ -13,16 +14,17 @@ def making_change(amount, denominations):
   #   return 0
   
   # return making_change(amount - denominations[-1], denominations) + making_change(amount, denominations[-1])
+  # --- method 1 end ---
   
   ways = [0] * (amount + 1)
   
-  ways[0] = 1
+  ways[0] = 1  # ways will be an array, each element is number of ways to get to 0 cents, 1 cent, 2 cents, 3 cents, ... , amount of cents
 
-  for coin in denominations:
-    for higher_amount in range(coin, amount + 1):
+  for coin in denominations:                       # Loop through each coin denomination 1, 5, 10, 25, 50
+    for higher_amount in range(coin, amount + 1):  # Loop through each integer from current coin (denomination) to (amount + 1)
       remainder = higher_amount - coin
-      ways[higher_amount] += ways[remainder]
-      # print(f'{coin} {ways}')
+      ways[higher_amount] += ways[remainder]       # shorthand for ways[higher_amount] = ways[higher_amount] + ways[remainder]
+      # print(f'{coin} {ways}')                      # number of ways to get to 0 cents, 1 cent, 2 cents, 3 cents, ... , amount of cents
     
   return ways[amount]
 
