bea652fcc17172e7454956aba0c5232392046fff
Kat Fries <katherynejf@gmail.com>
2019-01-14 15:33:31 -0500

    recipe_batches done

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python
 
 import math
-
+# recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
+# ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
 def recipe_batches(recipe, ingredients):
-  pass 
-
+  batches = []
+  for key in recipe:
+    if key not in ingredients.keys():
+      return 0
+    else:
+      batches.append(ingredients[key] // recipe[key])   
+  return min(batches)
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
