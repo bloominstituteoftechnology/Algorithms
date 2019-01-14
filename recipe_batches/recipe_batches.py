5170c473dcdd034600385d1cb025e4c8d6d62d5e
Patrick Thompson <patrick.jthompson93@gmail.com>
2019-01-14 13:07:41 -0800

    adds notes to recipe batches file

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python
 
 import math
+# need to check that both dicts have the same key
+# compare if the recipe value is > or < the ingredients value at that dict
+# if the recipe value > ingredients value we cant make any batches, so return count
+# else if the ingredients value > recipe value, subtract the recipe value from ingredients value, then call again
+
 
 def recipe_batches(recipe, ingredients):
-  pass 
+    pass
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
