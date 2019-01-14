0093019e4995e5d4f2f209dfe8f11ef1a7cfea47
Conner Alegre <cga0@pm.me>
2019-01-14 13:47:36 -0700

    Add stock_prices.py docstring

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,6 +3,15 @@
 import argparse
 
 def find_max_profit(prices):
+  """
+  Finds the max profit in a list of buy / sell points
+
+  NOTE: The last price point is never going to be the buy point as it would indicate no profit every time
+  because there is no sell point after the buy point, therefore we ignore the last point for a potential buy
+
+  This function finds the cheapest possible buy and the largest sell point (which is only available after the buy)
+  and returns the largest sell point minus the cheapest available buy point
+  """
   available_buys = prices[:-1]
   cheapest_buy_index = available_buys.index(min(available_buys))
   cheapest_buy = available_buys[cheapest_buy_index]
