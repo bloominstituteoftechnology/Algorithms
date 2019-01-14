c83275a35659bd441f9d06a4ef0238b4942f4d70
Patrick Thompson <patrick.jthompson93@gmail.com>
2019-01-14 15:27:51 -0800

    adds notes and finishes climbing stairs

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python
 
 import sys
+# num of remaining stairs is 0, then we break out of loop
+# if num of stairs is % 3 = 0, then thats one case
+# if num of stairs is % 2 = 0, then thats one case
+# else
+
 
 def climbing_stairs(n, cache=None):
-  pass 
+    if n < 0:
+        return 0
+    elif n == 0:
+        return 1
+    else:
+        return climbing_stairs(n-1, cache) + climbing_stairs(n-2, cache) + climbing_stairs(n-3, cache)
+        pass
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_stairs = int(sys.argv[1])
-    print("There are {ways} ways for a child to jump {n} stairs.".format(ways=climbing_stairs(num_stairs), n=num_stairs))
-  else:
-    print('Usage: climbing_stairs.py [num_stairs]')
+    if len(sys.argv) > 1:
+        num_stairs = int(sys.argv[1])
+        print("There are {ways} ways for a child to jump {n} stairs.".format(
+            ways=climbing_stairs(num_stairs), n=num_stairs))
+    else:
+        print('Usage: climbing_stairs.py [num_stairs]')
