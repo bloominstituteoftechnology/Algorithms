2dd2d3cb99100cacabe1a476a557f1a8a8219ce0
Troy Amlee <troyamlee19@gmail.com>
2019-01-14 16:12:06 -0600

    [mvp] Troy Amlee initial commit

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python
 
 import argparse
+# list_of_prices = [1050, 270, 1540, 3800, 2]
+
 
 def find_max_profit(prices):
+  sorted_prices = sorted(prices)
+  highest_num = sorted_prices[-1]
+  count = 0
+  def find_the_low(x):
+    if prices.index(highest_num) > prices.index(x):
+      lowest_num = x
+      return highest_num - lowest_num
+    if prices.index(highest_num) < prices.index(x):
+      count = count + 1
+      return find_the_low(sorted_prices[count])
+  return find_the_low(sorted_prices[0])
   pass
 
 
