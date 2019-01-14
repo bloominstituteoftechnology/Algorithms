8925df96c9578a8f2a6869ecf649d982787c54dd
lndubose <lndubose13@gmail.com>
2019-01-14 16:35:23 -0600

    Finish recipe function
    Finished the recipe batch. I believe the time complexity if O(n). I think I did
    pretty well on having this one work properly first go. I even have test cases
    to immediatley end the problem if the lengths are not the same, or a recipe
    and ingrediant key do not match.

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,35 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    countList = []
+
+    if len(recipe) != len(ingredients):
+        return 0
+
+    sorRec = sorted(recipe)
+    sorIng = sorted(ingredients)
+
+    for i in range(len(sorRec)):
+        ing = sorIng[i]
+        rec = sorRec[i]
+        print(ing, rec)
+        if rec == ing:
+            if recipe[rec] <= ingredients[ing]:
+                countList.append(int(ingredients[ing]/recipe[rec]))
+            else:
+                return 0
+        else:
+            return 0
+    return min(countList)
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("""{batches} batches can be made from the available ingredients:
+  {ingredients}.""".format(batches=recipe_batches(recipe, ingredients),
+                           ingredients=ingredients))
