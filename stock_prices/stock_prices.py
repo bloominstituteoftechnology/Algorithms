5296cec9bbb78c7bee7b3334d42ef48c1d19f2d3
Kevin <brackkevin@gmail.com>
2019-01-14 17:23:02 -0600

    Plugged recursive function back into boilerplate

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,47 +1,8 @@
 #!/usr/bin/python
 
-# COMMENTING INITIAL BOILERPLATE FOR NOW
-# import argparse
+import argparse
 
-# def find_max_profit(prices):
-#   pass
-
-
-# if __name__ == '__main__':
-#   # This is just some code to accept inputs from the command line
-#   parser = argparse.ArgumentParser(description='Find max profit from prices.')
-#   parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-#   args = parser.parse_args()
-
-#   print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
-
-##############
-# solution 1 #
-##############
-# Iterave
-def find_max_profit(prices):
-    top_profit = 0
-
-    for index in range(0, len(prices) - 1):
-        x = prices[index]
-        print(f'{index}: {x}')
-
-        for jndex in range(index + 1, len(prices)):
-            current_increase = prices[jndex] - prices[index]
-            if current_increase > top_profit:
-                top_profit = current_increase
-
-    return top_profit
-
-# find_max_profit([1050, 270, 1540, 3800, 2])
-print(f'SOLUTION 1 {find_max_profit([1050, 270, 1540, 3800, 2])}') # 3530
-
-##############
-# SOLUTION 2 #
-##############
-# Recursive
-
-def find_max_profit_2(prices, largest = 0):
+def find_max_profit(prices, largest = 0):
     local_largest = largest
     if len(prices) > 1:
         current = prices[0]
@@ -53,12 +14,64 @@ def find_max_profit_2(prices, largest = 0):
         if current_result > local_largest:
             local_largest = biggest_rest - current
         
-        return find_max_profit_2(prices[1:], local_largest)
+        return find_max_profit(prices[1:], local_largest)
 
     else:
         return local_largest
 
-print(f'SOLUTION 2 {find_max_profit_2([1050, 270, 1540, 3800, 2])}') # 3530
+
+if __name__ == '__main__':
+  # This is just some code to accept inputs from the command line
+  parser = argparse.ArgumentParser(description='Find max profit from prices.')
+  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
+  args = parser.parse_args()
+
+  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+
+##############
+# solution 1 #
+##############
+# Iterave
+# def find_max_profit(prices):
+#     top_profit = 0
+
+#     for index in range(0, len(prices) - 1):
+#         x = prices[index]
+#         print(f'{index}: {x}')
+
+#         for jndex in range(index + 1, len(prices)):
+#             current_increase = prices[jndex] - prices[index]
+#             if current_increase > top_profit:
+#                 top_profit = current_increase
+
+#     return top_profit
+
+# # find_max_profit([1050, 270, 1540, 3800, 2])
+# print(f'SOLUTION 1 {find_max_profit([1050, 270, 1540, 3800, 2])}') # 3530
+
+##############
+# SOLUTION 2 #
+##############
+# Recursive
+
+# def find_max_profit_2(prices, largest = 0):
+#     local_largest = largest
+#     if len(prices) > 1:
+#         current = prices[0]
+#         rest = prices[1:]
+#         rest.sort()
+#         biggest_rest = rest[-1]
+#         current_result = biggest_rest - current
+
+#         if current_result > local_largest:
+#             local_largest = biggest_rest - current
+        
+#         return find_max_profit_2(prices[1:], local_largest)
+
+#     else:
+#         return local_largest
+
+# print(f'SOLUTION 2 {find_max_profit_2([1050, 270, 1540, 3800, 2])}') # 3530
 
 # NOTES
 # 1 Understanding
@@ -98,3 +111,9 @@ print(f'SOLUTION 2 {find_max_profit_2([1050, 270, 1540, 3800, 2])}') # 3530
 #                   260                         129
 #[1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536,131072]
 #                   551                         195
+
+
+# in general iterative in this case starts lower and ramps quicker
+# recursive has higher overhead in the beginning but does not scale as rapidly
+# Unfortunately I did not account for a negative profit being the highest
+# need to look again
