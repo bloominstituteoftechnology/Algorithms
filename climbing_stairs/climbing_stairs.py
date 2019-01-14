566b546f965dd5f82b2f79e126e3a88ff77d32ca
Drew <drewsup123c@gmail.com>
2019-01-14 16:55:20 -0700

    finished what I could of the others, last commit of the day

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python
 
 import sys
-
+# can go up one stair at a time
+# two stairs at a time
+# three stairs at a time
+# and other random orders
 def climbing_stairs(n, cache=None):
-  pass 
+    options = [1, 2, 3]
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_stairs = int(sys.argv[1])
-    print("There are {ways} ways for a child to jump {n} stairs.".format(ways=climbing_stairs(num_stairs), n=num_stairs))
-  else:
-    print('Usage: climbing_stairs.py [num_stairs]')
+    if len(sys.argv) > 1:
+        num_stairs = int(sys.argv[1])
+        print("There are {ways} ways for a child to jump {n} stairs.".format(ways=climbing_stairs(num_stairs), n=num_stairs))
+    else:
+        print('Usage: climbing_stairs.py [num_stairs]')
--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,12 +3,17 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  array = []
+  options = ['rock', 'paper', 'scissors']
+  for i in range(0, len(options)):
+    array.append([options[i]] * n)
+  return array
 
 
-if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+print(rock_paper_scissors())
+# if __name__ == "__main__":
+#   if len(sys.argv) > 1:
+#     num_plays = int(sys.argv[1])
+#     print(rock_paper_scissors(num_plays))
+#   else:
+#     print('Usage: rps.py [num_plays]')
