bb2efdba6e2123773ad030a831c931fcebb94e99
Riley-Brown <riley@riley.gg>
2019-01-14 16:51:19 -0500

    Added function for stock prices

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -4,16 +4,28 @@ import argparse
 
 
 def find_max_profit(prices):
-    stocks = input('Enter your stock prices')
+    prices = iter(prices)
+    least = next(prices)
+    yield 0
+    for price in prices:
+        least = min(least, price)
+        yield price - least
 
 
-if __name__ == '__main__':
-    # This is just some code to accept inputs from the command line
-    parser = argparse.ArgumentParser(
-        description='Find max profit from prices.')
-    parser.add_argument('integers', metavar='N', type=int,
-                        nargs='+', help='an integer price')
-    args = parser.parse_args()
+# if __name__ == '__main__':
+#     # This is just some code to accept inputs from the command line
+#     parser = argparse.ArgumentParser(
+#         description='Find max profit from prices.')
+#     parser.add_argument('integers', metavar='N', type=int,
+#                         nargs='+', help='an integer price')
+#     args = parser.parse_args()
 
-    print("A profit of ${profit} can be made from the stock prices {prices}.".format(
-        profit=find_max_profit(args.integers), prices=args.integers))
+#     print("A profit of ${profit} can be made from the stock prices {prices}.".format(
+#         profit=find_max_profit(args.integers), prices=args.integers))
+
+
+prices = [100, 113, 110, 85, 105, 102, 86,
+          63, 81, 101, 94, 106, 101, 79, 94, 90, 97]
+print(max(find_max_profit(prices)))
+print(max(find_max_profit([1050, 270, 1540, 3800, 2])))
+print(max(find_max_profit([10, 7, 5, 8, 11, 9])))
