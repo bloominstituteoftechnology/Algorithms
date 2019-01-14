0bdb93d93c93adb78ade92c901dfa9fe8da39768
stdemicheli <stdemicheli@gmail.com>
2019-01-14 22:20:27 +0100

    Initial commit

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,8 +3,22 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  if len(prices) <= 1:
+    return 0
 
+  max_price = prices[0]
+  min_price = prices[0]
+
+  for i, price in enumerate(prices):
+    if price < min_price:
+      max_price = 0
+      min_price = price
+    elif price > max_price:
+      max_price = price
+    print(f"Max{max_price}")
+    print(f"Min{min_price}\n")
+
+  return max_price - min_price
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
