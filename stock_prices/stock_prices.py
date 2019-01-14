44b89d8fe7095976a0f4d0d0e41148b0e27902ec
Jeanette <jeanfern555@gmail.com>
2019-01-14 15:00:32 -0600

    pseudo code

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,6 +2,18 @@
 import argparse
 
 def find_max_profit(prices):
+  '''
+  description:
+    this function will find the max profit from buying/selling a stock
+
+  pseudo: 
+    - start with a loop
+    - at the index compare the other values to the right of it, if the right value is greater it'll find the difference
+      - add something to hold the profit value
+    - the loop will continue and when it reaches another greater right value it'll return the profit and check if it's 
+      greater than the previos profit value, which it'll then keep the greatest profit
+    - it'll do this through the whole array until there is only one max profit
+  '''
   pass
 
 
