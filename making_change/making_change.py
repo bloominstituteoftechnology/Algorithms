0d796eedada0da44db7063ad338c04d15bb20433
Katie Cleary <katiecleary@airmaster.lan>
2019-01-14 18:15:54 -0500

    work in progress

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -1,14 +1,27 @@
 #!/usr/bin/python
 
 import sys
-amount = 0
+amount = 30
 denominations =[1,5,10,25,50]
 
 def making_change(amount, denominations):
 
-  pass 
+  if amount >= 25 and amount <= 50:
+    x = round((amount/5)/2)
 
+    y = (amount/25)
+    z = round(amount/25)
+    xx = y - z
+    xy = xx(25)
 
+    combos = xy + x
+    return combos
+  else:
+    return (print("not in range"))
+
+making_change(amount, denominations)
+
+'''
 if __name__ == "__main__":
   # Test our your implementation from the command line
   # with `python making_change.py [amount]` with different amounts
@@ -17,4 +30,4 @@ if __name__ == "__main__":
     amount = int(sys.argv[1])
     print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
   else:
-    print("Usage: making_change.py [amount]")
+    print("Usage: making_change.py [amount]")'''
