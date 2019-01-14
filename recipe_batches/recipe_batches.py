d6b9db16728b70c116c0fdd4ceba8d646ed08882
lndubose <lndubose13@gmail.com>
2019-01-14 16:49:04 -0600

    Refactor function
    Found another way to do this function. I am not sure about the time complexity of it
    because of the 'if in' statement. I found that could be a O(n) or O(1).

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,7 +2,31 @@
 
 import math
 
+# O(n^2 log n)
+# def recipe_batches(recipe, ingredients):
+#     countList = []
 
+#     if len(recipe) != len(ingredients):
+#         return 0
+
+#     sorRec = sorted(recipe)  # O(n log n)
+#     sorIng = sorted(ingredients)
+
+#     for i in range(len(sorRec)):  # O(n)
+#         ing = sorIng[i]
+#         rec = sorRec[i]
+#         print(ing, rec)
+#         if rec == ing:
+#             if recipe[rec] <= ingredients[ing]:
+#                 countList.append(int(ingredients[ing]/recipe[rec]))
+#             else:
+#                 return 0
+#         else:
+#             return 0
+#     return min(countList)
+
+
+# O(n) possibly O(n^2)
 def recipe_batches(recipe, ingredients):
     countList = []
 
