99aa633ec971eb23fd0cf12dd251a474f58312dc
Kevin <brackkevin@gmail.com>
2019-01-14 15:46:42 -0600

    Brute force of stock_prices.py complete

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,18 +1,39 @@
 #!/usr/bin/python
 
-import argparse
+# COMMENTING INITIAL BOILERPLATE FOR NOW
+# import argparse
 
+# def find_max_profit(prices):
+#   pass
+
+
+# if __name__ == '__main__':
+#   # This is just some code to accept inputs from the command line
+#   parser = argparse.ArgumentParser(description='Find max profit from prices.')
+#   parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
+#   args = parser.parse_args()
+
+#   print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+
+##############
+# solution 1 #
+##############
 def find_max_profit(prices):
-  pass
+    top_profit = 0
 
+    for index in range(0, len(prices) - 1):
+        x = prices[index]
+        print(f'{index}: {x}')
 
-if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+        for jndex in range(index + 1, len(prices)):
+            current_increase = prices[jndex] - prices[index]
+            if current_increase > top_profit:
+                top_profit = current_increase
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+    return top_profit
+
+# find_max_profit([1050, 270, 1540, 3800, 2])
+print(find_max_profit([1050, 270, 1540, 3800, 2])) # 3530
 
 
 # NOTES
@@ -26,6 +47,12 @@ if __name__ == '__main__':
   # is that something we need to be concerned with?
 
 # 3 come up with first plan
+  # create a variable and set it to 0
+  # for each item in the list subtract it from each
+    # price to the right
+  # if the current is greater than what is in the initial variable
+    # over write the initial variable
+  # when the loop finishes return the initial variable
 
 # 4 Think of how to improve
 
