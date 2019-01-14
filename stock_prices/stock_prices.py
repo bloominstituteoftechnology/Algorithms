4a0fd5886d67c7d8fc74b32a68fe9ba72ad48ad1
lndubose <lndubose13@gmail.com>
2019-01-14 15:13:44 -0600

    First working solution
    Have a working solution, but time complexity is O(n^2).

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,14 +2,25 @@
 
 import argparse
 
+
 def find_max_profit(prices):
-  pass
+    max = -1000
+    for i in range(len(prices) - 1):
+        for j in range(i, len(prices)):
+            subtract = prices[j] - prices[i]
+            if subtract > max and i != j:
+                max = subtract
+    return max
 
 
 if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+    # This is just some code to accept inputs from the command line
+    parser = argparse.ArgumentParser(
+        description='Find max profit from prices.')
+    parser.add_argument('integers', metavar='N', type=int,
+                        nargs='+', help='an integer price')
+    args = parser.parse_args()
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+    print("A profit of ${profit} can be made from the stock prices {prices}."
+          .format(
+              profit=find_max_profit(args.integers), prices=args.integers))
