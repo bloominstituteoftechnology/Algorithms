d18406efa14e763489f831ce6f41be62d6a3dcf3
Vuk Radosavljevic <vukrado1@gmail.com>
2019-01-15 17:30:52 -0600

    rps works but not in the correct order of the unit tests

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,9 +1,33 @@
 #!/usr/bin/python
 
 import sys
+from itertools import permutations
+
+
 
 def rock_paper_scissors(n):
-  pass 
+  rps = { 1:'rock', 2:'paper', 3:'scissors' }
+  my_list = []
+
+  for _ in range(n):
+    my_list.append(1)
+    my_list.append(2)
+    my_list.append(3)
+  
+  perm = list(set(list(permutations(my_list, n))))
+  
+  different_ways = []
+
+  for item in perm:
+    current = []
+    for num in item:
+      current.append(rps[num])
+    different_ways.append(current)
+  return different_ways
+
+
+
+
 
 
 if __name__ == "__main__":
