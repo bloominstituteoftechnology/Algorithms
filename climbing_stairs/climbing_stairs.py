b93c0cee9d205546165b6bef877d8c0f6ee5e70b
luiscmartinez <mrluismartinezzz@gmail.com>
2019-01-15 12:24:18 -0800

    pass climbing stairs small test

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -2,13 +2,23 @@
 
 import sys
 
-def climbing_stairs(n, cache=None):
-  pass 
+
+def climbing_stairs(n, cache={}):
+    if n == 0:
+        return 1
+    if n < 0:
+        return 0
+
+    nth = climbing_stairs(
+        n-1) + climbing_stairs(n-2) + climbing_stairs(n-3)
+    cache[n] = nth
+    return nth
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_stairs = int(sys.argv[1])
-    print("There are {ways} ways for a child to jump {n} stairs.".format(ways=climbing_stairs(num_stairs), n=num_stairs))
-  else:
-    print('Usage: climbing_stairs.py [num_stairs]')
+    if len(sys.argv) > 1:
+        num_stairs = int(sys.argv[1])
+        print("There are {ways} ways for a child to jump {n} stairs.".format(
+            ways=climbing_stairs(num_stairs), n=num_stairs))
+    else:
+        print('Usage: climbing_stairs.py [num_stairs]')
--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -4,14 +4,13 @@ import math
 
 
 def recipe_batches(recipe, ingredients):
-  # return min(int(ingredients.get(elem, 0) / num) for elem, num in recipe.items())
+    # return min(int(ingredients.get(elem, 0) // num) for elem, num in recipe.items())
     batches = []
     if len(recipe) != len(ingredients):
         return 0
-
     for el in recipe:
         if recipe[el] <= ingredients[el]:
-            batches.append(int(ingredients[el]/recipe[el]))
+            batches.append(int(ingredients[el]//recipe[el]))
         else:
             return 0
 
@@ -21,7 +20,7 @@ def recipe_batches(recipe, ingredients):
 if __name__ == '__main__':
         # Change the entries of these dictionaries to test
         # your implementation with different inputs
-    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
-    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    recipe = {'milk': 2, 'sugar': 40, 'butter': 20}
+    ingredients = {'milk': 5, 'sugar': 120, 'butter': 500}
     print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
         batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
