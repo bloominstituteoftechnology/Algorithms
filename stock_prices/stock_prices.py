22ddae9fe63f51daa350f7099ff4a876ef91cbd8
IAN CAMERON <iancameron@IANs-MacBook-Pro.local>
2019-01-15 16:08:35 -0700

    stairway to recursive heaven

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -12,6 +12,7 @@ def find_max_profit(prices):
       max_profit = prices[i] - min_price
   return max_profit
 
+  
 
 
 if __name__ == '__main__':
@@ -21,3 +22,48 @@ if __name__ == '__main__':
   args = parser.parse_args()
 
   print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+
+#beej solution
+  #first swipe
+  # if buy at first price, check the rest of the prices for potential max profit
+  """def find_max_profit(prices):
+    bought = prices[0]
+
+    for p in prices[1:]:
+      profit = p - bought
+      print profit 
+      #how then to keep track of max profit
+      if profit > max_profit:
+        max_profit = profit
+        print(max_profit)"""
+      #good but only tells you if you buy at first value, not at any random value
+      #max(p - bought)
+
+"""  def find_max_profit(prices):
+    max_profit = 
+
+    while prices != []:
+      bought = prices[0]
+
+      for p in prices[1:]:
+        profit = p - bought
+          if profit > max_profit:
+            max_profit = profit
+        
+      prices.pop(0)
+    
+    print(max_profit)"""
+    #works but not optimized
+
+#optimized
+"""def find_max_profit(prices):
+  min_price = prices[0]
+  max_profit = prices[1] - min_price
+
+  for p in prices[1:]:
+    profit = p - min_price
+
+    max_profit = max(profit, max_profit)
+    min_price = min(p, min_price)
+
+  return max_profit"""
