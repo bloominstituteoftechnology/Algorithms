c816d65a1c7e7b99e6bd7c096f22d76408965d5a
sumi419 <sasgar04@gmail.com>
2019-01-14 18:07:02 -0800

    stock prices 🧁🎉

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,8 +2,26 @@
 
 import argparse
 
+# max profit variable start at inital index and compare 
+# min price make it index independent
+import math 
 def find_max_profit(prices):
-  pass
+  max_profit = -math.inf
+  min_price = math.inf
+  for price in prices:
+    max_profit = max(price - min_price, max_profit) # 0 0 1270 3530 3530
+    min_price = min(price, min_price) #1050 270 270 270 2
+  return max_profit
+
+
+print(find_max_profit([1050, 270, 1540, 3800, 2]))
+# current is 1050
+# maxProfit = max(current - min_price, maxProfit)
+
+print(find_max_profit([33, 350, 120, 600, 460]))
+find_max_profit([100, 90, 80, 50, 20, 10]) # -10
+
+
 
 
 if __name__ == '__main__':
