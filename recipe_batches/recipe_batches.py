db0fbcf8d96c1a57d03f958ba0d9d6acfb617be8
Samantha Gatt <smfgatt@gmail.com>
2019-01-15 16:55:39 -0500

    Optimizes recipe_batches.py for time efficiency

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -4,6 +4,8 @@ import math
 
 
 def recipe_batches(recipe, ingredients):
+    # First implementation
+    """
     count = 0
     while True:
         for item in recipe:
@@ -15,11 +17,21 @@ def recipe_batches(recipe, ingredients):
                 ingredients[item] -= recipe[item]
         # If each item in recipe gets to the else statement
         count += 1
+    """
+    
+    # Time efficiency optimization - O(n)
+    batches = []
+    for item in recipe:
+        if item not in ingredients:
+            return 0
+        else:
+            batches.append(ingredients[item] // recipe[item])
+    return min(batches)
 
 
 print(recipe_batches(
-  {'milk': 100, 'butter': 50, 'flour': 5},
-  {'milk': 138, 'butter': 51, 'flour': 51}
+    {'milk': 100, 'butter': 50, 'flour': 5},
+    {'milk': 138, 'butter': 51, 'flour': 51}
 ))
 
 if __name__ == '__main__':
