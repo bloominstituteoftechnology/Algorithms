056b05cc43476a2fcca657e2a80f7fc08501c047
lndubose <lndubose13@gmail.com>
2019-01-15 17:48:46 -0600

    Got a solution

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -7,18 +7,37 @@ def rock_paper_scissors(n):
     allpos = []
 
     def rec(i, item, clist=[]):
-        print('here', item, clist)
+        nonlocal allpos
         if i == 0:
-            return clist
-        else:
-            clist = clist + [item]
-            allpos = [[rec(i-1, 'rock', clist)] + [rec(i-1, 'paper', clist)]]
+            allpos = allpos + [clist]
+        elif i < 0:
+            return 0
 
+        clist = clist + [item]
+        rec(i-1, 'rock', clist)
+        rec(i-1, 'paper', clist)
+        rec(i-1, 'scissors', clist)
     rec(n, 'rock')
-    print('paper')
     rec(n, 'paper')
-    return allpos
+    rec(n, 'scissors')
+    return allpos[::3]
 
+
+# def rock_paper_scissors(n):
+#     allpos = []
+#     items = ['rock', 'paper', 'scissors']
+#     lis = []
+#     for i in items:
+#         lis = [i]
+#         while n != 0:
+#             for j in items:
+#                 lis += [j]
+#                 n -= 1
+#             allpos += [lis]
+#     return allpos
+
+
+print(rock_paper_scissors(2))
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
