b472a1db4132091c111a48f28565d2be7b82c8f2
Austinlp4 <apendergrast44@gmail.com>
2019-01-15 15:33:44 -0600

    rock paper scissors progress plus pseudo

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,8 +2,40 @@
 
 import sys
 
-def rock_paper_scissors(n):
-  pass 
+def rock_paper_scissors(n, round = [], round_num = 1, possibilities = []):
+  ''' 
+    define options - rock paper or scissors
+    declare 0 and 1 cases
+    round place variable
+    possibilities variable
+    current round
+
+    loop through options to generate rounds not in possibilities
+    return possibilities
+    or recurse
+  '''
+  options = ['rock', 'paper', 'scissors']
+
+  if n == 0:
+    return [[]]
+  if n == 1:
+    return [[options[0]], [options[1]], [options[2]]]
+
+  for i, val in enumerate(options):
+    round.append(options[i])
+    if round in possibilities:
+      return possibilities
+    if round_num == n:
+      possibilities.append(round)
+      round = []
+      round_num = 1
+    else:
+      rock_paper_scissors(n, round, round_num + 1, possibilities)
+
+  return possibilities
+
+
+print(rock_paper_scissors(2))
 
 
 if __name__ == "__main__":
