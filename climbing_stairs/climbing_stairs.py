350c3fa84cc70bd233e4a6746fd7764b94fd3dbf
Kevin Sooter <kevin.m.sooter@gmail.com>
2019-01-15 15:20:03 -0600

    Completed climbing stairs

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -2,8 +2,19 @@
 
 import sys
 
+
 def climbing_stairs(n, cache=None):
-  pass 
+  if (n == 0 or n == 1):
+    return 1
+  elif (n == 2):
+    return 2
+  
+  else:
+    return climbing_stairs(n - 1) + climbing_stairs(n - 2) + climbing_stairs(n - 3)
+
+
+n = 10
+print(climbing_stairs(n))
 
 
 if __name__ == "__main__":
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -4,13 +4,14 @@ import argparse
 
 
 def find_max_profit(prices):
-    max_profit = 0
-    while len(prices) > 1:
-        price = prices[0]
-        if max(prices[1:]) - price > max_profit or max_profit == 0:
-            max_profit = max(prices[1:]) - price
-        prices.pop(0)
-    return max_profit
+  prices = prices.copy()
+  max_profit = float("-inf")
+  while len(prices) > 1:
+    price = prices[0]
+    if max(prices[1:]) - price > max_profit:
+      max_profit = max(prices[1:]) - price
+    prices.pop(0)
+  return max_profit
 
 
 if __name__ == '__main__':
