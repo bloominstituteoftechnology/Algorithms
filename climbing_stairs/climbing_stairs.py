553ff5141e6f20637ab3c1ef418585bf92f25757
michaelagard <mdagard@gmail.com>
2019-01-15 17:04:12 -0500

    next attempt

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -2,13 +2,25 @@
 
 import sys
 
+
 def climbing_stairs(n, cache=None):
-  pass 
+    hops = 0
+    ways = 0
+    if n <= 0:
+        return hops
+    if n > 3:
+        if n % 3 == 0:
+            hops += 3
+            print("hop")
+            climbing_stairs((n - hops))
+    return hops
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_stairs = int(sys.argv[1])
-    print("There are {ways} ways for a child to jump {n} stairs.".format(ways=climbing_stairs(num_stairs), n=num_stairs))
-  else:
-    print('Usage: climbing_stairs.py [num_stairs]')
+    if len(sys.argv) > 1:
+        num_stairs = int(sys.argv[1])
+        print("There are {ways} ways for a child to jump {n} stairs.".format(
+            ways=climbing_stairs(num_stairs), n=num_stairs))
+    else:
+        print('Usage: climbing_stairs.py [num_stairs]')
+# recursion possible
