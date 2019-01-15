5b5cf4aeb4c862a5d9574e8139a0977e99cdb88d
Drew <drewsup123c@gmail.com>
2019-01-15 16:09:27 -0700

    did random work, achieved nothing lol

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python
 
 import sys
+#  Your output should be a list of lists containing strings.
+#  Each inner list should have length equal to the input n.
+
 
 def rock_paper_scissors(n):
-  array = []
-  options = ['rock', 'paper', 'scissors']
-  for i in range(0, len(options)):
-    array.append([options[i]] * n)
-  return array
+    array = []
+    options = ['rock', 'paper', 'scissors']
+    if n == 0:
+        return [array]
+    if n == 1:
+        for o in options:
+            array.append([o])
+        return array
+    for i in range(0, 3 ** n):
+        array.append([options[0]]*n)
+    return array
 
 
-print(rock_paper_scissors())
-# if __name__ == "__main__":
-#   if len(sys.argv) > 1:
-#     num_plays = int(sys.argv[1])
-#     print(rock_paper_scissors(num_plays))
-#   else:
-#     print('Usage: rps.py [num_plays]')
+print(rock_paper_scissors(1))
+
+if __name__ == "__main__":
+  if len(sys.argv) > 1:
+    num_plays = int(sys.argv[1])
+    print(rock_paper_scissors(num_plays))
+  else:
+    print('Usage: rps.py [num_plays]')
