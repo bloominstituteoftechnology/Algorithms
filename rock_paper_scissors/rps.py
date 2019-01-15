b549114b92a1f73539f455ca3877bb6833a8b4ac
lndubose <lndubose13@gmail.com>
2019-01-15 16:56:31 -0600

    Current solution
    Not working. Current idea.

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,27 @@
 
 import sys
 
+
 def rock_paper_scissors(n):
-  pass 
+    allpos = []
+
+    def rec(i, item, clist=[]):
+        print('here', item, clist)
+        if i == 0:
+            return clist
+        else:
+            clist = clist + [item]
+            allpos = [[rec(i-1, 'rock', clist)] + [rec(i-1, 'paper', clist)]]
+
+    rec(n, 'rock')
+    print('paper')
+    rec(n, 'paper')
+    return allpos
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
