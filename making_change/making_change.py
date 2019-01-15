acd5f6137486aa23df449e87b940afdcbb4a47ca
Kevin <brackkevin@gmail.com>
2019-01-15 16:41:13 -0600

    Variables returned to fit with boilerplate

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -4,54 +4,47 @@ import sys
 
 cache = {}
 
-denominations = {
-  'p': 1,
-  'n': 5,
-  'd': 10,
-  'q': 25,
-  'h': 50
-}
 
-def making_change(n, d = denominations):
+def making_change(amount, denominations):
   global cache
 
-  if n in cache:
-    return cache[n]
+  if amount in cache:
+    return cache[amount]
 
-  elif n == 0:
+  elif amount == 0:
     return 1
 
-  elif n < 5:
-    result = making_change(n - d['p'])
-    cache[n] = result
+  elif amount < 5:
+    result = making_change(amount - denominations[0], denominations)
+    cache[amount] = result
     return result
 
-  elif n < 10 and n >= 5 :
-    result = making_change(n - d['p']) + making_change(n - d['n'])
-    cache[n] = result
+  elif amount < 10 and amount >= 5 :
+    result = making_change(amount - denominations[0], denominations) + making_change(amount - denominations[1], denominations)
+    cache[amount] = result
     return result
 
-  elif n < 25 and n >= 10 :
-    result = making_change(n - d['p']) + making_change(n - d['n']) + making_change(n - d['d'])
-    cache[n] = result
+  elif amount < 25 and amount >= 10 :
+    result = making_change(amount - denominations[0], denominations) + making_change(amount - denominations[1], denominations) + making_change(amount - denominations[2], denominations)
+    cache[amount] = result
     return result
 
-  elif n < 50 and n >= 25 :
-    result = making_change(n - d['p']) + making_change(n - d['n']) + making_change(n - d['d']) + making_change(n - d['q'])
-    cache[n] = result
+  elif amount < 50 and amount >= 25 :
+    result = making_change(amount - denominations[0], denominations) + making_change(amount - denominations[1], denominations) + making_change(amount - denominations[2], denominations) + making_change(amount - denominations[3], denominations)
+    cache[amount] = result
     return result
 
   else:
-    result = making_change(n - d['p']) + making_change(n - d['n']) + making_change(n - d['d']) + making_change(n - d['q']) + making_change(n - d['h'])
+    result = making_change(amount - denominations[0], denominations) + making_change(amount - denominations[1], denominations) + making_change(amount - denominations[2], denominations) + making_change(amount - denominations[3], denominations) + making_change(amount - denominations[4], denominations)
+    cache[amount] = result
+    return result
 
-# if __name__ == "__main__":
-#   # Test our your implementation from the command line
-#   # with `python making_change.py [amount]` with different amounts
-#   if len(sys.argv) > 1:
-#     denominations = [1, 5, 10, 25, 50]
-#     amount = int(sys.argv[1])
-#     print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
-#   else:
-#     print("Usage: making_change.py [amount]")
-
-print(making_change(20))
+if __name__ == "__main__":
+  # Test our your implementation from the command line
+  # with `python making_change.py [amount]` with different amounts
+  if len(sys.argv) > 1:
+    denominations = [1, 5, 10, 25, 50]
+    amount = int(sys.argv[1])
+    print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
+  else:
+    print("Usage: making_change.py [amount]")
