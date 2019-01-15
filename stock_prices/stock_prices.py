2c3e9fd615bd7683ac353e4b397d57d6c943d394
Barbara Hunter <commandandcode@gmail.com>
2019-01-15 15:23:41 -0500

    stockprices finished

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,29 +1,43 @@
 #!/usr/bin/python
 
 import argparse
+# if x is negative, find the one closest to 0
+# if x is positive, find the one farthest from 0.
+#example: [10, 7, 5, 8, 11, 9]
+# input: [1050, 270, 1540, 3800, 2]
 
-highnum = []
-lownum = []
-complist = []
 
 def find_max_profit(prices):
-  for price in prices:
-    if price - [price] > 0:
-      append(lownum)
-    if price - [price] < 0:
-      append(highnum)  
-  for i in highnum:
-    for n in lownum:
-      highnum[i] > lownum[n]
-      complist.append([i]-[n])
-  print (complist)
-  pass 
+    negative_list = []
+    positive_list = []
+    for index, x in enumerate(prices):
+        if index < len(prices) - 1 and x >= prices[index + 1]:
+            negative_list.append(-(x-prices[index+1]))
+            negative_list.sort()
+        elif index == len(prices)-1:
+            break
+        else:
+            positive_list.append(prices[index+1]-x)
+            positive_list.sort()
+
+    print(negative_list)
+    print(positive_list)
+    if(positive_list == []):
+        return negative_list[-1]
+    elif(len(positive_list)-1 > 2):
+        return positive_list[-1]
+    else:
+        return sum(positive_list)
+    pass
 
 
 if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+    # This is just some code to accept inputs from the command line
+    parser = argparse.ArgumentParser(
+        description='Find max profit from prices.')
+    parser.add_argument('integers', metavar='N', type=int,
+                        nargs='+', help='an integer price')
+    args = parser.parse_args()
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+    print("A profit of ${profit} can be made from the stock prices {prices}.".format(
+        profit=find_max_profit(args.integers), prices=args.integers))
