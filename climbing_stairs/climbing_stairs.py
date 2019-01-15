7f0c06daf83d237d233888cbd459f494ce08046b
Troy Amlee <troyamlee19@gmail.com>
2019-01-15 14:53:05 -0600

    [mvp] completed climbing stairs

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -1,8 +1,41 @@
 #!/usr/bin/python
 
 import sys
+# count = 0
+# x = 0
+def climbing_stairs(n, cache={}):
+  #nCr = n!/r!(n-r)!
 
-def climbing_stairs(n, cache=None):
+  if n < 0:
+    return 0
+  if n == 0:
+    return 1
+  if n == 1:
+    return 1
+  if n == 2:
+    return 2
+  if n == 3:
+    return 4
+  if n == 4:
+    return 7
+
+  if n in cache:
+    return cache[n]
+
+  nth = climbing_stairs(n-1) + climbing_stairs(n-2) + climbing_stairs(n-3)
+  cache[n] = nth
+  return nth
+
+
+
+
+          
+            
+
+    
+
+
+
   pass 
 
 
