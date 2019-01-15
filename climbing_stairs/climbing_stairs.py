17a843999c4ef5df8e7b03483db78f9b47f446a1
sean-one <basement365@gmail.com>
2019-01-15 15:15:53 -0800

    right now climbing stairs works for smaller cases

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -14,12 +14,16 @@ def climbing_stairs(n, cache=None):
   create dictionary
   add k+1 : k-1.v + k-2.v + k-3.v until i reach the inputed number, also storing the values for future look up
   """
-  pass 
-
-
-if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_stairs = int(sys.argv[1])
-    print("There are {ways} ways for a child to jump {n} stairs.".format(ways=climbing_stairs(num_stairs), n=num_stairs))
+  past_values = {0:1, 1:1, 2:2}
+  if n in past_values:
+    return past_values[n]
   else:
-    print('Usage: climbing_stairs.py [num_stairs]')
+    past_values[n] = climbing_stairs(n-1) + climbing_stairs(n-2) + climbing_stairs(n-3)
+    return past_values[n]
+    
+# if __name__ == "__main__":
+#   if len(sys.argv) > 1:
+#     num_stairs = int(sys.argv[1])
+#     print("There are {ways} ways for a child to jump {n} stairs.".format(ways=climbing_stairs(num_stairs), n=num_stairs))
+#   else:
+#     print('Usage: climbing_stairs.py [num_stairs]')
