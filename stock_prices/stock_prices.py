222ed215e0cfc4c7c63f899592bdf53a8dd8522b
Riley-Brown <riley@riley.gg>
2019-01-15 18:25:18 -0500

    updated stock price algorithm

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,15 +1,36 @@
 #!/usr/bin/python
-
+import math
 import argparse
 
 
+# def find_max_profit(prices):
+    
+#     max_profit = float("inf")
+
+#     while prices != []:
+#         bought = prices[0]
+
+#         for p in prices[1:]:
+#             profit = p - bought
+#             if profit > max_profit:
+#                 max_profit = profit
+        
+#         prices.pop(0)
+    
+#     print(max_profit)
+
 def find_max_profit(prices):
-    prices = iter(prices)
-    least = next(prices)
-    yield 0
-    for price in prices:
-        least = min(least, price)
-        yield price - least
+    min_buy_price = prices[0]
+    max_profit = prices[1] - min_buy_price
+
+    for p in prices[1:]:
+        profit = p - min_buy_price
+
+        max_profit = max(profit, max_profit)
+        min_buy_price = min(p, min_buy_price)
+    
+    return max_profit
+
 
 
 # if __name__ == '__main__':
@@ -23,9 +44,8 @@ def find_max_profit(prices):
 #     print("A profit of ${profit} can be made from the stock prices {prices}.".format(
 #         profit=find_max_profit(args.integers), prices=args.integers))
 
-
 prices = [100, 113, 110, 85, 105, 102, 86,
           63, 81, 101, 94, 106, 101, 79, 94, 90, 97]
-print(max(find_max_profit(prices)))
-print(max(find_max_profit([1050, 270, 1540, 3800, 2])))
-print(max(find_max_profit([10, 7, 5, 8, 11, 9])))
+print(find_max_profit(prices))
+# print(max(find_max_profit([1050, 270, 1540, 3800, 2])))
+# print(max(find_max_profit([10, 7, 5, 8, 11, 9])))
