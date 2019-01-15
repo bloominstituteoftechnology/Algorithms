f35faf317e4cdefa584c4617897aaa146cdde0b8
Steven O'Neill <oneillsteven957@gmail.com>
2019-01-15 17:08:08 -0600

    completed climbing_stairs

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -2,9 +2,16 @@
 
 import sys
 
-def climbing_stairs(n, cache=None):
-  pass 
+def climbing_stairs(n, cache={}):
+  if n == 0 or n == 1:
+    return 1
+  if n == 2:
+    return 2
+  
+  if n not in cache:
+    cache[n] = climbing_stairs(n - 1) + climbing_stairs(n - 2) + climbing_stairs(n - 3)
 
+  return cache[n]
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
