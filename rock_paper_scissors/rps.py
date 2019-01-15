2e8fc160f4c55a6c57832e2b6fd0ca7f40999b17
lajawanti <myemail>
2019-01-15 17:39:35 -0500

    started rps.py

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,7 +3,17 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+    rps_list = ['rock', 'paper', 'scissors']
+    result = []
+
+    #need to define inner helper funtion as to store list in list, this function should take ans return list
+    def inner_helper(li = []):
+        if len(li) < n:
+            for items in rps_list: #1 : rock, 2 : paper, 3: scissors
+                print(items)
+            
+    
+    
 
 
 if __name__ == "__main__":
