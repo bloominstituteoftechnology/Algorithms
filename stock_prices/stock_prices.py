6517d811d37c08bfe69dbdc19b1a1fec528f6828
David Benavidez <benavidez_david13@yahoo.com>
2019-01-15 09:20:46 -0800

    stockprices done

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python
-
 import argparse
-
 def find_max_profit(prices):
+  max_profit=0
+  for i in range(len(prices)-1):
+    for j in range(i+1,len(prices)):
+      if prices[j]-prices[i]>max_profit:
+        max_profit= prices[j]-prices[i]
+    return max_profit
+
+
   pass
 
 
