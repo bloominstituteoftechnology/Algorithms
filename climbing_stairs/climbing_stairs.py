adea9701560c893e47a31302b05f87f076c979d2
jmbradley <jmbradley808@gmail.com>
2019-01-15 17:53:52 -0500

    tweaking cimbing

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -3,7 +3,25 @@
 import sys
 
 
-def climbing_stairs(n, cache=None):
+def climbing_stairs(n):
+    if n <= 1:
+        return 1
+    elif n == 2:
+        return 2
+    elif n == 3:
+        return 4
+    else:
+        return climbing_stairs(n-1) + climbing_stairs(n-2) + climbing_stairs(n-3)
+    # count = 0
+    # length = 'aaa'
+    # while length <= n:
+    #     a = b
+    #     b = c
+    #     c = a + b
+    # count = count + 1
+    # str1 = str(c)
+    # if len(str1) > len(length):
+    #     length = str1
 
 
 if __name__ == "__main__":
