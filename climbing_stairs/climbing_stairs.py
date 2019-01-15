daea5a980288491a51d2b89223148d3d5de9a3b8
sumi419 <sasgar04@gmail.com>
2019-01-15 14:33:04 -0800

    climbing stairs 🧗

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python
 import sys
-
+#cache holds previous values
 def climbing_stairs(n, cache=None):
-  pass 
+  if n <=  1:
+    return 1
+  if n == 2: 
+    return 2
+  if n == 3:
+    return 4
+  return climbing_stairs(n - 1) + climbing_stairs(n-2) + climbing_stairs(n-3)
+  # nth = climbing_stairs(n - 1) + climbing_stairs(n-2) + climbing_stairs(n-3)
+  # cache[n] = nth
+  # return nth
 
-
+# if n is 0 theres one way to climb it
+# 2 steps from theres only 2 ways = n - 2 steps
 if __name__ == "__main__":
   if len(sys.argv) > 1:
     num_stairs = int(sys.argv[1])
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -6,13 +6,59 @@ import argparse
 # min price make it index independent
 import math 
 def find_max_profit(prices):
-  max_profit = -math.inf
+  max_profit = -math.inf # or float("-inf")
   min_price = math.inf
   for price in prices:
+    print(f'This is the old max profit {max_profit}')
     max_profit = max(price - min_price, max_profit) # 0 0 1270 3530 3530
+    print(f'This is the next max profit if I buy it at this price {price - min_price}')
     min_price = min(price, min_price) #1050 270 270 270 2
   return max_profit
 
+# when you buy at first value and sell at the others
+# def find_max_profit(prices):
+#   bought = prices[0]
+#   max_profit = -9999999
+
+#   for p in prices[1:]:
+#     profit = p - bought
+#     if profit > max_profit:
+#       max_profit = profit
+  
+#   print(max_profit)
+
+
+# naive solution
+# def find_max_profit(prices):
+#   max_profit = float("-inf")
+#   while prices != []:
+#     bought = prices[0]
+#     for p in prices[1:]:
+#       profit = p - bought
+#       if profit > max_profit:
+#         max_profit = profit
+#     prices = prices[1:] # or prices.pop(0)
+
+#   print(max_profit)
+
+# def find_max_profit(prices):
+#   min_buy_price = prices[0]
+#   max_profit = prices[1] - min_buy_price
+
+#     for p in prices[1:]:
+#       profit = p - min_buy_price
+#       max_profit = max(profit, max_profit)
+#       # if profit > max_profit:
+#       #   max_profit = profit
+#       min_buy_price = min(p, min_buy_price)
+#       # if p < min_buy_price:
+#       #   min_buy_price = p
+#     prices = prices[1:] 
+
+#   print(max_profit)
+# for loop iterate everything after first index 
+# min_buy_price is the first index
+# look for a number lower than min_buy_price and subtract each index from it to see max profit
 
 print(find_max_profit([1050, 270, 1540, 3800, 2]))
 # current is 1050
