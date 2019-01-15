a7df7ce3f8513dfac376b5cba00e80898be3db52
Edwin Torres <edwin.torres.jr2@gmail.com>
2019-01-15 16:47:15 -0500

    added conditional to ensure ingredients list has all neccesary types

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -5,14 +5,18 @@ import math
 
 def recipe_batches(recipe, ingredients):
     ing_batches = []
+    if len(recipe) > len(ingredients):
+        print(0)
+        return 0
     for x in recipe:
         ing_batches.append(int(ingredients[x]/recipe[x]))
-    return (min(ing_batches))
+    print(min(ing_batches))
+    return min(ing_batches)
 
 
 if __name__ == '__main__':
-    # Change the entries of these dictionaries to test
-    # your implementation with different inputs
+        # Change the entries of these dictionaries to test
+        # your implementation with different inputs
     recipe = {'milk': 100, 'butter': 50, 'flour': 5}
     ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
     print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
