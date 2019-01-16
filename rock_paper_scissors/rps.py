db489468bb0c58a93084863beebd667546d69930
jmbradley <jmbradley808@gmail.com>
2019-01-16 11:39:24 -0500

    adding some pseudo to rps

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,20 @@
 
 import sys
 
+
 def rock_paper_scissors(n):
-  pass 
+    string = [['rock'], ['paper'], ['scissors']]
+    for x in string:
+      
+
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
+
+
+#### Enter number of rps games. Print the first position that man times. 
