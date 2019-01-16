69e85a4b438ba0dbe9d5dc009f1202531a81396a
lndubose <lndubose13@gmail.com>
2019-01-16 13:11:21 -0600

    Add lecture solution

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,39 +3,40 @@
 import sys
 
 
-def rock_paper_scissors(n):
-    allpos = []
-
-    def rec(i, item, clist=[]):
-        nonlocal allpos
-        if i == 0:
-            allpos = allpos + [clist]
-        elif i < 0:
-            return 0
-
-        clist = clist + [item]
-        rec(i-1, 'rock', clist)
-        rec(i-1, 'paper', clist)
-        rec(i-1, 'scissors', clist)
-    rec(n, 'rock')
-    rec(n, 'paper')
-    rec(n, 'scissors')
-    return allpos[::3]
-
 # def rock_paper_scissors(n):
 #     allpos = []
 
-#     def rec(i, clist=[]):
+#     def rec(i, item, clist=[]):
+#         nonlocal allpos
 #         if i == 0:
-#             allpos.append(clist)
-#             return
+#             allpos = allpos + [clist]
 #         elif i < 0:
 #             return 0
 
-#         for item in ['rock', 'paper', 'scissors']:
-#             rec(i-1, clist + [item])
-#         rec(n, [])
-#         return allpos
+#         clist = clist + [item]
+#         rec(i-1, 'rock', clist)
+#         rec(i-1, 'paper', clist)
+#         rec(i-1, 'scissors', clist)
+#     rec(n, 'rock')
+#     rec(n, 'paper')
+#     rec(n, 'scissors')
+#     return allpos[::3]
+
+# Solution from lecture
+def rock_paper_scissors(n):
+    outcomes = []
+    plays = ['rock', 'paper', 'scissors']
+
+    def find_outcomes(rl, result):
+        if rl == 0:
+            outcomes.append(result)
+            return
+
+        for item in plays:
+            find_outcomes(rl-1, result + [item])
+
+    find_outcomes(n, [])
+    return outcomes
 
 
 if __name__ == "__main__":
