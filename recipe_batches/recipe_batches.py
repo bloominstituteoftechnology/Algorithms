a62736740c0c6c10feb63f20b0efa860c2861c3b
Andrew <ghobrial.andrew@gmail.com>
2019-01-15 23:38:52 -0500

    completed recipe_batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,11 +3,23 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  batches = []
-  for i in recipe:
-    batches.append(ingredients.get(i, 0) // recipe[i])
-  return min(batches) 
-
+  # divide ingredients number by recipe number for each key
+  limitingFactor = 99999
+  for recipeIngredient, availableIngredient in zip(recipe.values(), ingredients.values()):
+    batchSize = availableIngredient / recipeIngredient
+    # if the quotient is less than the limitingFactor number, set limitingFactor to that new number
+    if batchSize < limitingFactor:
+      limitingFactor = batchSize
+  # if limitingFactor is less than 1, set it to 0
+  #return limiting factor without decimals
+  if limitingFactor < 1:
+    limitingFactor = 0
+  print(limitingFactor)
+ 
+recipe_batches(
+  { 'milk': 100, 'butter': 50, 'flour': 5 },
+  { 'milk': 138, 'butter': 48, 'flour': 51 }
+)
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
