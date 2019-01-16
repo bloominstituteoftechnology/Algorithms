aaec66d963ad89cc234498af6d18af1154c0af1d
sumi419 <sasgar04@gmail.com>
2019-01-15 19:56:40 -0800

    📄✂️

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python
 
 import sys
+from itertools import product
 
 def rock_paper_scissors(n):
-  pass 
-
+  print(n)
+  array = ['rock', 'paper', 'scissors']
+  if n == 1:
+    return [['rock'], ['paper'], ['scissors']]
+  else:
+    # print(f'This is the list === {list(product(array, repeat = n))}') #returns a tuple
+    combinations = [list(x) for x in list(product(array, repeat = n))]
+    return combinations
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
