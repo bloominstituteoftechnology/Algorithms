a51c40307f2b0b1a984f2535e47e8d549f324794
Vuk Radosavljevic <vukrado1@gmail.com>
2019-01-15 18:09:40 -0600

    Finished making change

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -2,8 +2,22 @@
 
 import sys
 
+#only works if you use coins [1, 5, 10, 25, 50]
+#if not get rid of options and use denominations instead and initialize the cache with amount + 1 0's and set cache[0] to 1
 def making_change(amount, denominations):
-  pass 
+  options = [5, 10, 25, 50]
+  cache = [1] * (amount + 1)
+
+  for coin in options:
+    for higher_amount in range(coin, amount + 1):
+      left_over = higher_amount - coin
+      cache[higher_amount] += cache[left_over]
+  
+  return cache[amount]
+
+
+  
+
 
 
 if __name__ == "__main__":
