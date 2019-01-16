ff0d178ad2c6585eb79c698d1b00a475d1ba4a6e
Jeanette <jeanfern555@gmail.com>
2019-01-16 13:16:36 -0600

    Added instructor's notes and solution for the small test for making_change.py

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -1,10 +1,25 @@
 #!/usr/bin/python
-
 import sys
 
-def making_change(amount, denominations):
-  pass 
+# Instructor Pseudo Code:
+# making_change(100, [1, 5]):
+#   making_change(100 - 1, [1, 5])
+#   making_change(100, [5])
 
+def making_change(amount, denominations):
+
+  # # Instructor's Solution for Small Test:
+  # if amount == 0:
+  #   return 1
+  # if amount < 0:
+  #   return 0
+  # if len(denominations) == 1:
+  #   return 1
+
+  # return making_change(amount - denominations[-1], denominations) + \
+  #   making_change(amount, denominations[:-1])
+  pass
+  
 
 if __name__ == "__main__":
   # Test our your implementation from the command line
