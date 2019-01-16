44f6c8a73dd3e879f0f73891b8f54c42afafaeaa
Jeanette <jeanfern555@gmail.com>
2019-01-16 12:05:40 -0600

    Added instructor solution to rps.py.

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -27,6 +27,22 @@ def rock_paper_scissors(n):
   helper(n, [])
   return output
 
+  # # Instructor Solution:
+  # outcomes = []
+  # plays = ['rock', 'paper', 'scissors']
+
+  # def find_outcomes(round_left, result):
+  #   if round_left == 0:
+  #       outcomes.append(result)
+  #       return
+
+  #   for play in plays:
+  #     find_outcomes(round_left - 1, result + [play])
+
+  # find_outcomes(n, [])
+  # return outcomes
+
+
 if __name__ == "__main__":
   if len(sys.argv) > 1:
     num_plays = int(sys.argv[1])
