00842c591ca81208a2667c367ac5301044002c25
jmbradley <jmbradley808@gmail.com>
2019-01-17 18:24:38 -0500

    adding count sort

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -2,16 +2,29 @@
 
 import sys
 
+# making_change(100):
+#     making_change(100-1)
+
+
 def making_change(amount, denominations):
-  pass 
+    if amount == 0:
+        return 1
+    if amount < 0:
+        return 0
+    if len(denominations) == 0:
+        return 0
+
+    return making_change(amount - denominations[-1], denominations) + \
+        making_change(amount, denominations[:-1])
 
 
 if __name__ == "__main__":
-  # Test our your implementation from the command line
-  # with `python making_change.py [amount]` with different amounts
-  if len(sys.argv) > 1:
-    denominations = [1, 5, 10, 25, 50]
-    amount = int(sys.argv[1])
-    print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
-  else:
-    print("Usage: making_change.py [amount]")
+    # Test our your implementation from the command line
+    # with `python making_change.py [amount]` with different amounts
+    if len(sys.argv) > 1:
+        denominations = [1, 5, 10, 25, 50]
+        amount = int(sys.argv[1])
+        print("There are {ways} ways to make {amount} cents.".format(
+            ways=making_change(amount, denominations), amount=amount))
+    else:
+        print("Usage: making_change.py [amount]")
