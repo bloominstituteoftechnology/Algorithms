d0ef9d9bf3afcd04c7acc6da5e5f0500b4195ca3
Kevin <brackkevin@gmail.com>
2019-01-18 16:36:47 -0600

    Getting closer to making_change, cache being overwritten?

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -17,13 +17,19 @@ def making_change(amount, denominations):
   else:
     poss_denom = [coin for coin in denominations if coin <= amount]
     # result = making_change(amount - greatest, denominations)
-    print(poss_denom)
+    # print(poss_denom)
     result = 0
-    for i in poss_denom:
-      print(i)
-      result += making_change(amount - i, denominations)
+    for index in range(0, len(poss_denom)):
+      curent_denom = poss_denom[index]
+      denoms_for_recursion = poss_denom[:index + 1]
+
+      result += making_change(amount - curent_denom, denoms_for_recursion)
+
     cache[amount] = result
+    print(cache)
     return result
+
+# print(making_change(10, [1,5,10,25,50]))
   
 
 if __name__ == "__main__":
