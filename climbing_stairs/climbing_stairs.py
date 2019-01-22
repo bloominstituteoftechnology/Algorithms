ced1344db2efe9f771ff705721aa935cebddf308
AM_Myrick <amyrick.820@gmail.com>
2019-01-22 13:55:22 -0500

    completes climbing_stairs.py

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -2,8 +2,23 @@
 
 import sys
 
-def climbing_stairs(n, cache=None):
-  pass 
+def climbing_stairs(n, cache={}):
+  cache[0] = 1
+  cache[1] = 1
+  cache[2] = 2
+  
+  if n in cache:
+    return cache[n]
+  else:
+    if cache.get(n-1, False) == False:
+      climbing_stairs(n-1)
+    if cache.get(n-2, False) == False:
+      climbing_stairs(n-2)
+    if cache.get(n-3, False) == False:
+      climbing_stairs(n-3)
+    else:
+      cache[n] = cache[n-1] + cache[n-2] + cache[n-3]
+      return climbing_stairs(n)
 
 
 if __name__ == "__main__":
