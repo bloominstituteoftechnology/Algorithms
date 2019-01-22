f3f2e51694dc704f70efe222601bc787c72bbc70
txiong000 <tkx422@gmail.com>
2019-01-22 16:02:33 -0600

    climbing stairs debugging

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -4,18 +4,24 @@ import sys
 
 
 def climbing_stairs(n, cache=None):
+
     
-    if (n == 1 or n == 0):
-      return 1
-    if n == 2:
-      return 2
-    else:
-      return climbing_stairs(n - 3) + climbing_stairs(n - 2) + climbing_stairs(n - 1)
+    place = [0] * (n)
+    print(place)
+    place[0] = 1
+    place[1] = 1
+    place[2] = 2
+    i = 3
+    while i <= n:
+        place[i] = climbing_stairs(i - 3) + climbing_stairs(i - 2) + climbing_stairs(i - 1)
+        i += 1
+    return place[n]
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_stairs = int(sys.argv[1])
-    print("There are {ways} ways for a child to jump {n} stairs.".format(ways=climbing_stairs(num_stairs), n=num_stairs))
-  else:
-    print('Usage: climbing_stairs.py [num_stairs]')
+    if len(sys.argv) > 1:
+        num_stairs = int(sys.argv[1])
+        print("There are {ways} ways for a child to jump {n} stairs.".format(
+            ways=climbing_stairs(num_stairs), n=num_stairs))
+    else:
+        print('Usage: climbing_stairs.py [num_stairs]')
