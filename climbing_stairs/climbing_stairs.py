2f30d84ff840c8443a4495bc115ce8b4942a00e2
Riley-Brown <riley@riley.gg>
2019-01-23 13:54:42 -0500

    added climbing stairs code

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -3,7 +3,10 @@
 import sys
 
 def climbing_stairs(n, cache=None):
-  pass 
+  if n == 0:
+    return 0
+  else:
+    return n + 1 
 
 
 if __name__ == "__main__":
@@ -12,3 +15,8 @@ if __name__ == "__main__":
     print("There are {ways} ways for a child to jump {n} stairs.".format(ways=climbing_stairs(num_stairs), n=num_stairs))
   else:
     print('Usage: climbing_stairs.py [num_stairs]')
+
+    print(climbing_stairs(3))
+
+    # visualize steps
+    # 4 steps = 1 big step, 3 steps and 1 step, 2 steps and 2 steps, 1 step and 3 steps, 4 steps
