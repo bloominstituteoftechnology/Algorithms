8e61e27e2eb1bf0c481ae214d68ef25b0389b8eb
Chris Cedeno <chrisrey95@gmail.com>
2019-02-20 18:43:43 -0500

    climbing_stairs

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -3,8 +3,20 @@
 import sys
 
 def climbing_stairs(n, cache=None):
-  pass 
+  # A Total of 3 steps has 4 different combinations
+  if n == 3:
+    return 4
+  # A Total of 2 steps has 2 different combinations
+  if n == 2:
+    return 2
+  # A Total of 1 and 0 steps has 1 different combinations
+  if n <= 1:
+    return 1
 
+  return climbing_stairs(n-1) + climbing_stairs(n-2) + climbing_stairs(n-3)
+
+
+print(f"Answer: {climbing_stairs(10)}")
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
