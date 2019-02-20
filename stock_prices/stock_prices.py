97bc7b8a91e1a922007f40632aacde504882897f
kyranmccann <kyran.mccann@gmail.com>
2019-02-20 15:42:08 -0800

    finish stock prices

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,8 +2,17 @@
 
 import argparse
 
+
 def find_max_profit(prices):
-  pass
+    min_price = prices[0]
+    max_profit = prices[1] - min_price
+
+    for i in range(1, len(prices)):
+        price = prices[i]
+        max_profit = max(price - min_price, max_profit)
+        min_price = min(price, min_price)
+
+    return max_profit
 
 
 if __name__ == '__main__':
