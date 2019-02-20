f7d5ab690d84d5a96c7a123c080a7cabb5173306
Carlos Lantigua <clantigua954@gmail.com>
2019-02-20 16:46:47 -0500

    getting all the right values and still can't pass a test lmao

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,14 +2,35 @@
 
 import argparse
 
+
 def find_max_profit(prices):
-  pass
+    profit = 0
+    loss = 0
+    difference = 0
+    while len(prices) > 0:
+        for num in range(len(prices)):
+            difference = prices[num] - prices[0]
+            if difference > profit:
+                profit = difference
+            if difference < profit:
+                loss = difference
+        del prices[0]
+        if profit == 0:
+            return loss
+        return profit
+
+
+myPrices = [100, 90, 80, 50, 20, 10, 200]
+print(find_max_profit(myPrices))
 
 
 if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+    # This is just some code to accept inputs from the command line
+    parser = argparse.ArgumentParser(
+        description='Find max profit from prices.')
+    parser.add_argument('integers', metavar='N', type=int,
+                        nargs='+', help='an integer price')
+    args = parser.parse_args()
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+    print("A profit of ${profit} can be made from the stock prices {prices}.".format(
+        profit=find_max_profit(args.integers), prices=args.integers))
