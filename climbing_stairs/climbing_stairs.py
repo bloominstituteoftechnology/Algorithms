cad9665d3e71d7125a90dcbfef4b2ebc5fb1ef93
Carlos Lantigua <clantigua954@gmail.com>
2019-02-20 15:12:34 -0500

    completed climbing stairs

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -2,13 +2,21 @@
 
 import sys
 
-def climbing_stairs(n, cache=None):
-  pass 
 
+def climbing_stairs(n, cache=None):
+    if n == 1:
+        return 1
+    if n <= 0:
+        return 0
+    return climbing_stairs(n - 1) + climbing_stairs(n - 2) + climbing_stairs(n - 3)
+
+
+print(climbing_stairs(5))
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_stairs = int(sys.argv[1])
-    print("There are {ways} ways for a child to jump {n} stairs.".format(ways=climbing_stairs(num_stairs), n=num_stairs))
-  else:
-    print('Usage: climbing_stairs.py [num_stairs]')
+    if len(sys.argv) > 1:
+        num_stairs = int(sys.argv[1])
+        print("There are {ways} ways for a child to jump {n} stairs.".format(
+            ways=climbing_stairs(num_stairs), n=num_stairs))
+    else:
+        print('Usage: climbing_stairs.py [num_stairs]')
