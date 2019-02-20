5f3d2700cdb79619eaca04f4387996498f056616
Hargobind Atwal <ethaipath@gmail.com>
2019-02-20 13:02:09 -0800

    Writing plan

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,6 +2,19 @@
 
 import argparse
 
+'''
+finding max profits by buying first and selling later
+takes in a list of numbers representing prices
+the max profit is going to be max_price - low_price
+
+if I step through the array keeping track of the lowest price I find
+and subtracting it from the max price then keep track of which max prices
+are the largest I should get the max profit possible
+
+
+'''
+
+
 def find_max_profit(prices):
   pass
 
