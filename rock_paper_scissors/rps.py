6baa74ff26096c41426f5c0c2df069825548245b
Kamry Bowman <kamry.bowman@gmail.com>
2019-02-20 15:32:57 -0700

    solution to rps.py

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,29 @@
 
 import sys
 
+
+OPTIONS = ['rock', 'paper', 'scissors']
+
+
+def helper(n, cache):
+    prior = cache.get(n - 1)
+    prior = prior if prior is not None else helper(n - 1, cache)
+    cache[n] = [[option] + perm
+                for option in OPTIONS for perm in prior]
+    return cache[n]
+
+
 def rock_paper_scissors(n):
-  pass 
+    if n == 0:
+        return [[]]
+    cache = {}
+    cache[0] = [[]]
+    return helper(n, cache)
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
