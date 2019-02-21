25326dca494b31f5e02f691d2b6648e28ac0f96a
qtn2932 <quan.nguyen199712@gmail.com>
2019-02-20 23:29:59 -0500

    finished stock price

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,7 +3,30 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  update_buy=True
+  buy_price=0
+  profit=0
+  count_negative=1
+  for i in range (len(prices)-1):
+    sell_price=prices[i+1]
+    if update_buy:
+      buy_price= prices[i]
+    if sell_price<buy_price:
+      update_buy=True
+      count_negative+=1
+    else:
+      update_buy=False
+      temp_profit= sell_price-buy_price
+      if temp_profit> profit:
+        profit=temp_profit
+  if count_negative==len(prices):
+    return prices[len(prices)-1]- prices[len(prices)-2]
+  else:
+    return profit
+      
+      
+    
+
 
 
 if __name__ == '__main__':
