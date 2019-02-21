43dc9368afb7a4b384d01bca667ebb7915a24d33
Kamry Bowman <kamry.bowman@gmail.com>
2019-02-20 17:41:40 -0700

    trying to figure out making_change

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -2,16 +2,42 @@
 
 import sys
 
+
 def making_change(amount, denominations):
-  pass 
+    sort_denoms = sorted(denominations)
+    return helper(amount, sort_denoms)
+
+
+def helper(amount, denominations, cache=None):
+    if amount < 0:
+        return 0
+    if amount == 0:
+        return 1
+
+    else:
+        total = 0
+
+        while denominations:
+            denom = denominations.pop()
+            print(denom)
+            remaining = amount - denom
+            while remaining > -1:
+                plus = helper(remaining, denominations[:])
+                total += plus
+                remaining = amount - denom
+
+    return total
 
 
 if __name__ == "__main__":
-  # Test our your implementation from the command line
-  # with `python making_change.py [amount]` with different amounts
-  if len(sys.argv) > 1:
-    denominations = [1, 5, 10, 25, 50]
-    amount = int(sys.argv[1])
-    print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
-  else:
-    print("Usage: making_change.py [amount]")
+    # Test our your implementation from the command line
+    # with `python making_change.py [amount]` with different amounts
+    if len(sys.argv) > 1:
+        denominations = [1, 5, 10, 25, 50]
+        amount = int(sys.argv[1])
+        print("There are {ways} ways to make {amount} cents.".format(
+            ways=making_change(amount, denominations), amount=amount))
+    else:
+        print("Usage: making_change.py [amount]")
+
+print(making_change(2, [1, 5, 10, 25, 50]))
