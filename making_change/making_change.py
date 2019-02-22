5fbd48941f6a8094b692b325168c6204de35f7fb
qtn2932 <quan.nguyen199712@gmail.com>
2019-02-21 19:06:27 -0500

    finished making change

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -3,9 +3,20 @@
 import sys
 
 def making_change(amount, denominations):
-  pass 
+  if amount ==0:
+    return 1
+  elif amount<0:
+    return 0
+  elif len(denominations)==0 and amount>0:
+    return 0
+  else:
+    use_coin= denominations[-1]
+    return sum(making_change(amt, denominations[0:-1])
+    for amt in range(amount, -1, -use_coin)
+    )
+    #return making_change(amount-denominations[-1], denominations[0:-1])+ making_change( amount-denominations[-2],denominations[0:-2]) 
 
-
+print(making_change(10, [1,5,10,25,50]))
 if __name__ == "__main__":
   # Test our your implementation from the command line
   # with `python making_change.py [amount]` with different amounts
