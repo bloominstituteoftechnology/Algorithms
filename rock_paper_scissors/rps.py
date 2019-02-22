c318293019605ed7e4e59086eaaf6c6c76824de9
qtn2932 <quan.nguyen199712@gmail.com>
2019-02-21 19:56:45 -0500

    finished rps

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,8 +3,23 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  #start with a list of choice
+  choices=['rock','paper','scissors']
+  #possible outcome will be store here
+  psb=[]
+  # def to determine the permutation
+  def permu (which_round, result):
+    #at round 0, which means no round left, append to psb
+    if which_round==0:
+      psb.append(result)
+    else:
+      # start going through choices and pick on to combine with the current index choice
+      for choice in choices:
+        permu(which_round-1, result+[choice])
+  permu(n,[])
+  return psb
 
+print(rock_paper_scissors(2))
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
