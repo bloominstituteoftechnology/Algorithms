48e1c8897c4a3082c09d3542586d3bd8dbd4aa6f
Troy Amlee <troyamlee19@gmail.com>
2019-02-21 18:35:02 -0600

    [chore] clean up

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -20,20 +20,6 @@ def rock_paper_sci(n):
 		return [rock, paper, scissors]
 	else:
 
-		# for i in range(3**n//3):
-		# 	r.append(rock)
-		# for i in range(3**n//3):
-		# 	p.append(paper)
-		# for i in range(3**n//3):
-		# 	s.append(scissors)
-		# print(r)
-		# print(p)
-		# print(s)
-
-		# print(rps)
-		
-		
-
 		def rps_rounds(n, rps_list):
 			new_list = []
 			r = []
@@ -47,14 +33,10 @@ def rock_paper_sci(n):
 					new_list.append(i + rock)
 					new_list.append(i + paper)
 					new_list.append(i + scissors)
-				# print(new_list)
+
 				rps_rounds(n-1, new_list)
 
-
-			# rps_rounds(n-1,rps_list)
-		
-
 			
-		print(rps_rounds(n,empty_list))
+		rps_rounds(n,empty_list)
 
-print(rock_paper_sci(2))
+print(rock_paper_sci(1))
