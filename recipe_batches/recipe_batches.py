76109387ac8b6a75163a1b2f4da9257aa30663d9
Nikita Thomas <nikcxz@gmail.com>
2019-03-26 08:20:25 -0400

    recipe batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,12 +3,21 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
 
+  count = 0
+
+  for name in recipe.keys():
+    if name not in ingredients:
+      return 0
+    tempCount = int(ingredients[name] / recipe[name])
+    if count == 0 or tempCount < count:
+      count = tempCount
+
+  return count
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
   # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+  recipe = { 'milk': 2}
+  ingredients = { 'milk': 200 }
   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
