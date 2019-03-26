500908c0a05514551c905d062391fcc76fb8d182
yzhukovs <yzhukovs@bumnetworks.com>
2019-03-26 19:01:46 -0400

    recipes

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,8 +3,17 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
-
+  min_ingridients = None
+  for i, min_amount in recipe.items():
+    #assign 0 for ingridients that are not there
+    amount_we_have = ingredients.get(i, 0)
+    amount_can_have = amount_we_have // min_amount
+    if min_amount != None:
+      min_amount = min(min_ingridients, amount_can_have)
+    else:
+      min_amount = amount_can_have
+  return min_amount        
+  
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
