5aaf704af13696ed9eba70b3bfb1d7d1321eeeda
yzhukovs <yzhukovs@bumnetworks.com>
2019-03-27 12:10:57 -0400

    rps

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,8 +3,17 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
-
+  if n <= 0:
+    return [[]]
+    #base case 
+  else:
+    first_result = rock_paper_scissors(n-1)
+    new_result = []
+    for r in first_result:
+      new_result.append(r + ["rock"])
+      new_result.append(r + ["paper"])
+      new_result.append(r + ["scissors"])
+    return new_result  
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
