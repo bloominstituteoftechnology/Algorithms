54e68302371be4701a9fc562b9fb7969bd3f3e9b
BrandCam <BrandCam>
2019-04-01 19:09:14 -0500

    cookies done

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,7 +6,17 @@ import sys
 # a solution that is more efficient than the naive 
 # recursive solution
 def eating_cookies(n, cache=None):
-  pass
+  if (n == 0) or (n == 1):
+    return 1
+  elif n == 2:
+    return 2
+  elif cache and cache[n] > 0:
+    return cache[n]
+  else:
+    if not cache:
+      cache = {i: 0 for i in range(n+1)}
+    cache[n] = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+    return cache[n]
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
