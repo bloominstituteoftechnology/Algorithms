bcdf268ea3939a59ca851955dd07a8b76e2f1d45
Thomas Claydon <tomclaydon102@gmail.com>
2019-05-01 15:59:50 -0400

    Completed Iterative Stock_Prices

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,8 +3,23 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  profit = 0
+  min = prices[0]
+  for i in prices:
+    val = i
+    print(f'val: {val}')
+    if val < min:
+      min = val
+      if val == prices[len(prices)-1] and profit == 0:
+        profit = profit - min
+    elif val > min and profit < val - min:
+      profit = val - min
+    print(f'min: {min}')
+    print(f'profit: {profit}')
+  return profit 
 
+# find_max_profit([1050, 270, 1540, 3800, 2])
+# find_max_profit([100, 90, 80, 50, 20, 10])
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
