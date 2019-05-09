421a78b657b774a44860c6d80de59ad6b241c63f
matt0418 <mfeldma7@gmail.com>
2019-05-09 17:14:36 -0400

    completed rps

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,9 +2,23 @@
 
 import sys
 
-def rock_paper_scissors(n):
-  pass 
 
+def rock_paper_scissors(n):
+  choices = [['rock'], ['paper'], ['scissors']]
+  combos = []
+  if n == 0:
+    return [[]]
+  if n == 1:
+    return choices
+  else:
+    game = rock_paper_scissors(n-1)
+    for i in range(0,3):
+      for j in range(0,3**(n-1)):
+        combos = combos + [choices[i] + game[j]]
+  return combos
+
+
+print(rock_paper_scissors(2))
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
