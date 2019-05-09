335aea3cfd6fa0f0fc64d7333301971f438660fc
matt0418 <mfeldma7@gmail.com>
2019-05-09 16:50:53 -0400

    added cacheing to cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,7 +5,7 @@ import sys
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
-def eating_cookies(n, cache=None):
+def eating_cookies(n, yeet=None):
   if n <= 0:
     return 1
   elif n == 1 :
@@ -14,8 +14,13 @@ def eating_cookies(n, cache=None):
     return 2
   elif n == 3:
     return 4
+  elif yeet and yeet[n] > 0:
+    return yeet[n]
   else:
-    return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+    if not yeet:
+      yeet = {i: 0 for i in range(n+1)}
+    yeet[n] = eating_cookies(n-1, yeet) + eating_cookies(n-2, yeet) + eating_cookies(n-3, yeet)
+    return yeet[n]
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
