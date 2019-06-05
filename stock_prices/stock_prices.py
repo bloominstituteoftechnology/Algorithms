0c149edceb127691633a95a00462a30095eb6d10
Carlos Gutierrez <chgutier@gmail.com>
2019-06-05 17:09:48 -0500

    Done with recipe_batches and stock_prices

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,8 +2,24 @@
 
 import argparse
 
+# "prices" is an array/list of a day's stock price variation (positive ints)
+# function should return "max_profit" from buy & sell that day
+# must buy before selling
+# "max_profit" can be negative int
 def find_max_profit(prices):
-  pass
+    # to keep track of highest profits:
+    max_profit = None
+    for i in range (0, len(prices)):
+        # substracting each price to each one ahead
+        # and keeping only if greater than "max_profit"
+        for j in range(i+1, len(prices)):
+            #profit -> prices[j]-prices[i] == sell_price-buy_price
+            profit = (prices[j]-prices[i])
+            # make "max_profit" = to "profit" in first iteration
+            # or when "profit" is greater than "max_profit"
+            if i == 0 or profit > max_profit:
+                max_profit = profit
+    return max_profit
 
 
 if __name__ == '__main__':
