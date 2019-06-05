d75ccdd088a7b2ccec94792484a4ed92672cf286
Michael Beck <brit228@gmail.com>
2019-06-05 19:14:37 -0400

    Finished recipe_batches, rps, and stock_prices

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,8 +3,14 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
-
+  map_dict = {0: 'rock', 1: 'paper', 2: 'scissors'}
+  out = []
+  for i in range(3**n):
+    temp = []
+    for j in range(n):
+      temp.insert(0, map_dict[(i // 3 ** j) % 3])
+    out.append(temp)
+  return out
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
