bff4d44e312bc5d05cafdf2cbe37a73abbe6bc44
Tony Tia <webmaster@tonytia.com>
2019-06-05 15:18:33 -0700

    stock price

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,9 +1,36 @@
 #!/usr/bin/python
+'''
+Understanding: 
+Input: num of input price follow by the number of price.
+Ouput: max profit output price
 
+Plan:
+if curPrice < nextPrice, then uptrend maxprofit = nextPrice - minPrice
+if curPrice > nextPrice, then downtrend minPrice = curPrice
+
+
+max profit = nextPrice - minPrice
+
+
+'''
 import argparse
 
 def find_max_profit(prices):
-  pass
+  minPrice=0
+  max_profit = 0
+  
+  print(prices)
+  for i in range(len(prices)-1):
+    if prices[i] < prices[i+1] :
+      max_profit = prices[i+1] - minPrice
+    else:
+      minPrice =  prices[i+1]
+  
+  if max_profit == 0:
+    max_profit = - minPrice
+  
+  return max_profit
+
 
 
 if __name__ == '__main__':
