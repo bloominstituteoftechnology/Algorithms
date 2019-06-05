ae5d6e68f28941bc0b4fb5af41991d62a69b0bf4
Tony Tia <webmaster@tonytia.com>
2019-06-05 16:26:37 -0700

    fixed stock price

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -5,33 +5,33 @@ Input: num of input price follow by the number of price.
 Ouput: max profit output price
 
 Plan:
-if curPrice < nextPrice, then uptrend maxprofit = nextPrice - minPrice
-if curPrice > nextPrice, then downtrend minPrice = curPrice
+if prevPrice > curPrice && curPrice < nextPrice => buyPrice = curPrice
+if prevPrice < curPrice && curPrice > nextPrice => sellPrice = curPrice
 
-
-max profit = nextPrice - minPrice
+maxProfit = sell - buy
 
 
 '''
 import argparse
 
 def find_max_profit(prices):
-  minPrice=0
+  buyPrice=0
+  sellPrice =0
   max_profit = 0
   
   print(prices)
   for i in range(len(prices)-1):
-    if prices[i] < prices[i+1] :
-      max_profit = prices[i+1] - minPrice
-    else:
-      minPrice =  prices[i+1]
-  
-  if max_profit == 0:
-    max_profit = - minPrice
-  
+    if  buyPrice == 0 and prices[i] < prices[i+1]:
+      buyPrice = prices[i]
+    elif buyPrice != 0 and prices[i] > prices[i+1]:
+      if sellPrice < prices[i]:
+        sellPrice =  prices[i]
+
+  if buyPrice == 0:
+    buyPrice =  prices[-1]
+  max_profit = sellPrice - buyPrice
+
   return max_profit
-
-
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
