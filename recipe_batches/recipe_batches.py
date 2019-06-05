f6311eb6611923d30ad8af7a7e45f1e56d9e966c
Thomas Hessburg <44884308+TomHessburg@users.noreply.github.com>
2019-06-05 16:14:42 -0400

    commented recipe batches. was able to do it with a single for loop

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -4,23 +4,25 @@ import math
 
 
 def recipe_batches(recipe, ingredients):
+    # holds amount of each ingredient/how many batches we can make with it
     batches = []
 
+    # checks to see that we have the same ingredients as we do keys
     if recipe.keys() != ingredients.keys():
         return 0
     else:
+        # we only have to loop once since we now know that we have all of the sam ingredients
         for key in ingredients:
+            # if we have enough of a specific ingredient, how many batches could we make with it?
             if ingredients[key] >= recipe[key]:
                 batches.append(ingredients[key]//recipe[key])
-
+            # if the answer is 0, just return 0 now
             else:
                 return 0
+    # the smallest number within the batches list is the max amount of the recipe we can make
     print(min(batches))
     return min(batches)
 
-
-# recipe_batches({'milk': 100, 'flour': 4, 'sugar': 10, 'butter': 5}, {
-#                'milk': 1288, 'flour': 9, 'sugar': 95})  # 0
 
 recipe_batches({'milk': 100, 'butter': 50, 'cheese': 10}, {
                'milk': 198, 'butter': 52, 'cheese': 10})  # 1
