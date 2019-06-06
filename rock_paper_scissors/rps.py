d519dacb033700d762f2a9d6e2765bc8c3edf75d
Carlos Gutierrez <chgutier@gmail.com>
2019-06-06 18:06:34 -0500

    Incoplete code for rps after trying for a while

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,7 +3,22 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+    # since we have 3 strings as n increases
+    # the number of strings potentially increases
+    # 3^n
+    permutations = len(rps)**n
+    rps = ['rock', 'paper', 'scissors']
+
+    # maybe we could have an empty string for each iteration
+    # and then popupulate it with the right string according to index???
+    plays = []
+    for i in range(0, len(rps)):
+        for j in range(n):
+            plays.append([])
+            plays[].append()
+            # plays.append(rps[i])
+        
+    return plays
 
 
 if __name__ == "__main__":
