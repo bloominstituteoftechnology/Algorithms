aa439ed1676cade4afb75c536f9b90052d572fe4
Samir Gadkari <samir.gadkari@gmail.com>
2019-06-06 15:48:05 -0700

    Rock-Paper_Scissors partial code

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,42 @@
 
 import sys
 
+
 def rock_paper_scissors(n):
-  pass 
+
+    rps = ['rock', 'paper', 'scissors']
+    selected = []
+
+    def loop(n, plays):
+
+        nonlocal rps, selected
+
+        # print(locals())
+        if n < 0:
+            plays.append(selected)
+            return None
+
+        for rotate in range(3):
+            for selector in range(3):
+                selected.append(rps[selector])
+                print('selected:', selected)
+                loop(n - 1, plays)
+
+                selected = selected[:-1]
+
+            rps = rps[1:] + [rps[0]]
+
+        return plays
+
+    if n == 0:
+        return [[]]
+    else:
+        return loop(n - 1, [])
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
