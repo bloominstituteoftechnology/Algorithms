62407d448374e586b7899f6e0f1c90899c870a4c
Mark Oliver <mark.c.oliver@gmail.com>
2019-06-05 23:47:17 -0400

    Rock Paper Scissors, still working on Eating Cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,14 +3,33 @@
 import sys
 
 # The cache parameter is here for if you want to implement
-# a solution that is more efficient than the naive 
+# a solution that is more efficient than the naive
 # recursive solution
+
+
+
+
 def eating_cookies(n, cache=None):
-  pass
+    if n == 0:
+        return 1
+    return 2 ** (n - 1)
+
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_cookies = int(sys.argv[1])
-    print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
-  else:
-    print('Usage: eating_cookies.py [num_cookies]')
+    if len(sys.argv) > 1:
+        num_cookies = int(sys.argv[1])
+        print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(
+            ways=eating_cookies(num_cookies), n=num_cookies))
+    else:
+        print('Usage: eating_cookies.py [num_cookies]')
+
+# 1: 1   
+# 2: 2  fib + 1
+# 3: 4  fib()
+# 4: 7?
+# 5: 13  fib(n+2)
+#10: 274
+
+# 0  1  2  3  4  5  6  7   8
+# 0, 1, 1, 2, 3, 5, 8, 13, 21,
+4 + 3 + 3
--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,67 @@
 
 import sys
 
+
+
+# Python3 Program to convert decimal to  
+# any given base 
+  
+# To return char for a value. For example  
+# '2' is returned for 2. 'A' is returned  
+# for 10. 'B' for 11 
+def reVal(num): 
+  
+    if (num >= 0 and num <= 9): 
+        return chr(num + ord('0')); 
+    else: 
+        return chr(num - 10 + ord('A')); 
+  
+# Utility function to reverse a string 
+def strev(str): 
+  
+    len = len(str); 
+    for i in range(int(len / 2)): 
+        temp = str[i]; 
+        str[i] = str[len - i - 1]; 
+        str[len - i - 1] = temp; 
+# Function to convert a given decimal  
+# number to a base 'base' and 
+def fromDeci(base, inputNum): 
+  
+    res = ""
+    index = 0; # Initialize index of result 
+  
+    # Convert input number is given base  
+    # by repeatedly dividing it by base  
+    # and taking remainder 
+    while (inputNum > 0): 
+        res+= reVal(inputNum % base); 
+        inputNum = int(inputNum / base); 
+  
+    # Reverse the result 
+    res = res[::-1]; 
+  
+    return res
+
+
 def rock_paper_scissors(n):
-  pass 
+    r = []
+    ro = [('rock', 0), ('paper', 1), ('scissors', 2)]
+    l3n = len(str(fromDeci(3,3**n))) - 1 # max number is 1 less
+    # print('l3n', l3n)
+    for i in range(3 ** n):
+        b3 = str(fromDeci(3, i)).zfill(l3n)
+        # print('b3', b3)
+        lr = []
+        for j in range(n):
+            lr.append(ro[int(b3[j])][0])
+        r.append(lr)
+    return r
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
