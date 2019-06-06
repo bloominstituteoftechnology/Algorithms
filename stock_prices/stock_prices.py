791b4c3ce088afba04f8a4cb03ff6c97176b2a69
Manjula Mishra <manjula.mishra@gmail.com>
2019-06-05 19:45:49 -0700

    first problem solved after 3 hours of thinking and working on the code

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,10 +1,37 @@
 #!/usr/bin/python
 
 import argparse
+"""
+Problem:
+Find the maximum profits in a stock price pick
+
+Understand the problem:
+----------------------
+You can sell only in future
+you can not reverse back in transcation (can't go back in time)
+integers only
+you want to maximize the profits
+
+anlayze:
+--------
+"""
 
 def find_max_profit(prices):
-  pass
-
+  #profit
+  max_value = 0
+  max_value_location = 0
+  min_value = 1e1000
+  # get the max value even the last element matters
+  for i in range(1, len(prices)):
+    if prices[i] > max_value:
+      max_value = prices[i]
+      max_value_location = i   
+  # get the min value when buying the stock
+  # the last element wouldn't matter because we wouldn't be able to sell it
+  for j in range(0, max_value_location):
+    if min_value > prices[j]:
+      min_value = prices[j] 
+  return max_value-min_value
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
