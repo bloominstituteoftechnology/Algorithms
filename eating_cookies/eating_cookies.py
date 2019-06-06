f99aab3d77e5df8ba2ba15d64374fc3aef22ba9d
Mark Oliver <mark.c.oliver@gmail.com>
2019-06-06 01:13:19 -0400

    Eating Cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,18 +1,42 @@
 #!/usr/bin/python
 
 import sys
+import pickle
 
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive
 # recursive solution
 
 
-
-
 def eating_cookies(n, cache=None):
-    if n == 0:
-        return 1
-    return 2 ** (n - 1)
+    try:
+        with open('eating_cookies.cache', 'rb') as F:
+            cache = pickle.load(F)
+            if len(cache) > n:
+                print('using cache')
+                return cache[n]  
+    except:
+        pass       
+
+    acc = [1, 1, 2]
+    if n < 3:
+        return acc[n]
+    if cache != None:
+        acc = cache[-3:]
+        n -= len(cache)
+    else:
+        n -= 2
+        cache = [1, 1, 2]
+
+    for i in range(n):
+        s = sum(acc)
+        acc[0] = acc[1]
+        acc[1] = acc[2]
+        acc[2] = s
+        cache.append(s)       
+    with open('eating_cookies.cache', 'wb') as F:    
+        pickle.dump(cache, F) 
+    return acc[2]
 
 
 if __name__ == "__main__":
@@ -22,14 +46,3 @@ if __name__ == "__main__":
             ways=eating_cookies(num_cookies), n=num_cookies))
     else:
         print('Usage: eating_cookies.py [num_cookies]')
-
-# 1: 1   
-# 2: 2  fib + 1
-# 3: 4  fib()
-# 4: 7?
-# 5: 13  fib(n+2)
-#10: 274
-
-# 0  1  2  3  4  5  6  7   8
-# 0, 1, 1, 2, 3, 5, 8, 13, 21,
-4 + 3 + 3
