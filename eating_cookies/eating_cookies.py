dc063b2074f5e4201756dfe2ab7fd6df4b1cb915
Jor_Poon <jormpoon@gmail.com>
2019-06-06 18:28:33 -0400

    finished eating cookies and tests passed with eating cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,12 +1,50 @@
 #!/usr/bin/python
 
+'''
+UNDERSTANDING:
+
+- Can eat 0-3 cookies per time
+- base case is 0, 1, 2, 3
+
+Pattern:
+result of the Nth case is the sum of 3 previous result.
+
+Similar to fibonacci sequence but instead of 2 previous numbers it is the 3 previous numbers
+'''
+
 import sys
 
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
 def eating_cookies(n, cache=None):
-  pass
+
+  cache = {}   # Memoization
+
+  def fib_inner(n):
+     # O(n)
+    nonlocal cache
+
+  # TODO make sure n is non-negative
+
+    # if n == None:
+    #   return 1
+
+    if n == 0:
+      return 1
+
+    if n == 1:
+      return 1
+        
+    if n < 0:
+      return 0
+
+    if n not in cache:
+      cache[n] = fib_inner(n-1) + fib_inner(n-2) + fib_inner(n-3)
+
+    return cache[n]
+
+  return fib_inner(n)
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
