356b2562d5800f7755315497d15393445b3d263f
vy3191 <venkyafhs@gmail.com>
2019-06-12 00:51:46 -0400

    Completed stock_prices assignment

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,10 +1,33 @@
 #!/usr/bin/python
 
 import argparse
-
+                      # list of 4
 def find_max_profit(prices):
-  pass
+  #Find the max value in the prices list
+  sell_value = max(prices)
+  # Get the index of max/sell in the prices list
+  sell_index = prices.index(sell_value)
+  # Slice the prices list after selling value(Min value) 
+  # if the sell value is the beginning of the list then ignore it consider the value after it 
+  if sell_index == 0:
+    sell_value = max(prices[1:])
+    sell_index = prices.index(sell_value)
 
+  sell_index = prices.index(sell_value)  
+  sliced_list = prices[0:sell_index]  
+  
+  print(sliced_list)
+  #Find the maximum value in the prices list after min value
+  buy_value = min(sliced_list) 
+  # print(sell_value)
+  # Find the profit by subtracting the min from max price
+  profit = sell_value - buy_value
+  
+  #return the profit
+  return profit
+  # print(profit)
+
+# find_max_profit([1050, 270, 1540, 3800, 2])
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
