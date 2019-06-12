77a3f45ae791ca28c335aa8562782b45adca1cc8
ills5771 <ilyayelizarov1@gmail.com>
2019-06-12 15:05:34 -0700

    adds algo for recipe batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -4,7 +4,27 @@ import math
 
 
 def recipe_batches(recipe, ingredients):
-    pass
+    batches = []
+    for rec, recAmount in recipe.items():
+        for ing, ingAmount in ingredients.items():
+            # if recipe key and ing key are identical and their lengths are identical, proceed
+            if rec == ing and len(recipe) == len(ingredients):
+                # add to empty array the quotient of ing value divided by recipe value, this gives us the amount of batches for each ingredient in the recipe
+                batches.append(ingAmount//recAmount)
+                # if theres a 0 in the list, we know that we don't have enough of an ingredient for our recipe
+                if 0 in batches:
+                    return 0
+                # check to see if all values in the bathes list are identical
+                elif all(i == batches[0] for i in batches):
+                    # if its true, we know we equal amount of ingredients for the recipe and we'll return that value
+                    return batches[0]
+                # if no zeros and no identical numbers exist in batches, return the lowest value in the list
+                else:
+                    # this lowest value is the minimun number of batches we can make
+                    return min(batches)
+            else:
+                return 0  # if length of recipes doesn't equal length of ingredients, we can't make any batches
+    return batches
 
 
 if __name__ == '__main__':
