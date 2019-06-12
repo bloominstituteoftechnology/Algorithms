a0cddd5e80725df8b25348b255e71a033fdc3d23
FreeKeyBoy <jjhutton81@gmail.com>
2019-06-11 19:14:38 -0700

    Wrote a function for num_ways. It passed tests, but I didn't expect it to, because I manually set the range. I'll wait for feedback, and see if I need to adjust the function.

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -9,12 +9,27 @@ import sys
 # Same goes for neg numbers.
 # Should take in integer, and return integer.
 
+
 def eating_cookies(n, cache=None):
     if n == 0:
         return 1
     elif n < 0:
         return 0
 
+    # Number of cookies cookie monster can eat is:
+    # 0, 1, 2, 3. Ignore 0 - it doesn't do anything.
+    num_ways = 0
+    for i in range(1, 4):
+        if cache is not None:
+            if cache[n - i] == 0:
+                cache[n - i] = eating_cookies(n - i, cache)
+            num_ways += cache[n - i]
+        else:
+            num_ways += eating_cookies(n - i)
+
+    if cache is not None:
+        cache[n] = num_ways
+    return num_ways
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
