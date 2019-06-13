0ac5c7bf4a0fd6ec17041f5b7f5a62525f9b82c4
Jason Hedrick <jasonleehedrick@gmail.com>
2019-06-13 01:14:03 -0700

    Changed the return variable name in make_change to make more sense. Added in some comments as to what the list selection is doing during the recursion.

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -3,15 +3,17 @@
 import sys
 
 def making_change(amount, denominations):
-  numCoins = 0
+  numWaysChange = 0
   if amount == 0:
     return 1
   elif amount < 0:
     return 0
 
   for n in range(0, len(denominations)):
-    numCoins += making_change(amount - denominations[n], denominations[n:])
-  return numCoins
+    # Limit the number of recursive calls each denomination can make
+    # The penny recursion shouldn't make nickel recursions but the nickel recursion can make both nickel and penny recursions.
+    numWaysChange += making_change(amount - denominations[n], denominations[n:])
+  return numWaysChange
 
 if __name__ == "__main__":
   # Test our your implementation from the command line
