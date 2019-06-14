cdfe31ae0e537cf91358b8c7c5efd43735bbe840
Alex Martin <atonymartin20@yahoo.com>
2019-06-13 21:00:05 -0500

    Completed rock paper scissors

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -3,8 +3,20 @@
 import sys
 
 def making_change(amount, denominations):
-  pass 
+  #base cases
+  if amount < 5 and amount >=0:
+    return 1
 
+  elif amount < 0:
+    return 0
+
+  # recursive case - not handling all cases correctly
+  else:
+    count = 0
+    for c in denominations:
+      count += making_change(amount - c, denominations)
+    # return making_change(amount - 1, coin) + making_change(amount - 5, coin) + making_change(amount - 10, coin) + making_change(amount - 25, coin) + making_change(amount - 50, coin)
+    return count
 
 if __name__ == "__main__":
   # Test our your implementation from the command line
@@ -15,3 +27,11 @@ if __name__ == "__main__":
     print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
   else:
     print("Usage: making_change.py [amount]")
+
+    #penny = n - 1
+    # nickle = n - 5
+    # dime = n - 10
+    # quarter = n - 25
+    # 1/2 dollar = n - 50
+    # n = 0 return 0
+    # n = 1 return 1
