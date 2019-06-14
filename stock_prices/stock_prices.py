da4abd10e5eee17039a3fd42420941a297f20eea
Alex Martin <atonymartin20@yahoo.com>
2019-06-13 21:35:26 -0500

    stock prices completed

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,18 +3,14 @@
 import argparse
 
 def find_max_profit(prices):
-  print(prices)
-
-# def find_max_profit(prices):
-#   # current_min_price_so_far and max_profit_so_far
-#   min_price = prices[0]
-#   max_profit = prices[1] - min_price
-#   for i in range(len(prices)):
-#     for j in range(i + 1,len(prices)):
-#       if  prices[j] - prices[i] > max_profit:
-#         max_profit = prices[j] - prices[i]
-  
-#   return max_profit
+  # 2 for loops
+  min_price = prices[0]
+  max_profit = prices[1] - min_price
+  for i in range(len(prices)):
+    for j in range(i + 1,len(prices)):
+      if prices[j] - prices[i] > max_profit:
+        max_profit = prices[j] - prices[i]
+  return max_profit
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
@@ -23,25 +19,3 @@ if __name__ == '__main__':
   args = parser.parse_args()
 
   print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
-
-'''
- # Stock Prices
-
-You want to write a bot that will automate the task of day-trading for you while you're going through Lambda. You decide to have your bot just focus on buying and selling Amazon stock. 
-
-Write a function `find_max_profit` that receives as input a list of stock prices. Your function should return the maximum profit that can be made from a single buy and sell. You must buy first before selling; no shorting is allowed here.
-
-For example, `find_max_profit([1050, 270, 1540, 3800, 2])` should return 3530, which is the maximum profit that can be made from a single buy and then sell of these stock prices. 
-
-## Testing
-
-Run the test file by executing `python test_stock_prices.py`.
-
-You can also test your implementation manually by executing `python stock_prices.py [integers_separated_by_a_single_space]`
-
-## Hints
-
- For this problem, we essentially want to find the maximum difference between the smallest and largest prices in the list of prices, but we also have to make sure that the max profit is computed by subtracting some price by another price that comes _before_ it; it can't come after it in the list of prices. 
-
- So what if we kept track of the `current_min_price_so_far` and the `max_profit_so_far`? 
- '''
