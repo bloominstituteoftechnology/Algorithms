094831533d8575ed1cadf10200870b6b86a8dc0a
Kendra Williams <kmonique.williams@gmail.com>
2019-06-14 22:06:59 -0400

    completed rock paper scissors

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,7 +3,22 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  #holds potential plays
+  rps = ["rock", "paper", "scissors"]
+  #holds results
+  result = []
+
+  def helper(action, playsLeft):
+    #base case for 0
+    if playsLeft == 0:
+      return result.append(action)
+    else:
+      #for each potential play, pass the list and the current play n times
+      for play in rps:
+        helper([*action, play], playsLeft - 1)
+
+  helper([], n)
+  return result
 
 
 if __name__ == "__main__":
