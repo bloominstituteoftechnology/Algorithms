372f74f6e97562aa3ca6d38b73fe54674b0d323e
Phil Hart <philhart23@gmail.com>
2019-07-17 13:59:11 -0600

    stock price test passing, return in wrong place

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,19 +3,18 @@
 import argparse
 
 def find_max_profit(prices):
-  max_profit = 0 
+  max_profit = 0
+  for i, price in enumerate(prices):
+    for j in range (i + 1, len(prices)):
+      current_profit = prices[j] - price
+      if max_profit == 0 or current_profit > max_profit:
+          max_profit = current_profit
+  return max_profit
+ 
+
+print(find_max_profit([100, 450, 2500, 56, 7]))
   
 
-  for i in range(0, len(prices) -1):
-
-    for j in range (i + 1, len(prices)):
-      current_profit = prices[j] - prices[i]
-      if current_profit > max_profit:
-         max_profit = current_profit
-
-    return max_profit
-
-find_max_profit([100, 450, 2500, 56, 7])
 
 
 if __name__ == '__main__':
