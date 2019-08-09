7af74b26f77e443a8f90eebf9c1c71fb891ef38e
Tony Tia <webmaster@tonytia.com>
2019-08-09 12:04:03 -0700

    fixed len(a) -1

--- misc/shiftedArr.py
+++ misc/shiftedArr.py
@@ -4,7 +4,7 @@ def shifted_arr_search(shiftArr, num):
     # pivot = 0 when whole array is in ascending order
     # or [5,4,0,1,2,3], num=1
     if (pivotIdx == 0) or num < shiftArr[0]:
-        return binarySearch(shiftArr, pivotIdx, len(shiftArr), num)
+        return binarySearch(shiftArr, pivotIdx, len(shiftArr)-1, num)
     else:
         return binarySearch(shiftArr, 0, pivotIdx-1, num)
 
@@ -25,11 +25,14 @@ def binarySearch(a, left, right, num):
   
 def findPivot(a):
     left = 0
-    right = len(a)
+    # if not -1 from len(a), the "=" in 'left <= right' will fail 
+    # calculating mid=(right) + left)//2 . eg a=[1,2]
+    right = len(a) - 1  
 
     while left <= right :
         mid = (right + left) // 2
         #mid=0 when left=right=0 [5,0,1,2,3,4]
+        #print(f'l {left} r {right} m {mid} a {a[mid]}')
         if a[mid] < a[mid -1] or mid == 0:  
             return mid
 
@@ -43,5 +46,6 @@ def findPivot(a):
 
 #a=[1,2,3,4,5,0] # pivot=5
 a = [5,0,1,2,3,4]   # pivot=1
+a = [1,2]
 n=1
 print(shifted_arr_search(a,n))
