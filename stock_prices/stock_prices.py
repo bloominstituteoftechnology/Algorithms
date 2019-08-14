56577b30f79a440cb899cf690cbce68843d2af1f
Austin James <apj2277@gmail.com>
2019-08-14 13:47:46 -0700

    stock prices pass test

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,10 +1,34 @@
 #!/usr/bin/python
 
+# Write a function `find_max_profit` that receives as input a list of stock prices.
+# Your function should return the maximum profit that can be made from a single buy and sell.
+# You must buy first before selling; no shorting is allowed here.
+
+# For example, `find_max_profit([1050, 270, 1540, 3800, 2])` should return 3530,
+# which is the maximum profit that can be made from a single buy and then sell of these stock prices.
+
+
+# Use two loops.
+# In the outer loop, pick elements one by one
+# In the inner loop, calculate the difference of the picked element with every other element in the array
+# compare the difference with the maximum difference calculated so far.
+
+
 import argparse
 
-def find_max_profit(prices):
-  pass
 
+def find_max_profit(prices):
+    profit = prices[1] - prices[0]
+
+    for i in range(0, len(prices)):
+        for j in range(i + 1, len(prices)):
+            if prices[j] - prices[i] > profit:
+                profit = prices[j] - prices[i]
+    return profit
+
+
+print(find_max_profit([44, 30, 24, 32, 35, 30, 40, 38, 15]))
+print(find_max_profit([1050, 270, 1540, 3800, 2]))
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
