e732b621e856e0806ecb096e2166cbe9deec9383
Austin James <apj2277@gmail.com>
2019-08-16 08:04:37 -0700

    working on rps

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python
 
+import itertools
 import sys
 
-def rock_paper_scissors(n):
-  pass 
 
+def rock_paper_scissors(n):
+    plays = ["rock", "paper", "scissors"]
+    combo = [list(c) for c in itertools.permutations(plays, n)]
+    new_combo = []
+
+    for i in combo:
+        new_combo.append(i)
+    return new_combo
+
+print(rock_paper_scissors(5))
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
