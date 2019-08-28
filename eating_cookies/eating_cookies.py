a682421d2a923d5bf3a3bd823744446083e9d252
badCompany55 <zachirvin45@gmail.com>
2019-08-27 23:04:46 -0500

    cookies problem solved using regression and cache

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,8 +5,17 @@ import sys
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
+memcache = {}
 def eating_cookies(n, cache=None):
-  pass
+    if n in memcache:
+        return memcache[n]
+    if n <= 1:
+        return 1
+    if n <= 2:
+        return 2
+    val = eating_cookies(n - 1) + eating_cookies(n - 2) + eating_cookies(n - 3)
+    memcache[n] = val
+    return val
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
