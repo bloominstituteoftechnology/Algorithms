3461a10ce8316c65316adb7454f845f137b56013
Ashley Owens <ash4741@gmail.com>
2019-08-30 00:15:20 -0400

    Complete Recipe_Batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -14,19 +14,18 @@ def recipe_batches(recipe, ingredients):
   key = ''
 
   for item in ingredients_list:
+    
     if item[1] == lowest:
       key = item[0]
-  
-  amount = recipe.get(key)
 
+      amount = recipe.get(key)
+      batches = item[1] // amount
+     
+  return batches
 
-
-  # for i, amt enumerate(ingredients):
-
-recipe_batches({ 'milk': 100, 'butter': 50, 'cheese': 11 }, { 'milk': 198, 'butter': 52, 'cheese': 10 })
-# if __name__ == '__main__':
-#   # Change the entries of these dictionaries to test 
-#   # your implementation with different inputs
-#   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-#   ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-#   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+if __name__ == '__main__':
+  # Change the entries of these dictionaries to test 
+  # your implementation with different inputs
+  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
+  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
