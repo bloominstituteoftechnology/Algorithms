f0623f3e41647f970f1d5fd56e7aaf3cf5c5064c
Ashley Owens <ash4741@gmail.com>
2019-09-01 14:55:14 -0400

    Complete Make_change and Eating_cookies

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -3,8 +3,16 @@
 import sys
 
 def making_change(amount, denominations):
-  pass 
-
+  count = [0 for x in range(amount+1)]
+  print(count) 
+  count[0] = 1
+  print(count) 
+  
+  for i in range(0,len(denominations)):
+    for j in range(denominations[i], amount+1):
+      count[j] += count[j-denominations[i]]     
+      
+  return count[amount] 
 
 if __name__ == "__main__":
   # Test our your implementation from the command line
