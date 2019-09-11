46aebfb471089c2c6a0d378ca18a812ce401d88e
Arron M <marshall.arron@gmail.com>
2019-09-11 15:37:15 -0700

    Implement naive solution for eating cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -9,12 +9,31 @@ import sys
 # [1, 1, 1], [2, 1], [3]
 # [1, 1, 1], [2, 1], [1, 2], [3]
 
+# 5 = eat(4) + eat(3) + eat(2)
+# 4 = eat(3) + eat(2) + eat(1)
+# 3 = eat(2) + eat(1) + eat(0)
+# 2 = eat(1) + eat(0) + eat(-1)
+# 1 = eat(0) + eat(-1) + eat(-2)
+
+
 def eating_cookies(n, cache=None):
     # For each possible number of cookies to eat
     # calculate the number of ways to eat n cookies
     # n = (n - 3) + (n -2) + (n - 1)
+    if cache is None:
+        cache = [0] * (n + 1)
+        cache[0] = 1
 
-    pass
+    if n < 0:
+        return 0
+
+    if cache[n] != 0:
+        return cache[n]
+
+    cache[n] = eating_cookies(n - 3, cache) \
+        + eating_cookies(n - 2, cache) \
+        + eating_cookies(n - 1, cache)
+    return cache[n]
 
 
 if __name__ == "__main__":
