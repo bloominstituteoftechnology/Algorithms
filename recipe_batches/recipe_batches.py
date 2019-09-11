733b2d09151dacbe457625bd51362abdbcb508d6
meginks <meganjones.dev@gmail.com>
2019-09-11 18:00:49 -0500

    finished stock prices problem

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -1,4 +1,31 @@
-#!/usr/bin/python
+# #!/usr/bin/python
+
+# # Recipe Batches
+
+# Write a function that receives a recipe in the form of a dictionary, as well as all of the ingredients you have available to you, also in the form of a dictionary. Both of these dictionaries will have the same form, and might look something like this:
+
+# ```python
+# {
+#   'eggs': 5,
+#   'butter': 10,
+#   'sugar': 8,
+#   'flour': 15
+# }
+# ```
+
+# The keys will be the ingredient names, with their associated values being the amounts. In the case of the recipe dictionary, these amounts will represent the amount of each ingredient needed for the recipe, while in the case of the ingredients dictionary, the amounts represent the amounts available to you. 
+
+# Your function should output the maximum number of whole batches that can be made for the supplied recipe using the ingredients available to you, as indicated by the second dictionary. 
+
+# For example
+
+# ```python
+# # should return 0 since we don't have enough butter!
+# recipe_batches(
+#   { 'milk': 100, 'butter': 50, 'flour': 5 },
+#   { 'milk': 138, 'butter': 48, 'flour': 51 }
+# )
+# ```
 
 import math
 
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,9 +2,39 @@
 
 import argparse
 
-def find_max_profit(prices):
-  pass
+# bought at $10
+# best one so far $4
+# sell on day 2, -3, day 3: -5, day 4: -2, day 5: 1, day 6: -1
+# bought at $7, -2, 1, 4, 2
+# buy on day 3 at $5 sell on day 5, make $6
+# set my maximum profit at the second price minus the first price
+# start at the first price, and pretend I'm buying that one
+# loop through all the prices that are after the current one (that I bought)
+# Every time I loop I'm going to calculate the "sell" price minus the "buy" price
+# If that profit is more than the max profit that I have, set the max profit to this new higher profit
+# go on to the next price, "buy" that one
+# repeat the steps where I'm looping through all the prices after the current one, and do the comparisons
 
+def find_max_profit(prices):
+  # initialize profit as index 1 (bought) - index 2 (sold)
+  profit = prices[1]-prices[0]
+  min_value = prices[0]
+  print(profit)
+  if len(prices) <= 1: 
+    profit = 0
+    print(profit)
+  else: 
+    profit = prices[1]-prices[0]
+    print(profit)
+    for i in range(1, len(prices)):
+      if prices[i]-min_value > profit: 
+        profit = prices[i]-min_value 
+      if prices[i] < min_value: 
+        min_value = prices[i] 
+      
+  return profit
+  
+  
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
