f9fbb3b664ed2a33d52638bb24c0b4e6e7953c98
Arron M <marshall.arron@gmail.com>
2019-09-11 13:12:41 -0700

    Add niave solution for stock_prices

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -2,16 +2,31 @@
 
 import sys
 
-def making_change(amount, denominations):
-  pass 
+# For each denomination get the number of ways to make change with that denomination
+
+
+def making_change(amount, coins, memo=None):
+    if memo is None:
+        memo = [0] * (amount + 1)
+        memo[0] = 1
+
+    if amount <= 1:
+        return 1
+
+    if memo[amount] > 0:
+        return memo[amount]
+
+    memo[amount] += making_change(amount - 1, coins, memo)
+
+    return memo[amount]
 
 
 if __name__ == "__main__":
-  # Test our your implementation from the command line
-  # with `python making_change.py [amount]` with different amounts
-  if len(sys.argv) > 1:
-    denominations = [1, 5, 10, 25, 50]
-    amount = int(sys.argv[1])
-    print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
-  else:
-    print("Usage: making_change.py [amount]")
+    # Test our your implementation from the command line
+    # with `python making_change.py [amount]` with different amounts
+    if len(sys.argv) > 1:
+        denominations = [1, 5, 10, 25, 50]
+        amount = int(sys.argv[1])
+        print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
+    else:
+        print("Usage: making_change.py [amount]")
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,14 +2,28 @@
 
 import argparse
 
+
 def find_max_profit(prices):
-  pass
+    # For each of our stock prices we want to
+    #  Loop through the rest of the stock prices going forward and calculate best profit
+    best_profit = None
+
+    # [5, 2, 6, 8]
+    for i in range(len(prices)):
+        for j in range(i + 1, len(prices)):
+            if best_profit is None:
+                best_profit = prices[j] - prices[i]
+                break
+            if prices[j] - prices[i] > best_profit:
+                best_profit = prices[j] - prices[i]
+
+    return best_profit
 
 
 if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+    # This is just some code to accept inputs from the command line
+    parser = argparse.ArgumentParser(description='Find max profit from prices.')
+    parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
+    args = parser.parse_args()
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+    print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
