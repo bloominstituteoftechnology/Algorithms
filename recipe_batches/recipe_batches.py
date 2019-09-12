a0c25b51a374444d8c99e8d642c1323016dd6949
Ash <ashley.cruz.crespo@gmail.com>
2019-09-12 16:01:11 -0400

    working on algorithms

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,24 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    if len(list(dict.values(recipe))) is not len(list(dict.values(ingredients))):
+        return 0
+    total_recipes_completed = None
+    for val in recipe:
+        total = ingredients[val] // recipe[val]
+        if total_recipes_completed is None:
+            total_recipes_completed = total
+        elif total < total_recipes_completed:
+            total_recipes_completed = total
+    return total_recipes_completed
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+        # Change the entries of these dictionaries to test
+        # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 52, 'flour': 100}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
