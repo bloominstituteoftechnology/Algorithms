e8b15ab505fdde325cc8ad369a2c16cd5cd54014
basilcea <ogbonnabasil3@gmail.com>
2019-10-10 08:53:19 +0100

    recipe batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,12 +3,24 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  batches =  []
+  quantity = None
+  try:
+    for i in  recipe:
+      if(ingredients[i]):
+        quantity = int(ingredients[i]/ recipe[i])
+        batches.append(quantity)
+      maxBatches = min(batches)
+    return maxBatches
+  except:
+    maxBatches = 0
+    return maxBatches
+
 
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
   # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+  recipe = { 'milk': 100, 'flour': 4, 'sugar': 10, 'butter': 5 },
+  ingredients = { 'milk': 1288, 'flour': 9, 'sugar': 95 }
   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
