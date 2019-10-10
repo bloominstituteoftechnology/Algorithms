6f292309e78e6422884c313dfba7762f0abf5b98
pierretd <42428262+pierretd@users.noreply.github.com>
2019-10-10 19:04:28 -0400

    everything except knapsack

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -2,16 +2,25 @@
 
 import sys
 
-def making_change(amount, denominations):
-  pass 
-
+# The cache parameter is here for if you want to implement
+# a solution that is more efficient than the naive 
+# recursive solution
+def eating_cookies(n, cache=None):
+  if n == 0:
+    return 1
+  elif n < 3:
+    return n
+  elif cache and cache[n] > 0:
+    return cache[n]
+  else:
+    if not cache:
+      cache = {i: 0 for i in range(n+1)}
+    cache[n] = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+    return cache[n]
 
 if __name__ == "__main__":
-  # Test our your implementation from the command line
-  # with `python making_change.py [amount]` with different amounts
   if len(sys.argv) > 1:
-    denominations = [1, 5, 10, 25, 50]
-    amount = int(sys.argv[1])
-    print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
+    num_cookies = int(sys.argv[1])
+    print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
   else:
-    print("Usage: making_change.py [amount]")
+    print('Usage: eating_cookies.py [num_cookies]')
