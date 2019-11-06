6619a4b7300c2270de44dddfb4da073d5161510e
tjforeman <tylerforeman92@gmail.com>
2019-11-06 16:57:52 -0500

    inital commit with stock prices

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,7 +3,27 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  max_price = prices[1]
+  min_price = prices[0]
+  max_profit = max_price - min_price
+
+  for i in range (len(prices)):
+    for j in range(i + 1, len(prices)):
+
+
+      if prices[j] - prices[i] > max_profit:
+        max_price = prices[j]
+        min_price = prices[i]
+        max_profit = max_price - min_price
+        print('min',min_price)
+        print('max', max_price)
+
+  return max_profit
+      
+
+prices1 = [15, 7, 9, 13, 45, 5]
+the_prices = find_max_profit(prices1)
+print(the_prices)
 
 
 if __name__ == '__main__':
