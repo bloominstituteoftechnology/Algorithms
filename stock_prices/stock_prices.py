e007c8f5f8c3d4f21252df0609cb051baf6e0fea
adam <bcuz@users.noreply.github.com>
2019-12-09 23:52:16 -0500

    refactor

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -4,22 +4,18 @@ import argparse
 import math
 
 def find_max_profit(prices):
-	# negative means profit
 
-	# max profit variable
-	maxProfit = math.inf
-	# nested loop
-	# reverse the sign of number at the end
+	maxProfit = -math.inf
 
 	for i in range(0, len(prices) - 1):
 		
 		for j in range(i+1, len(prices)):
-			# got things backwards lol
-			# print(prices[i], prices[j])
-			if (prices[i] - prices[j]) < maxProfit:
-				maxProfit = prices[i] - prices[j]
+			profit = prices[j] - prices[i]
 
-	return -maxProfit				
+			if (profit) > maxProfit:
+				maxProfit = profit
+
+	return maxProfit				
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
