f39d4fa311eed2e454cd627217f42584f4176b6e
adam <bcuz@users.noreply.github.com>
2019-12-09 23:48:48 -0500

    stock prices passing

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,10 +1,25 @@
 #!/usr/bin/python
 
 import argparse
+import math
 
 def find_max_profit(prices):
-  pass
+	# negative means profit
 
+	# max profit variable
+	maxProfit = math.inf
+	# nested loop
+	# reverse the sign of number at the end
+
+	for i in range(0, len(prices) - 1):
+		
+		for j in range(i+1, len(prices)):
+			# got things backwards lol
+			# print(prices[i], prices[j])
+			if (prices[i] - prices[j]) < maxProfit:
+				maxProfit = prices[i] - prices[j]
+
+	return -maxProfit				
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
