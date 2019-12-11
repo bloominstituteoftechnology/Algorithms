f1c80d21d69143b81273c9ca8f58f2bd2c415419
Lambda_School_Loaner_93 <antoniounivers@gmail.com>
2019-12-11 10:02:53 -0500

    eating_cookies, recipe_batches, rock_paper_scissors, stock_prices : compeleted

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,26 @@
 
 import math
 
-def recipe_batches(recipe, ingredients):
-  pass 
-
-
+def recipe_batches(recipe, ingredients, batch_count=0):
+  batch_count = batch_count
+  if len(recipe) > len(ingredients):
+    return batch_count
+  for key in ingredients.keys():
+   if ingredients[key] < recipe[key]:
+      return batch_count
+   else:
+      ingredients[key] = ingredients[key] - recipe[key]
+      batch_count += 1
+      newingreds = ingredients
+      
+      
+      return recipe_batches(recipe,newingreds,batch_count)
+      
+  
+  
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
   # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 , 'sugar': 90}
+  ingredients = { 'milk': 138, 'butter': 90, 'flour': 50 }
   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,9 +3,23 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  max_profit = prices[1]-prices[0]
 
+  for i in range(len(prices)-1):
+   current = i
+   currentprofit = 0
+   for j in range(current+1 ,len(prices[1:]) ):
+      # print(f'j: {prices[j]} - current: {prices[current]} = {prices[j]-prices[current]}')
+      profit = prices[j] - prices[current]
+     
+      
+      print(profit, currentprofit)
+      if profit >= max_profit:
+         max_profit = profit
+        
+  return max_profit
 
+find_max_profit([10, 7, 5, 8, 11, 9])
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
   parser = argparse.ArgumentParser(description='Find max profit from prices.')
