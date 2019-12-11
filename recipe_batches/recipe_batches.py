1695096b1a4def2ec4cb0dbb221447beb94d6f94
Chris-Delfaus <ChrisDelf@protonmail.com>
2019-12-11 18:06:29 -0500

    starting on recipe_batches.py

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,11 +3,17 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  max_batches = 0
+
+  recipe = list(recipe.values())
+  ingredients = list(ingredients.values())
+  print(f"hello {recipe}")
+  print(f"hello {ingredients}")
+  pass
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
+  # Change the entries of these dictionaries to test
   # your implementation with different inputs
   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
   ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
