6896315ad94210c5c00eb13d2603e631ee1ae79f
Eric Wuerfel <Eric@Erics-MacBook-Pro-5.local>
2019-12-11 18:42:47 -0500

    got first two

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,7 +3,25 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+    # Get ingredients
+    recipe_ingredients = [key for key, value in recipe.items()]
+    stock_ingredients = [key for key, value in ingredients.items()]
+    
+    # Check that we have all the ingredients
+    for entry in recipe_ingredients:
+        if entry not in stock_ingredients:
+            return 0
+        else:
+            pass
+        
+    # Get possible batch sizes for each
+    possible_batch_sizes = []
+    
+    for key, value in recipe.items():
+        batch_size = ingredients[key] // value
+        possible_batch_sizes.append(batch_size)
+        
+    return min(possible_batch_sizes)
 
 
 if __name__ == '__main__':
