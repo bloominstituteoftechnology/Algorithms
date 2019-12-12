3f624ef2b6e8e0767da97ff239afe84230fdfbce
JerryOsorio28 <jerryOsorio28@users.noreply.github.com>
2019-12-12 12:05:03 -0500

    finished first exercise

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,10 +1,25 @@
 #!/usr/bin/python
 
 import argparse
-
 def find_max_profit(prices):
-  pass
+ #global variable to hold profits
+ profits = []
+ #iterate through the array's length - 1
+ for i in range(0, len(prices) - 1):
+ #second for loop starting at index 1 to compare index's values
+ 	for j in range(i + 1, len(prices)):
+ #compare if i is less than j, if it is then substract j from i and push result to profits
+ 		if prices[i] < prices[j]:
+ 			result = prices[j] - prices[i]
+ 			profits.append(result)
+ #else if i is greater than j, subtract j from i and push results to profits
+ 		elif prices[i] > prices[j]:
+ 			result = prices[j] - prices[i]
+ 			profits.append(result)
+ #return the max of the profits variable
+ return max(profits)
 
+# find_max_profit([12, 5, 6, 11, 9, 3])
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
