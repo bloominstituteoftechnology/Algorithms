5b7a599a3dc5fa8d4a02b4e88be6827e9eca3ee4
Chris-Delfaus <ChrisDelf@protonmail.com>
2019-12-11 19:00:32 -0500

    commit before standup

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -5,16 +5,24 @@ import math
 def recipe_batches(recipe, ingredients):
   max_batches = 0
 
-  recipe = list(recipe.values())
-  ingredients = list(ingredients.values())
-  print(f"hello {recipe}")
+  recipe_keys = set(recipe.keys())
+  ingredients_keys = set(ingredients.keys())
+  ingredients_keys_list = list(ingredients.keys())
+
+  print(f"hello {recipe_keys}")
   print(f"hello {ingredients}")
-  pass
+
+  common_ingredients = set(recipe_keys).intersection(set(ingredients_keys))
+  print(len(common_ingredients))
+# first thing we want to check is if we even have the ingredients for the recipe in the first place.
+  if len(common_ingredients) != len(ingredients_keys_list):
+    return 0
+
 
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test
   # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
+  recipe = { 'milk': 100, 'butter': 50, 'flour': 5, 'dooper': 10 }
   ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
