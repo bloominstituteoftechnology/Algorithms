88c88fa3970ad15b7d290d46b1237472af6dc609
Laura Daugherty <lauradaugherty6@gmail.com>
2019-12-12 15:01:28 -0800

    stock prices

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,13 +5,8 @@ import sys
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
-def eating_cookies(n):
-  if n < 0:
-    return 0
-  if n == 0:
-    return 1
-  else:
-    return eating_cookies(n-3) + eating_cookies(n-2) + eating_cookies(n-1)
+def eating_cookies(n, cache=None):
+  pass
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,45 +3,35 @@
 import argparse
 
 def find_max_profit(prices):
-
-# track profit
-  profit = min(prices) - max(prices)
-# track index
-  cur_high_index = 0
+  cur_max_index = 0
   cur_low_index = 0
-# track high and low
-  cur_high = 0
+  cur_max = 0
+  print("cur_max", cur_max)
   cur_low = prices[cur_low_index]
+  print("cur_low", cur_low)
 
-  for i in range(len(prices)):
-    # if it's lower than low and isn't the last item record it as the lowest item and record its index
-    if prices[i] < cur_low and i != len(prices)-1:
-      cur_low_index = i
+  for price in range(len(prices)):
+    profit = cur_max - cur_low
+
+    print("profit", profit)
+    if prices[price] > cur_max and price > cur_low_index:
+      cur_max_index = price
+      cur_max = prices[cur_max_index]
+      print("new cur max num", cur_max)
+
+    elif prices[price] < cur_low and price != len(prices) - 1:
+      cur_low_index = price
       cur_low = prices[cur_low_index]
-      print("newLow", cur_low)
-      print("newLowIndex", cur_low_index)
-    # if it's higher than high and comes after the lowest number, record it as the new high and record index
-    if prices[i] > cur_high and i > cur_low_index:
-      cur_high_index = i
-      cur_high = prices[cur_high_index]
-      print("newHigh", cur_high)
-    # find profit from the highest and lowest that apply to params
-    profit = cur_high - cur_low
+      print("new cur low price", prices[cur_low_index])
+
+
   return profit
 
-  
 
-# profitFound = find_max_profit([1050, 270, 1540, 3800, 2])
-# print(profitFound)
+if __name__ == '__main__':
+  # This is just some code to accept inputs from the command line
+  parser = argparse.ArgumentParser(description='Find max profit from prices.')
+  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
+  args = parser.parse_args()
 
-# profitFoundtwo = find_max_profit([100, 55, 4, 98, 10, 18, 90, 95, 43, 11, 47, 67, 89, 42, 49, 79])
-# print(profitFoundtwo)
-
-
-# if __name__ == '__main__':
-#   # This is just some code to accept inputs from the command line
-#   parser = argparse.ArgumentParser(description='Find max profit from prices.')
-#   parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-#   args = parser.parse_args()
-
-#   print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
