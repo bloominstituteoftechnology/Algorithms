71f3b71cbae466f3f1765741be585aab64c4b83d
Sean Doyle <sdoylelambda@gmail.com>
2019-12-12 15:32:44 -0500

    Recipes are compared, but my key, not value.

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,20 +2,20 @@
 
 import math
 
-def recipe_batches(recipe, ingredients):
-  maxBatches = 0
-
-  return maxBatches
-
-
-if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
-
-
+# def recipe_batches(recipe, ingredients):
+#   maxBatches = 0
+#
+#   return maxBatches
+#
+#
+# if __name__ == '__main__':
+#   # Change the entries of these dictionaries to test
+#   # your implementation with different inputs
+#   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
+#   ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+#   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+#
+#
 
 
 
@@ -44,16 +44,43 @@ if __name__ == '__main__':
 # )
 # OUTPUT -- 2 -- NOT ENOUGH INGREDIENTS
 
-
+print('PROGRAM START')
 # PLAN
 # INPUT(DICTIONARY1, DICTIONARY2) RETURN => MAX BATCHES(TYPE=INT)
-# def recipe_batches(recipe, ingredients):
-#   maxBatches = 0
-#
-#
-#
-#   return maxBatches
+def recipe_batches(recipe, ingredients):
+  print('FUNCTION CALLED')
+  maxBatches = int
+  # FOR EACH VALUE IN THE FIRST DICTIONARY,
+  for i in recipe:
+    print('I IN RECIPE')
+    print(i)
+   # if value1 is lessor than value2 return value1 / value2
+    recipesCount = i[1]
+    print('recipesCount')
+    print(recipesCount)
+    for i2 in ingredients:
+      print('I2 IN RECIPE')
+      print(i2)
+      ingredientsCount = i2[1]
+      print('ingredientsCount')
+      print(ingredientsCount)
+      if recipesCount < ingredientsCount:
+        z = ingredientsCount
+        return z
+      # elif:
+      #   return 0
+      #
+  # COMPARE TO EACH VALUE IN 2ND DICTIONARY
+  # IF
+  # RETURN WHOLE NUMBER IF GREATOR THAN 1, ELSE RETURN 0
+
+  print('FUNCTION RAN')
+
+
+  print(maxBatches)
+  return maxBatches
 
 # EXECUTE
-
+recipe_batches({ 'milk': 100, 'flour': 4, 'sugar': 10, 'butter': 5 }, { 'milk': 1288, 'flour': 9, 'sugar': 95 })
+print('PROGRAM END')
 # REFLECT
