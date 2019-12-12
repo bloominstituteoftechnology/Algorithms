f3ae318d177eea0495afc85b030defb360926cd4
Sean Doyle <sdoylelambda@gmail.com>
2019-12-12 15:06:09 -0500

    Lambda UPER framework for recipes.

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,7 +3,9 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  maxBatches = 0
+
+  return maxBatches
 
 
 if __name__ == '__main__':
@@ -17,3 +19,41 @@ if __name__ == '__main__':
 
 
 
+#   LAMBDA PROBLEM SOLVING FRAMEWORK
+
+# UPER
+
+# UNDERSTAND
+#     GOAL - RETURN HOW MANY BATCHES OF RECIPE CAN BE MADE FROM INPUT OF 2 DICTIONARIES
+#     {1ST RECIPES}, 2ND {CURRENT INGREDIENTS}
+#       BASED ON TEST RESULTS NO NEED TO ACCOUNT FOR BAD INPUT
+#
+#     EX:
+# INPUT
+#         recipe_batches(
+#   { 'milk': 100, 'butter': 50, 'flour': 5 },
+#   { 'milk': 138, 'butter': 48, 'flour': 51 }
+# )
+# OUTPUT -- 0 -- NOT ENOUGH INGREDIENTS
+
+#     EX:
+# INPUT
+#       recipe_batches(
+#   { 'milk': 2, 'sugar': 40, 'butter': 20 },
+#   { 'milk': 5, 'sugar': 120, 'butter': 500 }
+# )
+# OUTPUT -- 2 -- NOT ENOUGH INGREDIENTS
+
+
+# PLAN
+# INPUT(DICTIONARY1, DICTIONARY2) RETURN => MAX BATCHES(TYPE=INT)
+# def recipe_batches(recipe, ingredients):
+#   maxBatches = 0
+#
+#
+#
+#   return maxBatches
+
+# EXECUTE
+
+# REFLECT
