3e0778546fb034e483ed9d74738a4dbdce98b9b7
Zach Lasky <zacharylasky123@gmail.com>
2019-12-11 19:11:42 -0500

    recipe_batch_test pass

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,32 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    min_batch = None
+    for ingredient in recipe:
+        if ingredient in ingredients:
+            # print(ingredients[ingredient], recipe[ingredient])
+            batches = ingredients[ingredient] // recipe[ingredient]
+            if min_batch is None:
+                min_batch = batches
+            else:
+                min_batch = min(batches, min_batch)
+        else:
+            return 0
+    return min_batch
 
 
-if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+recipe_batches(
+    {'milk': 2, 'sugar': 40, 'butter': 20},
+    {'milk': 5, 'sugar': 120, 'butter': 500}
+)
+
+
+# if __name__ == '__main__':
+#     # Change the entries of these dictionaries to test
+#     # your implementation with different inputs
+#     recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+#     ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+#     print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+#         batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
