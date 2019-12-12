417362b48d6a4fb0bae78c97e02dc0466a4de884
Chris-Delfaus <ChrisDelf@protonmail.com>
2019-12-11 19:50:59 -0500

    completed recipe_batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -7,22 +7,30 @@ def recipe_batches(recipe, ingredients):
 
   recipe_keys = set(recipe.keys())
   ingredients_keys = set(ingredients.keys())
-  ingredients_keys_list = list(ingredients.keys())
 
-  print(f"hello {recipe_keys}")
-  print(f"hello {ingredients}")
 
   common_ingredients = set(recipe_keys).intersection(set(ingredients_keys))
-  print(len(common_ingredients))
 # first thing we want to check is if we even have the ingredients for the recipe in the first place.
-  if len(common_ingredients) != len(ingredients_keys_list):
+  if len(common_ingredients) != len(recipe):
     return 0
+  for key, value in ingredients.items():
+    if not key in recipe:
+      continue
+    batch = value // recipe[key]
+
+    if max_batches is 0 or batch < max_batches:
+      max_batches = batch
+  return max_batches
+  # for key , value in recipe.items():
+  #   print(ingredients[key])
+  #   if key != ingredients[key]:
+  #     print(f"We are missing an ingredient {ingredients.items()}")
 
 
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test
   # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5, 'dooper': 10 }
+  recipe = { 'milk': 100, 'butter': 50, 'flour': 5}
   ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
