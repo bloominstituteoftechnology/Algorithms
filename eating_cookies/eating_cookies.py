e808c4625888fe94dbffb06e3580162becb712f9
JerryOsorio28 <jerryOsorio28@users.noreply.github.com>
2019-12-12 22:30:13 -0500

    finished MVP

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,7 +6,25 @@ import sys
 # a solution that is more efficient than the naive 
 # recursive solution
 def eating_cookies(n, cache=None):
-  pass
+  #BASE CASES
+  if n < 0: return 0
+  if n == 0: return 1
+  if n == 1: return 1
+  
+  return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+
+cache = {}
+
+def recursion(n):
+  #BASE CASES
+  if n < 0: return 0
+  if n == 0: return 1
+  if n == 1: return 1
+  
+  if n not in cache:
+	  cache[n] = recursion(n-1) + recursion(n-2) + recursion(n-3)
+
+  return cache[n]
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
---
+++ eating_cookies/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+
+  #   self.assertEqual(eating_cookies(50, [0 for i in range(51)]), 10562230626642)
+  #   self.assertEqual(eating_cookies(100, [0 for i in range(101)]), 180396380815100901214157639)
+  #   self.assertEqual(eating_cookies(500, [0 for
