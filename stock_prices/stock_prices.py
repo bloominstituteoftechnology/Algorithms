0503976229ed4a83693bf6c78aa993345316484a
Michael Redig <mredig@gmail.com>
2020-01-22 15:22:21 -0600

    stock prices working
    autoformatting for pep8

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,14 +2,33 @@
 
 import argparse
 
+
 def find_max_profit(prices):
-  pass
+    if len(prices) < 2:
+        return 0
+    # set initial lowest and maxProfit values from actual array values
+    lowest = prices[0]
+    maxProfit = prices[1] - prices[0]
+    # for each item in prices
+    for index in range(1, len(prices)):
+        price = prices[index]
+        # each new item, calculate the difference from the current item and the lowest previous price
+        profitFromTrade = price - lowest
+        # track the lowest price
+        lowest = min(lowest, price)
+        # save the maximum, positive difference
+        maxProfit = max(profitFromTrade, maxProfit)
+    # return maximum when finished
+    return maxProfit
 
 
 if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+    # This is just some code to accept inputs from the command line
+    parser = argparse.ArgumentParser(
+        description='Find max profit from prices.')
+    parser.add_argument('integers', metavar='N', type=int,
+                        nargs='+', help='an integer price')
+    args = parser.parse_args()
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+    print("A profit of ${profit} can be made from the stock prices {prices}.".format(
+        profit=find_max_profit(args.integers), prices=args.integers))
