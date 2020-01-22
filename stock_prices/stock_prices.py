50e13ae9a93064bf0502b3c2b96e63f2a0c1be7f
JerryOsorio28 <jerryOsorio28@users.noreply.github.com>
2020-01-22 18:01:03 -0500

    finished stock prices problem

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,8 +3,26 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
-
+	# we need to substract all values with each other and return the greatest number of all substractions
+	# a temporary variable that will hold all the subtractions made in the sequence
+	max_profit = []
+	# we need a loop to iterate over the list of prices
+	for curr_index in range(0, len(prices) - 1):
+		# another loop to compare indexes
+		for next_index in range(curr_index + 1, len(prices)):
+			# we check if the current index is greater than the current's next index
+			if(prices[curr_index] < prices[next_index]):
+				# if it is, we subtract the current's next index with the current index
+				profit = prices[next_index] - prices[curr_index]
+				# we append the profit to our max profit array
+				max_profit.append(profit)
+			# else, we subtract the current index with the current's next index
+			elif(prices[curr_index] > prices[next_index]):
+				profit = prices[next_index] - prices[curr_index]
+				# we append the profit to our max profit array
+				max_profit.append(profit)
+	# once we are done with the loop, we return the max of the temporary index, holding the max profit.
+	return max(max_profit)
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
