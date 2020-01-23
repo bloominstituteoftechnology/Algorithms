0b6813573201942ca6f8ef83e18326f7604da828
Zach <zpman95@hotmail.com>
2020-01-23 11:52:14 -0600

    eating_cookies, and recipes

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,6 +6,13 @@ import sys
 # a solution that is more efficient than the naive 
 # recursive solution
 def eating_cookies(n, cache=None):
+  if cache is None or type(cache) == list:
+     cache = {0:1, 1:1, 2:2}
+  if n < 0:
+     return 0
+  elif n not in cache:
+      cache[n] = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+  return cache[n]
   pass
 
 if __name__ == "__main__":
--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,12 +3,22 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  min_ratio = math.inf
+
+  for ingredient, amount in recipe.items():
+    if ingredient not in ingredients:
+       return 0
+    ratio = ingredients[ingredient] // amount
+    if ratio < min_ratio:
+       min_ratio = ratio
+
+  return min_ratio
+pass 
 
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
   # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+  recipe = { 'milk': 10, 'butter': 500, 'flour': 45 }
+  ingredients = { 'milk': 248, 'butter': 8, 'flour': 1 }
   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
