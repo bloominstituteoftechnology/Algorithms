a1eced1c2deed454e312c224af0d8232b3944814
Michael Redig <mredig@gmail.com>
2020-01-22 21:43:22 -0600

    rock paper scissors working

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,14 +1,27 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 import sys
 
+validPlays = [["rock"], ["paper"], ["scissors"]]
 def rock_paper_scissors(n):
-  pass 
+    if n == 0:
+        return [[]]
+    if n == 1:
+        return validPlays
+    
+    output = []
+    arrA = rock_paper_scissors(n - 1)
+    for subArr in arrA:
+        for play in validPlays:
+            newPlay = subArr + play
+            output.append(newPlay)
+
+    return output
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
