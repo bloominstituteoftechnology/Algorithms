3b283e6ff62a2ad6359c5240b9f3cdd99e8f5c65
Zach <zpman95@hotmail.com>
2020-01-23 17:22:23 -0600

    making_changes

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python
 
 import sys
+cache = {}
+sys.setrecursionlimit(1500)
 
 def making_change(amount, denominations):
-  pass 
+  if amount == 0 or amount == 1:
+     return 1
+
+  different_ways = [1] + [0] * amount
+  for coins in denominations:
+    for higher in range(coins, amount + 1):
+        different_ways[higher] += different_ways[higher - coins]
+  return different_ways[amount]
+  pass
+
 
 
 if __name__ == "__main__":
