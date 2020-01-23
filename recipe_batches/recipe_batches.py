e0478ba30e50fdb23ebd09c6f02ff829fa7fae5f
jkaunert <44586402+jkaunert@users.noreply.github.com>
2020-01-23 15:38:02 -0600

    (feat) recipe_batches.py functional+MVP complete

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python
 
-import math
+from math import inf, floor
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  max_batches = inf
+
+  for ingredient, amount in recipe.items():
+    if ingredient not in ingredients:
+      return 0
+
+    batches = floor(ingredients[ingredient] / amount)
+    if batches < max_batches:
+       max_batches = batches
+
+  return max_batches
 
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
   # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+  recipe = { 'milk': 420, 'butter': 150, 'flour': 50 }
+  ingredients = { 'milk': 1132, 'butter': 348, 'flour': 751 }
   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
