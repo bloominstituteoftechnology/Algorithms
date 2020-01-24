79d75b5b2816852a16b58da78c7c094cb872f3ea
Jeff <theta_158@yahoo.com>
2020-01-23 21:16:53 -0500

    completed rps.py

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,32 @@
 
 import sys
 
+
+# Number of lists = len(cache[n-1]) * 3
+
+
 def rock_paper_scissors(n):
-  pass 
+    options = ['rock', 'paper', 'scissors']
+    perms = []
+
+    def helper_func(rounds_left, result=[]):
+        if rounds_left == 0:
+            perms.append(result)
+            return perms
+
+        else:
+            for i in options:
+                helper_func(rounds_left-1, result + [i])
+        return perms
+
+    helper_func(n, result=[])
+
+    return perms
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
