05d1581ddc48b7a311805a0a27481cc1247af6e9
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2020-02-06 19:19:41 -0800

    i think this is how you play rps

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,9 +2,22 @@
 
 import sys
 
-def rock_paper_scissors(n):
-  pass 
+validMoves = [['rock'], ['paper'], ['scissors']]
 
+def rock_paper_scissors(n)
+  if n == 0:
+    return [[]]
+  if n == 1:
+    return validMoves
+
+    output = []
+    arr = rock_paper_scissors(n -1)
+    for subArr in arr:
+      for move in validMoves:
+        newMove = subArr + move
+        output.append(newMove)
+
+    return output
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
