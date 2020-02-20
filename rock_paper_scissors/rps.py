ce3bb9f4dbd689ba1f8948394ddda0eb53f2e21b
jambis <imjaamees@gmail.com>
2020-02-20 13:48:43 -0600

    RPS work in progress

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,33 @@
 
 import sys
 
+
 def rock_paper_scissors(n):
-  pass 
+    plays = ['rock', 'paper', 'scissors']
+    result = []
+
+    if n == 0:
+        result.append([])
+        return result
+
+    def helper(arr, round):
+        for i in range(len(plays)):
+            arr.append(plays[i])
+            if round == n:
+                result.append(arr[i::1])
+            else:
+                helper(arr, round + 1)
+
+    helper([], 1)
+    return result
+
+
+print(rock_paper_scissors(2))
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
