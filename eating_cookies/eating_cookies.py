f4cbe15e3fe8879c71c8b484afe3587c50e7899c
Anna <townsendanna15@gmail.com>
2020-02-19 19:15:30 -0500

    mvp day 1

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,8 +5,13 @@ import sys
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
-def eating_cookies(n, cache=None):
-  pass
+def eating_cookies(n):
+  if n == 0:
+    return 1
+  elif n < 1:
+    return 0
+  else:
+    return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
