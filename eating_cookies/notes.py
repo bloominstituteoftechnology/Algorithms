6e2db05b8a3f0c76af988dce75cb44b335883fc7
LilySu <lilyxsu@gmail.com>
2020-02-20 11:00:14 -0500

    updated cookies

--- eating_cookies/notes.py
+++ eating_cookies/notes.py
@@ -7,7 +7,7 @@ n = 3
 #         for j in range(sec):
 #             print(sec - j)
 
-def c(n, count = 0):
+def c(n, count = 1):
     if n < 1:
         return count
     # print(n)
@@ -31,6 +31,38 @@ def c(n, count = 0):
 #     c(n-1, count + 1)
 #     print(f'count = {count}')
 
-l = c(n)
+# def eating_cookies(n, cache=1):
+#     if n < 1:
+#         return cache
+#     eating_cookies(n-1, cache+1)
+#     return eating_cookies(n-1, cache+1)
 
-print(l)
+
+# l = eating_cookies(n)
+
+# l = c(n)
+
+# print(l)
+
+
+# for i in n:
+#     n = n-i
+#     i -= 1
+n = 3
+def eating_cookies(n):
+    if n < 0:
+        return 0
+    elif n == 0 :
+        return 1
+    else:
+        a = eating_cookies(n-1)
+        print(f'a = {a}')
+        b = eating_cookies(n-2)
+        print(f'b = {b}')
+        c = eating_cookies(n-3)
+        print(f'c = {c}')
+        total = a + b + c
+        print(f'a + b + c = {a + b + c}')
+        return total
+
+print(eating_cookies(n))
