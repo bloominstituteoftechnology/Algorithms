8fa47904398a98c467520c9bdf8498db517aedee
Gatrrr <davidgatrmacaulay@gmail.com>
2020-02-19 16:20:11 -0800

    Finished RPS + Recipe

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,8 +2,24 @@
 
 import sys
 
+validPlays = [["rock"], ["paper"], ["scissors"]]
+
 def rock_paper_scissors(n):
-  pass 
+  if n == 0:
+    return [[]]
+  if n == 1:
+    return validPlays
+
+  output = []
+
+  arrA = rock_paper_scissors(n - 1)
+
+  for subArr in arrA:
+    for play in validPlays:
+      newPlay = subArr + play
+      output.append(newPlay)
+
+  return output
 
 
 if __name__ == "__main__":
