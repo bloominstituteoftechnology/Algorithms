31dfcc174fe77f0abcc7ca655d3c58a35c3d611e
dpancho <dalepancho7@gmail.com>
2020-02-20 17:15:09 -0800

    cookies done, rps not working YET. ignore it

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,28 @@
 
 import sys
 
+# define valid moves.....no 'spock', 'monster' etc.
+validMoves = [['rock'], ['paper'], ['scissors']]
+
+
 def rock_paper_scissors(n):
-  pass 
+    if n == 0:
+        return [[]]
+    if n == 1:
+        return validMoves
+
+        output = []
+        arr = rock_paper_scissors(n - 1)
+        for subArr in arr:
+            for move in validMoves:
+                newMove = subArr + move
+                output.append(newMove)
+        return output
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
