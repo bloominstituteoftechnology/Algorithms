935d13d3aec156f1fd60a8b0d33e7af9117adc13
Skyler Dowdy <skylerwebdev@gmail.com>
2020-03-06 15:01:10 -0500

    WIP

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,8 +3,24 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  #Define list of plays and possibilites
+  plays = ["rock", "paper", "scissors"]
+  all_poss = []
+  
+  #Helper function to keep track of the amt or rounds left and the results
+  def rounds(rleft,res):
+    
 
+    if rleft== 0:
+      all_poss.append(res)
+      return
+    
+    for i in range(0,len(plays)):
+      rounds(rleft -1, res + [plays[i]])
+    
+  rounds(n,[])
+  return all_poss
+  
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
