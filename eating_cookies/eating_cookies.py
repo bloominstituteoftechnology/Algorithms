51ab2cbd2e605643cfab6c146aedae17fbeceecf
saganator84 <kingtrav@gmail.com>
2020-03-06 20:33:45 -0700

    eating cookies passed

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,7 +6,34 @@ import sys
 # a solution that is more efficient than the naive 
 # recursive solution
 def eating_cookies(n, cache=None):
-  pass
+  # base case, he cannot eat anymore cookies
+  
+  if cache and n in cache:
+    print(f'Used cache for {n}: {cache[n]}')
+    return cache[n]
+  
+  if cache == None or type(cache) != dict: ## type check because large test inputs an array and breaks cache
+    cache = {0: 1}
+   
+  if n == 0:
+    return 1
+  
+  total_ways = 0
+  
+  if n >= 3:
+    # eat all 3 cookies
+    total_ways += eating_cookies(n-3, cache)
+    
+  if n >= 2:
+    # eat 2 cookies
+    total_ways += eating_cookies(n-2, cache)
+
+  if n >= 1:
+    # eat 1 cookie
+    total_ways += eating_cookies(n-1, cache)
+    
+  cache[n] = total_ways
+  return total_ways
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
