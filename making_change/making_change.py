8ae50273dc7610bc6f938d595426bf04b0ba8624
Isvvc <lyons@tuta.io>
2020-03-18 15:52:20 -0600

    Implement overshooting making_change

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -2,8 +2,31 @@
 
 import sys
 
-def making_change(amount, denominations):
-  pass 
+def making_change(amount, denominations, is_sorted = False):
+    # Should be similar to eating_cookies, but using
+    # the denominations array instead of range(1, 4)
+    # The order things are done in doesn't matter here, though
+
+    # Sort so you'll always be decreasing in denomination
+    if not is_sorted:
+        denominations.sort(reverse=True)
+
+    count = 0
+    if amount == 0:
+        return 1
+    for d in denominations:
+        if d == 1:
+            # If you reach pennies, you're done with this iteration
+            count += 1
+        else:
+            for i in range(1, int(amount / d) + 1):
+                if amount > d * i:
+                    newAmount = amount - d * i
+                    newDenoms = [x for x in denominations if x != d]
+                    count += making_change(newAmount, newDenoms, True)
+                elif amount == d * i:
+                    count += 1
+    return count
 
 
 if __name__ == "__main__":
