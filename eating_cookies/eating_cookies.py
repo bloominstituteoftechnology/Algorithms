3926e9c6972eaa8d38fc00589c511267d875d3c3
Albert Yakubov <46692454+albert-yakubov@users.noreply.github.com>
2020-03-18 14:24:34 -0600

    Update eating_cookies.py
    could not pass the test on eating cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,8 +5,35 @@ import sys
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
+# cache is the inventory --> 4 cookies
+# n is for cookies
+
 def eating_cookies(n, cache=None):
-  pass
+  # starter
+  combinations = 0
+
+  # base
+  if n == 0:
+    combinations += 1
+    return combinations
+    # check if inventory has items:
+  if cache is not None:
+    if cache[n] > 0:
+      return cache
+  if n >= 3:
+    combinations = eating_cookies(n-3, cache)
+    if cache is not None:
+      cache[n] = combinations
+  if n >= 2:
+    combinations = eating_cookies(n-2, cache)
+    if cache is not None:
+      cache[n] = combinations
+  if n >= 1:
+    combinations = eating_cookies(n-1, cache)
+    if cache is not None:
+      cache[n] = combinations
+
+  return combinations            
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
