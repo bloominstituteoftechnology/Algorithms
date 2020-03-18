c2441e43987685bd36b96c7e520f589bad8d46ad
Isvvc <lyons@tuta.io>
2020-03-18 14:52:01 -0600

    Print the corrent number of rps plays but without correct hands

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,7 +3,35 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+    rps = ['rock', 'paper', 'scissors']
+
+    # We need 3^(n-1) arrays starting with each hand
+    # 3^(n-2) arrays starting with hadn1, hand2
+    # 3^(n-3) arrays starting with hand1, hand2, hand3
+    # etc.
+
+    def add_hands(n, arr):
+        if n == 0:
+            return arr
+
+        # for the first iteration we should have an array of length 3^(n-1)
+        # for the first 3^(n-2) arrays of each hand, we need to add each hand
+        for i in range(0, len(rps)):
+            # first iteration: add rock to the first 3^(n-2) elements
+            # 3^(n-2) is also the length of the array / 3
+            length = int(len(arr) / 3)
+            for j in range(length * i, length * (i + 1)):
+                arr[j] += [rps[i]]
+            
+        return add_hands(n - 1, arr)
+
+    result = []
+    # set up arrays with their first elements
+    for hand in rps:
+        newArrs = [[hand] for i in range(0, pow(3, n - 1))]
+        result += newArrs
+        add_hands(n - 1, newArrs)
+    return result
 
 
 if __name__ == "__main__":
