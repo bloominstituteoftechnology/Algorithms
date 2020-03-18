4feab0ab9e85a2d19b351be4b8bbf828df89f01c
Isvvc <lyons@tuta.io>
2020-03-18 11:28:45 -0600

    Complete recipe_batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,7 +3,16 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+    max_batches = None
+    for key in recipe:
+        if not key in ingredients:
+            # if you don't have any of one ingredient,
+            # then you can't make any
+            return 0
+        local_max = math.floor(ingredients[key] / recipe[key])
+        if max_batches is None or local_max < max_batches:
+            max_batches = local_max
+    return max_batches
 
 
 if __name__ == '__main__':
