bad2b37f2aa3fdb9d229517dac72efa85c7d8eb9
Samantha Finley <srfinley@uchicago.edu>
2020-03-18 17:00:05 -0400

    memoization implementation passes modified big tests

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,6 +6,8 @@ import sys
 # a solution that is more efficient than the naive 
 # recursive solution
 def eating_cookies(n, cache=None):
+    # if cache[n] != 0:
+    #     return cache[n]
     if n <= 1:
         return 1
     if n == 2:
@@ -16,7 +18,25 @@ def eating_cookies(n, cache=None):
     total += eating_cookies(n-3)
     total += eating_cookies(n-2)
     total += eating_cookies(n-1)
+    # cache[n] = total
     return total
+
+# tutorial https://dbader.org/blog/python-memoization
+def memoize(func):
+    cache = dict()
+
+    def memoized_func(*args):
+        if args in cache:
+            return cache[args]
+        result = func(*args)
+        cache[args] = result
+        return result
+
+    return memoized_func
+
+# this works great!
+# has to be tested with cacheless versions of the big calls tho
+eating_cookies = memoize(eating_cookies)
     
 
 if __name__ == "__main__":
