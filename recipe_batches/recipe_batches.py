19542954b99ebfd529e78ac2bbe821a14fa22deb
Samantha Finley <srfinley@uchicago.edu>
2020-03-18 15:41:02 -0400

    recipes pass tests

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,12 +3,25 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+
+    per_ingredient = []
+    
+    for key in recipe.keys():
+        try:  # handles when the recipe calls for unavailable ingredient
+            owned = ingredients[key]
+        except KeyError:
+            return 0
+        if recipe[key] > owned:
+            return 0
+        else:
+            makable = owned // recipe[key]
+            per_ingredient.append(makable)
+    return min(per_ingredient)
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test 
+    # your implementation with different inputs
+    recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
+    ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
