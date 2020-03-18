1c0b4611af72ed2cc71804b973aef6f17b393af7
Isvvc <lyons@tuta.io>
2020-03-18 11:17:49 -0600

    Complete stock_prices

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,7 +3,13 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+    max_profit_neg = prices[0] - prices[1]
+
+    for i in range(0, len(prices) - 1):
+        for j in range(i + 1, len(prices) - 1):
+            if prices[i] - prices[j] < max_profit_neg:
+                max_profit_neg = prices[i] - prices[j]
+    return max_profit_neg * -1
 
 
 if __name__ == '__main__':
