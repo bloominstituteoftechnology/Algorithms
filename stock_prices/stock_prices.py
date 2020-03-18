788842c71cca32e275087adc39b03457ac181526
Sofia Levin <sofiafontes@me.com>
2020-03-18 13:45:06 -0700

    First pass on stock_prices.

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,8 +3,18 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  current_min_price_so_far = prices[0]
+  current_profit = prices[1] - current_min_price_so_far
+  max_profit_so_far = prices[1] - current_min_price_so_far
 
+  for i in range(len(prices) - 1):
+    current_profit = prices[i + 1] - current_min_price_so_far
+    if current_min_price_so_far > prices[i + 1]:
+        current_min_price_so_far = prices[i + 1]
+    if current_profit > max_profit_so_far:
+      max_profit_so_far = current_profit
+
+  return max_profit_so_far
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
