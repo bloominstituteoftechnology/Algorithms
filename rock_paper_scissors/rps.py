92d656c16d50656d2484bb7b9614a7f4b79fa2b5
Samantha Finley <srfinley@uchicago.edu>
2020-03-18 19:50:22 -0400

    rock paper scissors passes tests

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,28 @@
 
 import sys
 
+plays = ('rock', 'paper', 'scissors')
+
 def rock_paper_scissors(n):
-  pass 
+    results = []
+    if n == 0:
+        return [[]]
+    if n == 1:
+        results = [[play] for play in plays]
+    if n > 1:
+        for play in plays:
+            current = [play]
+            for item in rock_paper_scissors(n-1):
+                current.extend(item)
+                results.append(current)
+                current = [play]
+    return results
+    
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
