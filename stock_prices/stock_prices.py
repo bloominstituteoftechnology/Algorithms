5ed590ade8c4b6c9e9da3fd21a36e410376607fe
DeAndre Johnson <dre@phreshr.com>
2020-03-19 21:00:27 +0800

    wip

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,7 +3,22 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  current_price = prices[0]
+  profit = prices[1] - prices[0]
+
+  # loop over prices starting at the second item in the list
+  for price in prices[1:]:
+    # if price - current_price is greater than profit
+    # set profit to price - current_price
+    if (price - current_price) > profit:
+      profit = price - current_price
+    # if the price is less than the current_price 
+    # set current_price equal to the next price
+    if price < current_price:
+      current_price = price
+    
+
+  return profit
 
 
 if __name__ == '__main__':
