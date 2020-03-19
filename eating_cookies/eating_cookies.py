793f7dc4d52cbaea96d6eaa9d08821786f63d159
mefrem <maxim.efremov@gmail.com>
2020-03-18 21:16:34 -0400

    cookies problem

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,8 +5,22 @@ import sys
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
-def eating_cookies(n, cache=None):
-  pass
+
+def __calculatePaths(currPathLength, paths, currSeries):
+    if currPathLength == 0:
+        return 1
+    elif currPathLength < 0:
+        return 0
+    paths = 0
+    for i in range(1, 4):
+        newSeries = list(currSeries)  # make new series to track steps
+        newSeries.append(i)
+        paths += __calculatePaths(currPathLength - i, paths, newSeries)
+    return paths
+
+def eating_cookies(pathLength, cache=None):
+    paths = __calculatePaths(pathLength, 0, [])
+    return paths
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
