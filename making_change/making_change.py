3c766b5641d150edb73d1bf0f975730b8edd9618
mefrem <maxim.efremov@gmail.com>
2020-03-19 17:16:56 -0400

    making change assignment

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -3,7 +3,12 @@
 import sys
 
 def making_change(amount, denominations):
-  pass 
+    cache = [0] * (amount + 1)
+    cache[0] = 1
+    for coin in denominations:
+        for higher_amount in range(coin, amount + 1):
+            cache[higher_amount] += cache[higher_amount - coin]
+    return cache[amount]
 
 
 if __name__ == "__main__":
