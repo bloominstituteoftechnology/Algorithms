f121274ef6f039f82605e48d0dd61d296c9696eb
Bryan <17adamsbryk@washk12.org>
2020-04-15 18:49:13 -0600

    Got the command arguments working

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,4 +1,4 @@
-
+#!/c/Users/Bryan/AppData/Local/Programs/Python/Python38/python
 
 import argparse
 
@@ -20,12 +20,13 @@ def find_max_profit(prices):
   return max_profit
 
 
-print(find_max_profit([45, 24, 35, 31, 40, 38, 11]))
+print(find_max_profit([1050, 270, 1540, 3800, 2]))
 
-# if __name__ == '__main__':
-#   # This is just some code to accept inputs from the command line
-#   parser = argparse.ArgumentParser(description='Find max profit from prices.')
-#   parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-#   args = parser.parse_args()
+if __name__ == '__main__':
+  # This is just some code to accept inputs from the command line
+  parser = argparse.ArgumentParser(description='Find max profit from prices.')
+  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
+  args = parser.parse_args()
 
-#   print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+  
--- stock_prices/tempCodeRunnerFile.py
+++ stock_prices/tempCodeRunnerFile.py
@@ -1,1 +1,7 @@
-[45, 24, 35, 31, 40, 38, 11]
+# if __name__ == '__main__':
+#   # This is just some code to accept inputs from the command line
+#   parser = argparse.ArgumentParser(description='Find max profit from prices.')
+#   parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
+#   args = parser.parse_args()
+
+#   print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
