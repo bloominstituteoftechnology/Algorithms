2be23fa0c589c395fbe67fed8d64b785fe8b5aa8
Dorothy Gasque <dggasque@gmail.com>
2020-04-15 17:20:08 -0700

    Update eating_cookies.py
    Now works on large numbers

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,11 +6,19 @@ import sys
 # a solution that is more efficient than the naive 
 # recursive solution
 def eating_cookies(n, cache=None):
+  if cache is None:
+    cache = [0 for i in range(n+1)]
   if n < 0:
     return 0
-  if n == 0:
+  elif n >= 0 and n <= 1:
     return 1
-  return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+  elif cache[n] != 0:
+    return cache[n]
+  else: 
+    answer = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+    cache[n] = answer
+    
+    return answer
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
