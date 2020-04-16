aa99c5c59ef3ae29a888c85b298976df0fd4ed78
cappers86 <52895127+cappers86@users.noreply.github.com>
2020-04-16 16:32:48 +0100

    cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,8 +5,23 @@ import sys
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
+
+##eating cookies is going to allow us to figure out how many Ways there are to eating cookies 
+## n is effictbley cookies with 1,1,1 1,2 2,1 3
 def eating_cookies(n, cache=None):
-  pass
+  count = 0 
+  if n == 0 or n == 1:
+    return 1
+  elif n == 2:
+    return 2
+  else: 
+    arr = [1, 1, 2]
+    for number in range(3, n+1):
+      count = arr[-3] + arr[-2] + arr[-1]
+      arr.append(count)
+    
+    return count
+
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
