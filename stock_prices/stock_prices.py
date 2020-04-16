4dbe463bacb36b1df5a8b1bb94a1bc94ab707359
Jaytee <Padilla.Jaytee@gmail.com>
2020-04-15 17:09:16 -0700

    Completed stock_prices algo

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,7 +3,31 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  # each value in prices represents a price on a given day. Once a stock is bought, it can only be sold at a price found to the right of it in the prices list
+  # loop through prices
+  # the value of the current index is represented as the buy price
+  # the value of all indexes after the current index is represented as the sell price
+  # at each interval of the loop, the buy price should be subtracted from the sell price
+  # the result of sell price - buy price is stored as max profit
+  # at each interval of the loop, if the result of the current inverval's sell price - buy price is a higher value than what's currently stored in max profit, then overwrite max profit's current value with new result
+  # return max profit
+  buy_price = 0
+  sell_price = 0
+  max_profit = 0
+
+  for i in range(0, len(prices)):
+    cur_index = i
+    buy_price = prices[i]
+
+    for x in range(cur_index, len(prices)):
+      sell_price = prices[x]
+      if max_profit < sell_price - buy_price:
+        max_profit = sell_price - buy_price
+
+  if max_profit == 0:
+    max_profit = buy_price * -1
+
+  return max_profit
 
 
 if __name__ == '__main__':
