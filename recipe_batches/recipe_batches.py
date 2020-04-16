59363cbf9a0a0194a6aa887c074ff2b751672bee
davidhennig <davidjhennig@gmail.com>
2020-04-15 20:08:09 -0400

    wednesday

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,7 +3,25 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  # for ingredient in recipe
+  # if recipe.keys() == ingredients.keys():
+  #   print(recipe.values())
+  
+  # for key in recipe.items() and ingredients.keys():
+  #   if recipe.keys() == ingredients.keys():
+  #     print(recipe[key], ingredients[key])
+  #   else:
+  #     return 0
+
+  for ingredient in ingredients:
+    if recipe.keys() == ingredients.keys():
+      if ingredients[ingredient] > recipe[ingredient]:
+        return math.floor(ingredients[ingredient] / recipe[ingredient])
+      else:
+        return 0
+      print(recipe[ingredient], ingredients[ingredient])
+    else:
+      return 0
 
 
 if __name__ == '__main__':
