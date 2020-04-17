452d6c000aa5f0869622022ef330d316a185fe5c
Jaytee <Padilla.Jaytee@gmail.com>
2020-04-16 17:11:32 -0700

    Currently working on recipe_batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,12 +3,37 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  # assuming the keys of both parameters match up, check if there's enough ingredients to make at least 1 batch
+  # if there's not enough to make at least 1 batch, 0 batches can be made
+  # if there's enough ingredients for at least 1 batch, check how many batches in total can be made based on supplied ingredients amount
+  # if recipe.keys() != ingredients.keys():
+  #   return 0
+
+  total_batches = 0
+  temp_list = [] # will be used to check if at least 1 batch can be made or not
+
+  # check if each of the keys that exist in recipe all exist in ingredients, if they don't, 0 batches can be created due to lack of proper ingredients
+  if all(key in ingredients.keys() for key in recipe.keys()): # NOTE: all() takes a single parameter, an interable. Not sure if this effects time complexity
+    for key, value in ingredients.items():
+      temp_list.append(value >= recipe.get(key))
+
+    # if the recipe and ingredients match up, check if there's enough to make a whole batch
+    # if there isn't, return the current value of total_batches
+    if all(temp_list) == False:
+      return total_batches
+
+  else:
+    return total_batches
+
+  
+
+
+
 
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
   # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
+  recipe = { 'milk': 100, 'butter': 50, 'flour': 5}
   ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -11,22 +11,41 @@ def find_max_profit(prices):
   # the result of sell price - buy price is stored as max profit
   # at each interval of the loop, if the result of the current inverval's sell price - buy price is a higher value than what's currently stored in max profit, then overwrite max profit's current value with new result
   # return max profit
-  buy_price = 0
-  sell_price = 0
-  max_profit = 0
+  # buy_price = 0
+  # sell_price = 0
+  # max_profit = 0
 
-  for i in range(0, len(prices)):
-    cur_index = i
-    buy_price = prices[i]
+  # for i in range(0, len(prices)):
+  #   cur_index = i
+  #   buy_price = prices[i]
 
-    for x in range(cur_index, len(prices)):
-      sell_price = prices[x]
-      if max_profit < sell_price - buy_price:
-        max_profit = sell_price - buy_price
+  #   for x in range(cur_index, len(prices)):
+  #     sell_price = prices[x]
+  #     if max_profit < sell_price - buy_price:
+  #       max_profit = sell_price - buy_price
 
-  # if max_profit's value is still zero or less after looping through the list, that means taking a loss on the buy_price's value is the optimal value
-  if max_profit <= 0:
-    max_profit = buy_price * -1
+  # # if max_profit's value is still zero or less after looping through the list, that means taking a loss on the buy_price's value is the optimal value
+  # if max_profit <= 0:
+  #   max_profit = buy_price * -1
+
+  # return max_profit
+
+  # current_min_price = prices[0]
+  # current_max_profit = prices[1] - current_min_price
+  # for i in range(1, len(prices)):
+  #     if prices[i] < current_min_price:
+  #         current_min_price = prices[i]
+  #     elif current_max_profit < prices[i] - current_min_price:
+  #         current_max_profit = prices[i] - current_min_price
+  # return current_max_profit
+
+  min_price = prices[0]
+  max_profit = prices[1] - min_price
+
+  for i in range(1, len(prices)):
+    price = prices[i]
+    max_profit = max(price - min_price, max_profit)
+    min_price = min(price, min_price)
 
   return max_profit
 
