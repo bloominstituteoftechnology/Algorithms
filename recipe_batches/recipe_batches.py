e9f776d5e83628699d328c145e7e0ee0dcd59c2b
Elisabeth <elisabeth@github.com>
2020-05-13 15:31:32 -1000

    Add recipe batches.

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -1,14 +1,28 @@
 #!/usr/bin/python
 
-import math
-
 def recipe_batches(recipe, ingredients):
-  pass 
+    max_batches = None
+    for item, qty in recipe.items():
+        try:
+            max_batches_for_item = ingredients[item] // qty
+        except KeyError:
+            return 0
+        
+        if max_batches is None:
+            max_batches = max_batches_for_item
+        else:
+            max_batches = min([max_batches_for_item, max_batches])
+    return max_batches
+            
+        
+        
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test 
+    # your implementation with different inputs
+    recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
+    ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+    print("{batches} batches can be made from the available ingredients: "
+          "'{ingredients}.".format(batches=recipe_batches(recipe, ingredients),
+          ingredients=ingredients))
