d55680759a6d71d5ffaefa4178437575e0889ebf
Jwilson1172 <jw59615@gmail.com>
2020-06-18 15:31:49 -0400

    mod 3&4 progress

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,13 +2,18 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
-    # Your code here
+def eating_cookies(n, cache):
+    if n < 0:
+        return 0
+    elif n == 0:
+        return 1
+    # check if the answer is in our cache
+    elif cache[n] > 0:
+        return cache[n]
+    else:
+        # otherwise, our cache doesn't contain the answer, so we'll use our
+        # recursive logic to calculate it, and then save the answer in our
+        # cache for future uses
+        cache[n] = eating_cookies(n-3, cache) + eating_cookies(n-2, cache)+ eating_cookies(n-1, cache)
+    return cache[n]
 
-    pass
-
-if __name__ == "__main__":
-    # Use the main function here to test out your implementation
-    num_cookies = 5
-
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
